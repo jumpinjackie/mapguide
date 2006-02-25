a678d35a31db5178c7e16e91bb44927af63756ff
harteshchhabra <harteshchhabra@example.org>
2006-02-25 22:42:58 +0000

    MG19
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/MgDev@67 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ Server/RepositoryAdmin/RestoreRepositories.bat
@@ -0,0 +1,2 @@
+php -n RepositoryAdmin.php -c Restore -i "%cd%\..\BackupRepositories\Library\HOT_BK_2006-06-30_00-00-00" -o "%cd%\..\Repositories\Library"
+php -n RepositoryAdmin.php -c Restore -i "%cd%\..\BackupRepositories\Site\HOT_BK_2006-06-30_00-00-00"    -o "%cd%\..\Repositories\Site"
