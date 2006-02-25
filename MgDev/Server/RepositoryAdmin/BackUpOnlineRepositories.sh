e222de397a63d1e8c9b2ebe61d7f366f04182b6a
harteshchhabra <harteshchhabra@example.org>
2006-02-25 22:42:58 +0000

    MG19
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@67 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ MgDev/Server/RepositoryAdmin/BackUpOnlineRepositories.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+if [ ! -d ../BackupRepositories/Library ]; then
+  mkdir -p ../BackupRepositories/Library
+fi
+if [ ! -d ../BackupRepositories/Site ]; then
+  mkdir -p ../BackupRepositories/Site
+fi
+
+export LD_LIBRARY_PATH=$(pwd)/../bin
+
+php -n RepositoryAdmin.php -c Backup -l 2 -i "$(pwd)/../Repositories/Library" -o "$(pwd)/../BackupRepositories/Library"
+php -n RepositoryAdmin.php -c Backup -l 2 -i "$(pwd)/../Repositories/Site"    -o "$(pwd)/../BackupRepositories/Site"
