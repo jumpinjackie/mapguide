448d212f07e1097396a51c9f985488aca94e1684
timstrang <timstrang@example.org>
2006-03-09 23:16:01 +0000

    MG84 - fix RepositoryAdmin (Backup and Restore MapGuide Server database)
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@269 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Server/RepositoryAdmin/BackUpOnlineRepositories.sh
+++ MgDev/Server/RepositoryAdmin/BackUpOnlineRepositories.sh
@@ -6,7 +6,5 @@ if [ ! -d ../BackupRepositories/Site ]; then
   mkdir -p ../BackupRepositories/Site
 fi
 
-export LD_LIBRARY_PATH=$(pwd)/../bin
-
-php -n RepositoryAdmin.php -c Backup -l 2 -i "$(pwd)/../Repositories/Library" -o "$(pwd)/../BackupRepositories/Library"
-php -n RepositoryAdmin.php -c Backup -l 2 -i "$(pwd)/../Repositories/Site"    -o "$(pwd)/../BackupRepositories/Site"
+php -c $(pwd) RepositoryAdmin.php -c Backup -l 2 -i "$(pwd)/../Repositories/Library" -o "$(pwd)/../BackupRepositories/Library"
+php -c $(pwd) RepositoryAdmin.php -c Backup -l 2 -i "$(pwd)/../Repositories/Site"    -o "$(pwd)/../BackupRepositories/Site"
