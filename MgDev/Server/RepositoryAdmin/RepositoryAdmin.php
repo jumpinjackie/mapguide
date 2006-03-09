609f0affaffc48a576f51a247ecb99041ace70be
timstrang <timstrang@example.org>
2006-03-09 17:41:12 +0000

    MG84 - fix RepositoryAdmin (Backup and Restore MapGuide Server database)
    git-svn-id: http://svn.osgeo.org/mapguide/branches/1.0.0@264 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Server/RepositoryAdmin/RepositoryAdmin.php
+++ MgDev/Server/RepositoryAdmin/RepositoryAdmin.php
@@ -1,5 +1,17 @@
 <?php
 
+    $version_required = "5.0.5";
+
+    if(!phpVersion_Check($version_required)) {
+        print "Your current PHP version ("
+               . phpversion() . ") is too low\n";
+        print "Please upgrade your copy of PHP ";
+        print "to at least version : "
+               .  $version_required;
+        print "\n";
+        exit;
+    }
+
     include 'RepositoryAdminApp.inc';
 
     try
@@ -15,5 +27,25 @@
     {
         echo $e->getMessage()."\n".$IDS_PROGRESS_OPERATION_FAILED;
     }
+
+function phpVersion_Check ($version) {
+
+    if(!function_exists("version_compare")) {
+    /* the function version_compare was
+    only introduced in PHP version 4.1.0
+    which is what we normally recommend as
+    the lower limit for our scripts.
+    
+    So if the function does not exist then
+    we must be using a copy of PHP < 4.1.0 */
+        return FALSE;
+    }
+
+    if(version_compare($version, phpversion()) <= 0){
+        return TRUE;
+    } else {
+        return FALSE;
+    }
+}
 
 ?>
