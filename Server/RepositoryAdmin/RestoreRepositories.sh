d5646e481295e141eaf1c81b6152dd54db9c8288
timstrang <timstrang@example.org>
2006-03-10 06:01:31 +0000

    MG84 - fix RepositoryAdmin (Backup and Restore MapGuide Server database)
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/MgDev@275 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Server/RepositoryAdmin/RestoreRepositories.sh
+++ Server/RepositoryAdmin/RestoreRepositories.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-php -c $(pwd) RepositoryAdmin.php -c Restore -i "$(pwd)/../BackupRepositories/Library/HOT_BK_2006-06-30_00-00-00" -o "$(pwd)/../Repositories/Library"
-php -c $(pwd) RepositoryAdmin.php -c Restore -i "$(pwd)/../BackupRepositories/Site/HOT_BK_2006-06-30_00-00-00"    -o "$(pwd)/../Repositories/Site"
+php -c $(pwd) RepositoryAdmin.php -c Restore -i "$(pwd)/../BackupRepositories/Library/CurrentHotBackup" -o "$(pwd)/../Repositories/Library"
+php -c $(pwd) RepositoryAdmin.php -c Restore -i "$(pwd)/../BackupRepositories/Site/CurrentHotBackup"    -o "$(pwd)/../Repositories/Site"
