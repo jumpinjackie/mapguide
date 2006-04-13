3efc28309fbb9ab50f60c53599e8e9326742ce45
vishalbangia <vishalbangia@example.org>
2006-04-13 00:12:29 +0000

    MG214
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@477 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/build_oem.sh
+++ MgDev/build_oem.sh
@@ -12,176 +12,329 @@
 #**********************************************************
 
 echo "MapGuide Open Source build script for OEM components"
-cd Oem
-
 INSTALLDIR=/usr/local/mapguideopensource
-
+CLEAN_FLAG=0
 while [ $# -gt 0 ]; do    # Until you run out of parameters . . .
   case "$1" in
     -prefix|--prefix)
               INSTALLDIR="$2"
               shift
               ;;
+    -clean|--clean)
+	      CLEAN_FLAG=1
+	      shift
+	      ;;
     -help|--help)
               echo "Usage: $0 (options)"
               echo "Options:"
               echo "  --prefix [installation directory]"
-              echo "  --help Display usage"
+	      echo "  --clean [clean all objects and binaries in Oem]"
+	      echo "  --help [Display usage]"
+	      exit	
               ;;
   esac
   shift       # Check next set of parameters.
 done
+
+
+
+
 echo "OEM Build Initialization ............................"
 echo "Oem Libraries will be installed at: ${INSTALLDIR}"
+
+LIB_NAME=""
+
+#**********************************************************
+# Generic Functions
+#**********************************************************
+check_build ()
+{
+  if [ $? -ne 0 ]; then
+    error=$?
+    echo "$LIB_NAME: Error build failed ($error)................."
+    exit $error
+  fi
+}
+check_clean ()
+{
+  if [ $? -ne 0 ]; then
+    error=$?
+    echo "$LIB_NAME: Error clean failed ($error)................."
+    exit $error
+  fi
+}
+
 
 #**********************************************************
 # Build ACE 5.4
 # Notes: none
 #**********************************************************
+init_ace ()
+{
+	LIB_NAME="ACE 5.4"
+}
 
-echo "ACE 5.4: Initialization ............................"
-cd ACE/ACE_wrappers
-ACE_PATH="${PWD}"
+build_ace ()
+{
+	pushd ACE/ACE_wrappers
+	ACE_PATH="${PWD}"
+	pushd ace
+	env ACE_ROOT="${ACE_PATH}" make
+	check_build
+	popd 
+	popd
+}
 
-echo "ACE 5.4: Configuration / Building .................."
-cd ace
-env ACE_ROOT="${ACE_PATH}" make
-
-echo "ACE 5.4: Build Successful .........................."
-cd ../../..
+clean_ace ()
+{
+	pushd ACE/ACE_wrappers
+	ACE_PATH="${PWD}"
+	pushd ace
+	env ACE_ROOT="${ACE_PATH}" make clean
+	check_clean
+	popd 
+	popd
+}
 
 
 #**********************************************************
-# Build DWFTK 7.1
+# Build DWFCORE 1.0
 # Notes: none
 #**********************************************************
+init_dwfcore ()
+{
+	LIB_NAME="DWFCORE 1.0"
+	pushd DWFTK7.1/develop/global/build/gnu/dwfcore
+	sh ./build_setup.sh
+	popd
+}
 
-echo "DWFTK 7.1: Initialization .........................."
+build_dwfcore ()
+{
+	pushd DWFTK7.1/develop/global/src/dwfcore
+	sh ./configure --enable-optimized --prefix="${INSTALLDIR}"
+	make
+	check_build
+	popd
+}
 
-# dwfcore
-cd DWFTK7.1/develop/global/build/gnu/dwfcore
-sh ./build_setup.sh
+clean_dwfcore ()
+{
+	pushd DWFTK7.1/develop/global/src/dwfcore
+	make clean
+	check_clean
+	popd
 
-# dwftoolkit
-cd ../dwftoolkit
-sh ./build_setup.sh
+}
 
-#dwfemap
-cd ../dwfemap
-sh ./build_setup.sh
+#**********************************************************
+# Build DWFTOOLKIT 7.1
+# Notes: none
+#**********************************************************
+init_dwftk ()
+{
+	LIB_NAME="DWFTK 7.1"
+	pushd DWFTK7.1/develop/global/build/gnu/dwftoolkit
+	sh ./build_setup.sh
+	popd	
+}
 
-# configure and build dwfcore
-echo "DWFTK 7.1: Configure dwfcore ......................."
-cd ../../../src/dwfcore
-sh ./configure --enable-optimized --prefix="${INSTALLDIR}"
+build_dwftk ()
+{
+	pushd DWFTK7.1/develop/global/src/dwf
+	sh ./configure --enable-optimized --prefix="${INSTALLDIR}"
+	make
+	check_build
+	popd
+}
 
-echo "DWFTK 7.1: Make dwfcore ............................"
-make
+clean_dwftk ()
+{
+	pushd DWFTK7.1/develop/global/src/dwf
+	make clean
+	check_clean
+	popd
 
-# configure and build dwftoolkit
-echo "DWFTK 7.1: Configure dwftoolkit ...................."
-cd ../dwf
-sh ./configure --enable-optimized --prefix="${INSTALLDIR}"
+}
+#**********************************************************
+# Build DWFEMAP 1.0
+# Notes: none
+#**********************************************************
+init_dwfemap ()
+{
+	LIB_NAME="DWFEMAP 1.0"
+	pushd DWFTK7.1/develop/global/build/gnu/dwfemap
+	sh ./build_setup.sh
+	popd	
+}
 
-echo "DWFTK 7.1: Make dwftoolkit ........................."
-make
+build_dwfemap ()
+{
+	pushd DWFTK7.1/develop/global/src/dwfemap
+	sh ./configure --enable-optimized --prefix="${INSTALLDIR}"
+	make
+	check_build
+	popd
+}
 
-# configure and build emaptoolkit
-echo "DWFTK 7.1: Configure emaptoolkit ..................."
-cd ../dwfemap
-sh ./configure --enable-optimized --prefix="${INSTALLDIR}"
-
-echo "DWFTK 7.1: Make emaptoolkit ........................"
-make 
-
-echo "DWFTK 7.1: Build Successful ........................"
-cd ../../../../..
-
-
+clean_dwfemap ()
+{
+	pushd DWFTK7.1/develop/global/src/dwfemap
+	make clean
+	check_clean
+	popd
+}
 #**********************************************************
 # Build GEOS 2.2.0
 # Notes: none
 #**********************************************************
+init_geos ()
+{
+	LIB_NAME="GEOS 2.2.0"
+}
 
-echo "GEOS 2.2.0: Initialization ........................."
-cd geos-2.2.0
+build_geos ()
+{
+	pushd geos-2.2.0
+	sh ./configure --prefix="${INSTALLDIR}"
+	make
+	# The check build is disabled as the build will fail with automake version < 2.59
+	# check_build
+	popd
+}
 
-echo "GEOS 2.2.0: Configuration .........................."
-sh ./configure --prefix="${INSTALLDIR}"
-
-echo "GEOS 2.2.0: Make ..................................."
-make
-
-echo "GEOS 2.2.0: Build Successful ......................."
-cd ..
-
+clean_geos ()
+{
+	pushd geos-2.2.0
+	make clean
+	check_clean
+	popd
+}
 
 #**********************************************************
 # Build SWIGEx 1.0
 # Notes: none
 #**********************************************************
+init_swigex ()
+{
+	LIB_NAME="SWIGEx 1.0"
+}
 
-echo "SWIGEx 1.0: Initialization ........................."
-cd SWIGEx
-mkdir -p Linux/obj/release
+build_swigex ()
+{
+	pushd SWIGEx
+	mkdir -p Linux/obj/release	
+	make
+	check_build
+	popd
+}
 
-echo "SWIGEx 1.0: Configuration / Building ..............."
-make
-
-echo "SWIGEx 1.0: Build Successful ......................."
-cd ..
-
+clean_swigex ()
+{
+	pushd SWIGEx
+	# Note: the qmake system is required to regenerate the Makefile.
+	#   	This is disabled.
+	# make clean
+	# check_clean
+	popd
+}
 
 #**********************************************************
 # Build BDBXML 2.2.13
 # Notes: none
 #**********************************************************
+init_bdxml ()
+{
+	LIB_NAME="BDBXML 2.2.13"
+}
 
-echo "BDBXML 2.2.13: Initialization ......................"
-cd dbxml-2.2.13
+build_bdxml ()
+{
+	pushd dbxml-2.2.13
+	sh ./buildall.sh
+	check_build
+	popd 
+}
 
-echo "BDBXML 2.2.13: Configuration / Building ............"
-sh ./buildall.sh
-
-echo "BDBXML 2.2.13: Build Successful ...................."
-cd ..
-
-
+clean_bdxml ()
+{
+	pushd dbxml-2.2.13
+	sh ./buildall.sh --clean
+	check_clean
+	popd 
+}
 #**********************************************************
 # Build CPPUNIT 1.9.14
 # Notes: none
 #**********************************************************
 
-echo "CPPUNIT 1.9.14: Initialization ....................."
-cd CppUnit-1.9.14
+init_cppunit ()
+{
+	LIB_NAME="CPPUNIT 1.9.14"
+}
 
-echo "CPPUNIT 1.9.14: Configuration ......................"
-sh ./configure --prefix="${INSTALLDIR}"
+build_cppunit ()
+{
+  	pushd CppUnit-1.9.14
+	sh ./configure --prefix="${INSTALLDIR}"
+	make 
+	check_build
+	popd
+}
 
-echo "CPPUNIT 1.9.14: Make ..............................."
-make
-
-echo "CPPUNIT 1.9.14: Build Successful ..................."
-cd ..
-
+clean_cppunit ()
+{
+  	pushd CppUnit-1.9.14
+	sh ./configure --prefix="${INSTALLDIR}"
+	make clean 
+	check_clean
+	popd
+}
 
 #**********************************************************
 # Build IMake 1.0
 # Notes: none
 #**********************************************************
+init_imake ()
+{
+	LIB_NAME="IMake 1.0"
+}
 
-echo "IMake 1.0: Initialization .........................."
-cd ../BuildTools/WebTools/IMake
-mkdir -p Linux/obj/debug
+build_imake ()
+{
+	pushd ../BuildTools/WebTools/IMake
+	mkdir -p Linux/obj/debug
+	make
+	check_build
+	popd
+}
 
-echo "IMake 1.0: Configuration / Building ................"
-make
-
-echo "IMake 1.0: Build Successful ........................"
-
+clean_imake ()
+{
+	pushd ../BuildTools/WebTools/IMake
+	# Note: does not support clean as qmake is needed to regenerate the Makefile.
+	# make clean 
+	# check_clean
+	popd
+}
 
 #**********************************************************
-# Done
+# Script loop 
 #**********************************************************
-
-echo "Done building OEM components ......................."
+pushd Oem
+for lib in ace dwfcore dwftk dwfemap geos swigex bdxml cppunit imake;
+do
+	echo "$lib: Initialization..........................."
+	init_"$lib"
+	
+	if test "$CLEAN_FLAG" = "1"; then
+		echo "$lib: Clean ....................................."
+		clean_"$lib"
+		echo "$lib: Clean Successful .........................."
+	else
+		echo "$lib: Configure /Build..........................."
+		build_"$lib"	
+		echo "$lib: Build Successful .........................."
+	fi	
+done
+popd
