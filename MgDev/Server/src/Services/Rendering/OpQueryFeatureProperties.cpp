9e612c15dcedb9f3c894b82a4db1e009a0a407b0
traianstanev <traianstanev@example.org>
2006-04-13 19:47:05 +0000

    MG215: WMS feature info support requires new backend query API
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@481 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ MgDev/Server/src/Services/Rendering/OpQueryFeatureProperties.cpp
@@ -0,0 +1,110 @@
+//
+//  Copyright (C) 2004-2006  Autodesk, Inc.
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of version 2.1 of the GNU Lesser
+//  General Public License as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+#include "OpQueryFeatureProperties.h"
+#include "LogManager.h"
+
+
+MgOpQueryFeatureProperties::MgOpQueryFeatureProperties()
+{
+}
+
+
+MgOpQueryFeatureProperties::~MgOpQueryFeatureProperties()
+{
+}
+
+
+void MgOpQueryFeatureProperties::Execute()
+{
+    ACE_DEBUG((LM_DEBUG, ACE_TEXT("  (%t) MgOpQueryFeatureProperties::Execute()\n")));
+    ACE_ASSERT(0 != m_data);
+
+    bool operationCompleted = false;
+    bool argsRead = false;
+    Ptr<MgStream> stream;
+
+    MG_LOG_OPERATION_MESSAGE(L"QueryFeatures");
+
+    MG_TRY()
+
+    MG_LOG_OPERATION_MESSAGE_INIT(m_packet.m_OperationVersion, m_packet.m_NumArguments);
+
+    stream = new MgStream(m_data->GetStreamHelper());
+
+    if (5 == m_packet.m_NumArguments)
+    {
+        Ptr<MgMap> map = (MgMap*)stream->GetObject();
+        Ptr<MgStringCollection> layerNames = (MgStringCollection*)stream->GetObject();
+        Ptr<MgGeometry> geom = (MgGeometry*)stream->GetObject();
+
+        INT32 selectionVariant = 0;
+        stream->GetInt32(selectionVariant);
+
+        INT32 maxFeatures = 0;
+        stream->GetInt32(maxFeatures);
+
+        argsRead = true;
+
+        MG_LOG_OPERATION_MESSAGE_PARAMETERS_START();
+        MG_LOG_OPERATION_MESSAGE_ADD_STRING(L"MgMap");
+        MG_LOG_OPERATION_MESSAGE_ADD_SEPARATOR();
+        MG_LOG_OPERATION_MESSAGE_ADD_STRING(L"MgStringCollection");
+        MG_LOG_OPERATION_MESSAGE_ADD_SEPARATOR();
+        MG_LOG_OPERATION_MESSAGE_ADD_STRING(L"MgGeometry");
+        MG_LOG_OPERATION_MESSAGE_ADD_SEPARATOR();
+        MG_LOG_OPERATION_MESSAGE_ADD_STRING(L"selectionVariant");
+        MG_LOG_OPERATION_MESSAGE_ADD_SEPARATOR();
+        MG_LOG_OPERATION_MESSAGE_ADD_STRING(L"maxFeatures");
+        MG_LOG_OPERATION_MESSAGE_PARAMETERS_END();
+
+        Ptr<MgBatchPropertyCollection> info =
+            m_service->QueryFeatureProperties(map, layerNames, geom, selectionVariant, maxFeatures);
+
+        operationCompleted = true;
+        WriteResponseStream(*stream, info);
+    }
+    else
+    {
+        MG_LOG_OPERATION_MESSAGE_PARAMETERS_START();
+        MG_LOG_OPERATION_MESSAGE_PARAMETERS_END();
+    }
+
+    if ( !argsRead )
+    {
+        throw new MgOperationProcessingException(L"MgOpQueryFeatureProperties.Execute",
+            __LINE__, __WFILE__, NULL, L"", NULL);
+    }
+
+    // Successful operation
+    MG_LOG_OPERATION_MESSAGE_ADD_STRING(MgResources::Success.c_str());
+
+    MG_CATCH(L"MgOpGeneratePlot.Execute")
+
+    if (mgException != 0 && !operationCompleted && stream != 0)
+    {
+        WriteResponseStream(*stream, mgException);
+
+        // Failed operation
+        MG_LOG_OPERATION_MESSAGE_ADD_STRING(MgResources::Failure.c_str());
+    }
+
+    // Add access log entry for operation
+    MG_LOG_OPERATION_MESSAGE_ACCESS_ENTRY();
+
+    MG_THROW()
+}
