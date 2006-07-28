6d4b42de743b7ecad059be348dfbb4378a8c3ce9
trevorwekel <trevorwekel@example.org>
2006-07-28 23:22:47 +0000

    MG343: Refactor Common Service code base to facilitate use in other applications
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/MgDev@728 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ UnitTest/Common/FoundationTest/FoundationTest.cpp
@@ -0,0 +1,55 @@
+// FoundationTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "TestResourceService.h"
+#include "SiteConnection.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    MgSiteConnection conn;
+
+    Ptr<MgResourceService> svc = (MgResourceService*) conn.CreateService(MgServiceType::ResourceService);
+
+    Ptr<MgByteReader> rdr = svc->EnumerateRepositories(L"Session");
+
+    Ptr<MgStringProperty> prop = new MgStringProperty();
+    prop->SetName(L"TempPath");
+    prop->SetValue(L"?*BadPathTextShouldFailAndThrowMgConfigurationException");
+
+    MgMemoryStreamHelper* helper = new MgMemoryStreamHelper();
+    Ptr<MgStream> stream = new MgStream(helper);
+
+    stream->WriteObject(prop);
+
+    MgObject* obj = stream->GetObject();
+
+    MgConfiguration* config = MgConfiguration::GetInstance();
+
+    config->LoadConfiguration(L"webconfig.ini");
+
+    Ptr<MgPropertyCollection> propCol = new MgPropertyCollection();
+    propCol->Add(prop);
+
+    // If you get an MgConfigurationException here then MgConfiguration is working properly.
+    config->SetProperties(L"GeneralProperties", propCol);
+
+    return 0;
+}
+
+
+bool InitializeStatic();
+
+bool bStatic = InitializeStatic();
+
+
+bool InitializeStatic()
+{
+    MgServiceFactory* fact = MgServiceFactory::GetInstance();
+
+    fact->RegisterService(MgServiceType::ResourceService, MgTestResourceService::CreateService);
+
+    return true;
+}
+
