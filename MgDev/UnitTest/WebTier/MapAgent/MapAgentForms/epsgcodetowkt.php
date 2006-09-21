f3e3fa25c87dd3e4c0e357e59d009f14fd5e9179
brucedechant <brucedechant@example.org>
2006-09-21 17:59:29 +0000

    MG401
    git-svn-id: http://svn.osgeo.org/mapguide/branches/1.0.x@826 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ MgDev/UnitTest/WebTier/MapAgent/MapAgentForms/epsgcodetowkt.php
@@ -0,0 +1,52 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<body>
+<?php
+    include "initwebtier.php";
+
+    $code = "";
+    $wkt = "";
+    $errorMsg = "";
+    $status = "";
+
+    try
+    {
+        echo "<b>Coordinate System API: ConvertEpsgCodeToWkt</b><br><br>";
+
+        // We have to use a factory because there is no direct access to the MgCoordinateSystem constructor
+        $ll84 = "GEOGCS [ \"Longitude / Latitude (WGS 84)\", DATUM [\"WGS 84\", SPHEROID [\"WGS 84\", 6378137.000000, 298.257224]], PRIMEM [ \"Greenwich\", 0.000000 ], UNIT [\"Decimal Degree\", 0.01745329251994330]]";
+        $factory = new MgCoordinateSystemFactory();
+        $mgcoordinatesystem = $factory->Create($ll84);
+        
+        $code = $_GET['CODE'];        
+        $wkt = $mgcoordinatesystem->ConvertEpsgCodeToWkt($code);
+        $status = "Pass";
+    }
+    catch ( MgException $e )
+    {
+        $errorMsg = $e->GetMessage();
+        $status = "Fail";
+    }
+    catch ( Exception $e )
+    {
+        $errorMsg = $e->getMessage();
+        $status = "Fail";
+    }
+    
+    echo "<b>EPSG Code:</b><br>";
+    echo "$code<br><br>";
+    echo "<b>OGC WKT:</b><br>";
+    echo "$wkt<br><br>";
+    echo "<b>Status:</b><br>";
+    echo "$status<br><br>";
+
+    if($errorMsg != "")
+    {
+        echo "<b>Error:</b><br>";
+        echo $errorMsg;
+    }
+    
+?>
+
+</body>
+</html>
