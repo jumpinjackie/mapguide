2839154d747705352941b0138bbcd01e2bc57e4e
brucedechant <brucedechant@example.org>
2006-10-04 17:52:58 +0000

    MG406 - Updated trunk with changes done in branch.
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@851 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Server/src/Services/Feature/ServerDescribeSchema.cpp
+++ MgDev/Server/src/Services/Feature/ServerDescribeSchema.cpp
@@ -303,8 +303,17 @@ MgFeatureSchemaCollection* MgServerDescribeSchema::DescribeSchema(MgResourceIden
             STRING::size_type nDelimiter = featureClass.find(L":");
             STRING primSchemaName;
             STRING primClassName;
-            primSchemaName = featureClass.substr(0, nDelimiter);
-            primClassName = featureClass.substr(nDelimiter + 1);
+
+            if(STRING::npos == nDelimiter)
+            {
+                primSchemaName = L"";
+                primClassName = featureClass;
+            }
+            else
+            {
+                primSchemaName = featureClass.substr(0, nDelimiter);
+                primClassName = featureClass.substr(nDelimiter + 1);
+            }
 
             if (schemaName != primSchemaName)
             {
@@ -371,8 +380,17 @@ MgFeatureSchemaCollection* MgServerDescribeSchema::DescribeSchema(MgResourceIden
                 STRING::size_type nDelimiter = attributeClass.find(L":");
                 STRING secSchemaName;
                 STRING secClassName;
-                secSchemaName = attributeClass.substr(0, nDelimiter);
-                secClassName = attributeClass.substr(nDelimiter + 1);
+
+                if(STRING::npos == nDelimiter)
+                {
+                    secSchemaName = L"";
+                    secClassName = attributeClass;
+                }
+                else
+                {
+                    secSchemaName = attributeClass.substr(0, nDelimiter);
+                    secClassName = attributeClass.substr(nDelimiter + 1);
+                }
 
                 // Get the relation name
                 STRING relationName;
@@ -1447,19 +1465,25 @@ MgPropertyDefinitionCollection* MgServerDescribeSchema::GetIdentityProperties(Mg
             FdoStringP qname = fc->GetQualifiedName();
             FdoStringP name = fc->GetName();
 
-            STRING qualifiedName = (const wchar_t*)qname;
-            STRING::size_type nLength = qualifiedName.length();
-
-            // Get identity properties for only the primary source (ie extensionName is blank)
-            STRING::size_type nExtensionStart = qualifiedName.find(L"[");
-            if (STRING::npos == nExtensionStart)
+            int idx1 = wcscmp(className.c_str(), qname);
+            int idx2 = wcscmp(className.c_str(), name);
+            // className can be either fully qualified or non-qualified name
+            if (idx1 == 0 || idx2 == 0)
             {
-                // retrieve identity properties from FDO
-                FdoPtr<FdoDataPropertyDefinitionCollection> fdpdc = fc->GetIdentityProperties();
+                STRING qualifiedName = (const wchar_t*)qname;
+                STRING::size_type nLength = qualifiedName.length();
 
-                MgServerGetFeatures msgf;
-                msgf.GetClassProperties(idProps, fdpdc);
-                break;
+                // Get identity properties for only the primary source (ie extensionName is blank)
+                STRING::size_type nExtensionStart = qualifiedName.find(L"[");
+                if (STRING::npos == nExtensionStart)
+                {
+                    // retrieve identity properties from FDO
+                    FdoPtr<FdoDataPropertyDefinitionCollection> fdpdc = fc->GetIdentityProperties();
+
+                    MgServerGetFeatures msgf;
+                    msgf.GetClassProperties(idProps, fdpdc);
+                    break;
+                }
             }
         }
     }
