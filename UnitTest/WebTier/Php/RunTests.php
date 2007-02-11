c681036f13d765a4e0057278ebeaf96c49a9cada
tomfukushima <tomfukushima@example.org>
2007-02-11 03:42:10 +0000

    Add missing copyrights
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/MgDev@1103 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- UnitTest/WebTier/Php/RunTests.php
+++ UnitTest/WebTier/Php/RunTests.php
@@ -1,4 +1,22 @@
 <?php
+
+//
+//  Copyright (C) 2004-2006  Autodesk, Inc.
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of version 2.1 of the GNU Lesser
+//  General Public License as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
 //Command line script that executes all tests in the given database files
 
 define('Run_MAIN_METHOD', true);
