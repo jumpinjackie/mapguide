d38966197654c7c24782bdf1a3bb792467fc41bd
chrisclaydon <chrisclaydon@example.org>
2007-05-10 18:50:22 +0000

    Fix # 103 - SchemaReport: Preview does not display for some feature source
    git-svn-id: http://svn.osgeo.org/mapguide/branches/1.2.x@1664 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Web/src/schemareport/displayschemafunctions.php
+++ MgDev/Web/src/schemareport/displayschemafunctions.php
@@ -53,47 +53,44 @@ function GetPropertyName($featureReader, $property, $propertyType)
 			break;
 		default : $val = NULL;
 	}
-    
+
 	return $val;
 }
 
 function CreateLayerDef($factory, $featureClass, $featureName, $geomName, $geomType)
 {
-	switch ($geomType) {
-		case "point ":
-			// Create a point rule
-			$color = 'FFFF0000';
-			$pointRule = $factory->CreatePointRule($color);
-			$pointTypeStyle = $factory->CreatepointTypeStyle($pointRule);
-			// Create a scale range
-			$minScale = '0';
-			$maxScale = '1000000000000';
-			$scaleRange = $factory->CreateScaleRange($minScale, $maxScale, $pointTypeStyle);
-			break;
-		case "curve ":
-			// Create a line rule
-			$color = 'FF0000FF';
-			$lineRule = $factory->CreateLineRule($color);
-			// Create a line type style
-			$lineTypeStyle = $factory->CreateLineTypeStyle($lineRule);
-			// Create a scale range
-			$minScale = '0';
-			$maxScale = '1000000000000';
-			$scaleRange = $factory->CreateScaleRange($minScale, $maxScale, $lineTypeStyle);
-			break;
-		case "surface ":
-			// Create three area rules for thress different scale ranges
-			$color = 'FFFFBF20';
-			$areaRule = $factory->CreateAreaRule($color);
-			// Create an area type style
-			$areaTypeStyle = $factory->CreateAreaTypeStyle($areaRule);
-			// Create a scale range
-			$minScale = '0';
-			$maxScale = '1000000000000';
-			$scaleRange = $factory->CreateScaleRange($minScale, $maxScale, $areaTypeStyle);
-			break;
+	$typeStyle = "";
+	$geomTypes = explode(' ', $geomType);
+	for($i=0;$i<(sizeof($geomTypes)-1);$i++)
+	{
+		switch ($geomTypes[$i]) {
+			case "point":
+				// Create a point rule
+				$color = 'FFFF0000';
+				$pointRule = $factory->CreatePointRule($color);
+				$typeStyle .= $factory->CreatePointTypeStyle($pointRule);
+				break;
+			case "curve":
+				// Create a line rule
+				$color = 'FF0000FF';
+				$lineRule = $factory->CreateLineRule($color);
+				// Create a line type style
+				$typeStyle .= $factory->CreateLineTypeStyle($lineRule);
+				break;
+			case "surface":
+				// Create three area rules for thress different scale ranges
+				$color = 'FFFFBF20';
+				$areaRule = $factory->CreateAreaRule($color);
+				// Create an area type style
+				$typeStyle .= $factory->CreateAreaTypeStyle($areaRule);
+				break;
+		}
 	}
-	
+
+	// Create a scale range
+	$minScale = '0';
+	$maxScale = '1000000000000';
+	$scaleRange = $factory->CreateScaleRange($minScale, $maxScale, $typeStyle);
 	$layerDefinition = $factory->CreateLayerDefinition($featureClass, $featureName, $geomName, $scaleRange);
 	return $layerDefinition;
 }
@@ -102,7 +99,7 @@ function CreateMapDef($factory, $className, $resId, $coordinate, $minX, $maxX, $
 {
 	// Inputs for Extents
 	$extents = $factory->CreateExtents($minX, $maxX, $minY, $maxY);
-	
+
 	// Inputs for MapLayer
 	$layerName = $className;
 	$selectable = 'true';
@@ -112,7 +109,7 @@ function CreateMapDef($factory, $className, $resId, $coordinate, $minX, $maxX, $
 	$visible = 'true';
 	$groupName = 'Geometry';
 	$mapLayer = $factory->CreateMapLayer($layerName, $resId, $selectable, $showLegend, $legendLabel, $expandLegend, $visible, $groupName);
-	
+
 	// Inputs for MapLayerGroup
 	$groupName = 'Geometry';
 	$visible = 'true';
@@ -120,12 +117,12 @@ function CreateMapDef($factory, $className, $resId, $coordinate, $minX, $maxX, $
 	$expandLegend = 'true';
 	$legendLabel = 'Geometry';
 	$mapLayerGroup = $factory->CreateMapLayerGroup($groupName, $visible, $showLegend, $expandLegend, $legendLabel);
-	
+
 	// Inputs for MapDefinition
 	$mapName = 'Geometry Map';
 	$backgroundColor = 'FFCDBD9C';
 	$mapDefinition = $factory->CreateMapDefinition($mapName, $coordinate, $extents, $backgroundColor, $mapLayer, $mapLayerGroup);
-	
+
 	return $mapDefinition;
 }
 
@@ -134,7 +131,7 @@ function CreateWebLay($factory, $resId)
 	// Inputs for WebLayout
 	$layoutName = 'Geometry Layout';
 	$webLayout = $factory->CreateWebLayout($layoutName, $resId);
-	
+
 	return $webLayout;
 }
 
@@ -149,7 +146,7 @@ function DisplayPaging($index, $resName, $schemaName, $className, $sessionId, $m
 	}
 	else
 	  	echo '&lt;&lt;FIRST&nbsp&nbsp&nbsp&lt;PREV';
-	  
+
 	echo '&nbsp;&nbsp;&nbsp;Page ' . $currentPage . ' of ' . $maxPage . '&nbsp;&nbsp;&nbsp;';
 
 	if($currentPage<$maxPage)
