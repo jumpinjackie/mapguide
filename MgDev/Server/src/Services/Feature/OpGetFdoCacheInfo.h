c367c15edc9f58b0b77e5abaef85d6334be0b6fd
stevedang <stevedang@example.org>
2007-05-11 04:39:34 +0000

    Fix some compiling errors.
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@1678 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Server/src/Services/Feature/OpGetFdoCacheInfo.h
+++ MgDev/Server/src/Services/Feature/OpGetFdoCacheInfo.h
@@ -31,36 +31,3 @@ public:
 };
 
 #endif
-//
-//  Copyright (C) 2004-2007 by Autodesk, Inc.
-//
-//  This library is free software; you can redistribute it and/or
-//  modify it under the terms of version 2.1 of the GNU Lesser
-//  General Public License as published by the Free Software Foundation.
-//
-//  This library is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-//  Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public
-//  License along with this library; if not, write to the Free Software
-//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-//
-
-#ifndef MG_OP_GETFDOCACHEINFO_H
-#define MG_OP_GETFDOCACHEINFO_H
-
-#include "FeatureOperation.h"
-
-class MgOpGetFdoCacheInfo : public MgFeatureOperation
-{
-public:
-
-    MgOpGetFdoCacheInfo();
-    virtual ~MgOpGetFdoCacheInfo();
-
-    virtual void Execute();
-};
-
-#endif
