fb8cc23f6b2efa4747bd5cb0c7569eb3f0552851
philipsharman <philipsharman@example.org>
2007-05-16 21:25:18 +0000

    Added the Doxygen configuration file and the support files required to bulid the Web API Reference HTML documentation.
    git-svn-id: http://svn.osgeo.org/mapguide/branches/1.2.x@1717 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ MgDev/Doc/external_topics/Glossary_FDO.h
@@ -0,0 +1,10 @@
+/**
+\addtogroup FDO
+<p>
+FDO stands for Feature Data Objects. The FDO API provides an
+encapsulation layer for accessing feature data in a broad
+range of commercial and open source data storage
+technologies.
+</p>
+
+**/
---
+++ MgDev/Doc/external_topics/Glossary_library.h
@@ -0,0 +1,17 @@
+/**
+\addtogroup library
+
+<p>
+One of the two types of \link resource_repository resource repositories \endlink
+. The <i>library</i> is used for persistent storage. It
+corresponds to what is shown in the Site Explorer when you
+use the Studio application.
+</p>
+<h3>
+See Also
+</h3>
+<p>
+\link session_repository session repository \endlink
+</p>
+
+**/
