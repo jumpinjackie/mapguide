39260485b5308f58f308256f389b3a73e3aea6bf
timstrang <timstrang@example.org>
2007-06-12 17:16:46 +0000

    Fix #201 Repository Admin scripts fail when using PHP4
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/MgDev@1812 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Server/RepositoryAdmin/BackUpOnlineRepositories.sh
+++ Server/RepositoryAdmin/BackUpOnlineRepositories.sh
@@ -6,5 +6,9 @@ if [ ! -d ../BackupRepositories/Site ]; then
   mkdir -p ../BackupRepositories/Site
 fi
 
-php -c $(pwd) RepositoryAdmin.php -c Backup -l 2 -i "$(pwd)/../Repositories/Library" -o "$(pwd)/../BackupRepositories/Library"
-php -c $(pwd) RepositoryAdmin.php -c Backup -l 2 -i "$(pwd)/../Repositories/Site"    -o "$(pwd)/../BackupRepositories/Site"
+php -c $(pwd) CheckVersion.php
+STATUS=$?
+if [ $STATUS -eq 0 ]; then
+  php -c $(pwd) RepositoryAdmin.php -c Backup -l 2 -i "$(pwd)/../Repositories/Library" -o "$(pwd)/../BackupRepositories/Library"
+  php -c $(pwd) RepositoryAdmin.php -c Backup -l 2 -i "$(pwd)/../Repositories/Site"    -o "$(pwd)/../BackupRepositories/Site"
+fi
