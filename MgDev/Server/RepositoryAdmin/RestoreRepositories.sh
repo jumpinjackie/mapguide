7087c9d91c3bc19118bd550a7cd4d3676bd42d6c
timstrang <timstrang@example.org>
2007-06-12 17:16:46 +0000

    Fix [https://trac.osgeo.org/mapguide/ticket/201] Repository Admin scripts fail when using PHP4
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@1812 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Server/RepositoryAdmin/RestoreRepositories.sh
+++ MgDev/Server/RepositoryAdmin/RestoreRepositories.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-php -c $(pwd) RepositoryAdmin.php -c Restore -i "$(pwd)/../BackupRepositories/Library/CurrentHotBackup" -o "$(pwd)/../Repositories/Library"
-php -c $(pwd) RepositoryAdmin.php -c Restore -i "$(pwd)/../BackupRepositories/Site/CurrentHotBackup"    -o "$(pwd)/../Repositories/Site"
+php -c $(pwd) CheckVersion.php
+STATUS=$?
+if [ $STATUS -eq 0 ]; then
+  php -c $(pwd) RepositoryAdmin.php -c Restore -i "$(pwd)/../BackupRepositories/Library/CurrentHotBackup" -o "$(pwd)/../Repositories/Library"
+  php -c $(pwd) RepositoryAdmin.php -c Restore -i "$(pwd)/../BackupRepositories/Site/CurrentHotBackup"    -o "$(pwd)/../Repositories/Site"
+fi
