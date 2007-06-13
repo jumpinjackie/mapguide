d49f899fe516236cebf813d9a9ae6839d2f7c177
chrisclaydon <chrisclaydon@example.org>
2007-06-13 19:15:05 +0000

    Fix #207 - mapadmin/constants.php file name is different in Win and Linux (Submitted for Rex Szeto)
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/MgDev@1823 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Web/src/schemareport/displayschema.php
+++ Web/src/schemareport/displayschema.php
@@ -17,7 +17,7 @@
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 
-    include '../mapadmin/Constants.php';
+    include '../mapadmin/constants.php';
     include 'stringconstants.php';
 ?>
 
