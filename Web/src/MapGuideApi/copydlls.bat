c978263fae34fb8b0eb410d5e049d86d7b759dbb
waltweltonlair <waltweltonlair@example.org>
2007-06-18 16:36:17 +0000

    Web-tier build fixes: - delete unused cleanall.bat files in MapGuideApi project - copy isapi_fcgi.dll to mapagent run directory - make FastCgiAgent the default project in Web.sln
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/MgDev@1831 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Web/src/MapGuideApi/copydlls.bat
+++ Web/src/MapGuideApi/copydlls.bat
@@ -4,27 +4,27 @@ REM For Windows only
 REM ----------------
 
 REM **** Do not edit these unless you know what you are doing *****
-SET WEB_BIN_DEBUG=..\..\bin\debug
-SET WEB_BIN_RELEASE=..\..\bin\release
+set WEB_BIN_DEBUG=..\..\bin\debug
+set WEB_BIN_RELEASE=..\..\bin\release
 
-SET WEB_ROOT=..\..
+set WEB_ROOT=..\..
 
-SET COMMON_ROOT=..\..\..\Common
+set COMMON_ROOT=..\..\..\Common
 
-SET COMMON_BIN_DEBUG=..\..\..\Common\bin\debug
-SET COMMON_BIN_RELEASE=..\..\..\Common\bin\release
+set COMMON_BIN_DEBUG=..\..\..\Common\bin\debug
+set COMMON_BIN_RELEASE=..\..\..\Common\bin\release
 
 REM **** Edit this to point to your PHP Extension directory *****
-SET PHP_EXT_DIR=C:\PHP\ext
+set PHP_EXT_DIR=C:\PHP\ext
 
-IF "%1"=="BUILD" GOTO copy_build
-IF "%1"=="RELEASE" GOTO copy_release
-IF "%1"=="DEBUG" GOTO copy_debug
+if "%1"=="BUILD" goto copy_build
+if "%1"=="RELEASE" goto copy_release
+if "%1"=="DEBUG" goto copy_debug
 
 REM ---- DEFAULT -----
 :default
 echo Need to use RELEASE, DEBUG or BUILD arguments
-GOTO end_copy
+goto end_copy
 
 REM ---- RELEASE -----
 REM This assumes all DLLs are copied to the release folder
@@ -61,8 +61,11 @@ copy "..\..\..\Oem\proj-4.5.0\src\proj.dll" %WEB_BIN_RELEASE%
 echo Copying Xerces Release DLL to %WEB_BIN_RELEASE%
 copy "..\..\..\Oem\dbxml-2.2.13\bin\xerces-c_2_7.dll" %WEB_BIN_RELEASE%
 
-echo Copying LIBFCGI Release DLL to %WEB_ROOT%\src\MapAgent
-copy "..\..\..\Oem\fcgi\fcgi-2.4.0\libfcgi\Release\libfcgi.dll" %WEB_ROOT%\src\MapAgent
+echo Copying LIBFCGI Release DLL to %WEB_ROOT%\src\mapagent
+copy "..\..\..\Oem\fcgi\fcgi-2.4.0\libfcgi\Release\libfcgi.dll" %WEB_ROOT%\src\mapagent
+
+echo Copying ISAPI_FCGI Release DLL to %WEB_ROOT%\src\mapagent
+copy "..\..\..\Oem\fcgi\isapi\isapi_fcgi\Release\isapi_fcgi.dll" %WEB_ROOT%\src\mapagent
 
 goto end_copy
 
@@ -101,22 +104,26 @@ copy "..\..\..\Oem\proj-4.5.0\src\projd.dll" %WEB_BIN_DEBUG%
 echo Copying Xerces Debug DLL to %WEB_BIN_DEBUG%
 copy "..\..\..\Oem\dbxml-2.2.13\bin\debug\xerces-c_2_7D.dll" %WEB_BIN_DEBUG%
 
-echo Copying LIBFCGI Debug DLL to %WEB_ROOT%\src\MapAgent
-copy "..\..\..\Oem\fcgi\fcgi-2.4.0\libfcgi\Debug\libfcgid.dll" %WEB_ROOT%\src\MapAgent
+echo Copying LIBFCGI Debug DLL to %WEB_ROOT%\src\mapagent
+copy "..\..\..\Oem\fcgi\fcgi-2.4.0\libfcgi\Debug\libfcgid.dll" %WEB_ROOT%\src\mapagent
+
+echo Copying ISAPI_FCGI Debug DLL to %WEB_ROOT%\src\mapagent
+copy "..\..\..\Oem\fcgi\isapi\isapi_fcgi\Debug\isapi_fcgi.dll" %WEB_ROOT%\src\mapagent
 
 goto end_copy
 
 REM ---- BUILD -----
-:copy_build
 REM This assumes the batch file is being run in the build folder
 REM and that the dependent DLLs are in the current directory
 
-SET /P COPY_DIR="Where do you want to copy the files to? "
+:copy_build
+set /P COPY_DIR="Where do you want to copy the files to? "
 
 echo Copying all dependent DLLs to %COPY_DIR%
 copy Mg*.dll %COPY_DIR%
 copy ACE*.dll %COPY_DIR%
 copy libfcgi*.dll %COPY_DIR%
+copy isapi*.dll %COPY_DIR%
 copy proj*.dll %COPY_DIR%
 
 goto end_copy
