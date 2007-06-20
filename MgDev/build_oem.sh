46a0548b975804bd589274a28eee65a392a79776
waltweltonlair <waltweltonlair@example.org>
2007-06-20 08:20:21 +0000

    The GDAL and PROJ components are OEM components, yet in Linux they are still being built as part of the main build.  I've changed it so that now they are built along with the other OEM components. This involved: - updating build_oem.sh to now build these components - updating configure.in to no longer configure these components   as part of the main build - updating Oem/Makefile.am to install the necessary files
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@1836 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/build_oem.sh
+++ MgDev/build_oem.sh
@@ -2,10 +2,9 @@
 #! /bin/bash
 #
 
+#
 # MapGuide Open Source build script.
 #
-
-
 
 #**********************************************************
 # Initialization
@@ -14,30 +13,27 @@
 echo "MapGuide Open Source build script for OEM components"
 INSTALLDIR=/usr/local/mapguideopensource
 CLEAN_FLAG=0
-while [ $# -gt 0 ]; do    # Until you run out of parameters . . .
-  case "$1" in
-    -prefix|--prefix)
-              INSTALLDIR="$2"
-              shift
-              ;;
-    -clean|--clean)
-	      CLEAN_FLAG=1
-	      shift
-	      ;;
-    -help|--help)
-              echo "Usage: $0 (options)"
-              echo "Options:"
-              echo "  --prefix [installation directory]"
-	      echo "  --clean [clean all objects and binaries in Oem]"
-	      echo "  --help [Display usage]"
-	      exit	
-              ;;
-  esac
-  shift       # Check next set of parameters.
+while [ $# -gt 0 ]; do    # Until you run out of parameters...
+    case "$1" in
+        -prefix|--prefix)
+            INSTALLDIR="$2"
+            shift
+            ;;
+        -clean|--clean)
+            CLEAN_FLAG=1
+            shift
+            ;;
+        -help|--help)
+            echo "Usage: $0 (options)"
+            echo "Options:"
+            echo "  --prefix [installation directory]"
+            echo "  --clean [clean all objects and binaries in Oem]"
+            echo "  --help [Display usage]"
+            exit
+            ;;
+    esac
+    shift   # Check next set of parameters.
 done
-
-
-
 
 echo "OEM Build Initialization ............................"
 echo "Oem Libraries will be installed at: ${INSTALLDIR}"
@@ -47,272 +43,282 @@ LIB_NAME=""
 #**********************************************************
 # Generic Functions
 #**********************************************************
-check_build ()
+
+check_build()
 {
-  if [ $? -ne 0 ]; then
-    error=$?
-    echo "$LIB_NAME: Error build failed ($error)................."
-    exit $error
-  fi
-}
-check_clean ()
-{
-  if [ $? -ne 0 ]; then
-    error=$?
-    echo "$LIB_NAME: Error clean failed ($error)................."
-    exit $error
-  fi
+    if [ $? -ne 0 ]; then
+        error=$?
+        echo "$LIB_NAME: Error build failed ($error)................."
+        exit $error
+    fi
 }
 
+check_clean()
+{
+    if [ $? -ne 0 ]; then
+        error=$?
+        echo "$LIB_NAME: Error clean failed ($error)................."
+        exit $error
+    fi
+}
 
 #**********************************************************
 # Build ACE 5.4
 # Notes: none
 #**********************************************************
-init_ace ()
+
+init_ace()
 {
-	LIB_NAME="ACE 5.4"
+    LIB_NAME="ACE 5.4"
 }
 
-build_ace ()
+build_ace()
 {
-	pushd ACE/ACE_wrappers
-	ACE_PATH="${PWD}"
-	pushd ace
-	env ACE_ROOT="${ACE_PATH}" make
-	check_build
-	popd 
-	popd
+    pushd ACE/ACE_wrappers
+    ACE_PATH="${PWD}"
+    pushd ace
+    env ACE_ROOT="${ACE_PATH}" make
+    check_build
+    popd
+    popd
 }
 
-clean_ace ()
+clean_ace()
 {
-	pushd ACE/ACE_wrappers
-	ACE_PATH="${PWD}"
-	pushd ace
-	env ACE_ROOT="${ACE_PATH}" make clean
-	check_clean
-	popd 
-	popd
+    pushd ACE/ACE_wrappers
+    ACE_PATH="${PWD}"
+    pushd ace
+    env ACE_ROOT="${ACE_PATH}" make clean
+    check_clean
+    popd
+    popd
 }
-
 
 #**********************************************************
 # Build DWFCORE 1.0
 # Notes: none
 #**********************************************************
-init_dwfcore ()
+
+init_dwfcore()
 {
-	LIB_NAME="DWFCORE 1.0"
-	pushd DWFTK7.1/develop/global/build/gnu/dwfcore
-	sh ./build_setup.sh
-	popd
+    LIB_NAME="DWFCORE 1.0"
+    pushd DWFTK7.1/develop/global/build/gnu/dwfcore
+    sh ./build_setup.sh
+    popd
 }
 
-build_dwfcore ()
+build_dwfcore()
 {
-	pushd DWFTK7.1/develop/global/src/dwfcore
-	sh ./configure --enable-optimized --prefix="${INSTALLDIR}"
-	make
-	check_build
-	popd
+    pushd DWFTK7.1/develop/global/src/dwfcore
+    sh ./configure --enable-optimized --prefix="${INSTALLDIR}"
+    make
+    check_build
+    popd
 }
 
-clean_dwfcore ()
+clean_dwfcore()
 {
-	pushd DWFTK7.1/develop/global/src/dwfcore
-	make clean
-	check_clean
-	popd
-
+    pushd DWFTK7.1/develop/global/src/dwfcore
+    make clean
+    check_clean
+    popd
 }
 
 #**********************************************************
 # Build DWFTOOLKIT 7.1
 # Notes: none
 #**********************************************************
-init_dwftk ()
+
+init_dwftk()
 {
-	LIB_NAME="DWFTK 7.1"
-	pushd DWFTK7.1/develop/global/build/gnu/dwftoolkit
-	sh ./build_setup.sh
-	popd	
+    LIB_NAME="DWFTK 7.1"
+    pushd DWFTK7.1/develop/global/build/gnu/dwftoolkit
+    sh ./build_setup.sh
+    popd
 }
 
-build_dwftk ()
+build_dwftk()
 {
-	pushd DWFTK7.1/develop/global/src/dwf
-	sh ./configure --enable-optimized --prefix="${INSTALLDIR}"
-	make
-	check_build
-	popd
+    pushd DWFTK7.1/develop/global/src/dwf
+    sh ./configure --enable-optimized --prefix="${INSTALLDIR}"
+    make
+    check_build
+    popd
 }
 
-clean_dwftk ()
+clean_dwftk()
 {
-	pushd DWFTK7.1/develop/global/src/dwf
-	make clean
-	check_clean
-	popd
-
+    pushd DWFTK7.1/develop/global/src/dwf
+    make clean
+    check_clean
+    popd
 }
+
 #**********************************************************
 # Build DWFEMAP 1.0
 # Notes: none
 #**********************************************************
-init_dwfemap ()
+
+init_dwfemap()
 {
-	LIB_NAME="DWFEMAP 1.0"
-	pushd DWFTK7.1/develop/global/build/gnu/dwfemap
-	sh ./build_setup.sh
-	popd	
+    LIB_NAME="DWFEMAP 1.0"
+    pushd DWFTK7.1/develop/global/build/gnu/dwfemap
+    sh ./build_setup.sh
+    popd
 }
 
-build_dwfemap ()
+build_dwfemap()
 {
-	pushd DWFTK7.1/develop/global/src/dwfemap
-	sh ./configure --enable-optimized --prefix="${INSTALLDIR}"
-	make
-	check_build
-	popd
+    pushd DWFTK7.1/develop/global/src/dwfemap
+    sh ./configure --enable-optimized --prefix="${INSTALLDIR}"
+    make
+    check_build
+    popd
 }
 
-clean_dwfemap ()
+clean_dwfemap()
 {
-	pushd DWFTK7.1/develop/global/src/dwfemap
-	make clean
-	check_clean
-	popd
+    pushd DWFTK7.1/develop/global/src/dwfemap
+    make clean
+    check_clean
+    popd
 }
+
 #**********************************************************
 # Build GEOS 2.2.0
 # Notes: none
 #**********************************************************
-init_geos ()
+
+init_geos()
 {
-	LIB_NAME="GEOS 2.2.0"
+    LIB_NAME="GEOS 2.2.0"
 }
 
-build_geos ()
+build_geos()
 {
-	pushd geos-2.2.0
-	sh ./configure --prefix="${INSTALLDIR}"
-	make
-	# The check build is disabled as the build will fail with automake version < 2.59
-	# check_build
-	popd
+    pushd geos-2.2.0
+    sh ./configure --prefix="${INSTALLDIR}"
+    make
+    # The check build is disabled as the build will fail with automake version < 2.59
+    # check_build
+    popd
 }
 
-clean_geos ()
+clean_geos()
 {
-	pushd geos-2.2.0
-	make clean
-	check_clean
-	popd
+    pushd geos-2.2.0
+    make clean
+    check_clean
+    popd
 }
+
 #**********************************************************
 # Build PHP 5.2.1
 # Notes: Configure PHP source tree for building PHP extension
 #**********************************************************
-init_php ()
+
+init_php()
 {
-	LIB_NAME="PHP 5.2.1"
+    LIB_NAME="PHP 5.2.1"
 }
 
-build_php ()
+build_php()
 {
-	pushd php
-	sh ./configure 
-	check_build
-	popd
+    pushd php
+    sh ./configure
+    check_build
+    popd
 }
 
-clean_php ()
+clean_php()
 {
-	pushd php
-	make distclean
-	check_clean
-	popd
+    pushd php
+    make distclean
+    check_clean
+    popd
 }
 
 #**********************************************************
 # Build SWIGEx 1.0
 # Notes: none
 #**********************************************************
-init_swigex ()
+
+init_swigex()
 {
-	LIB_NAME="SWIGEx 1.0"
+    LIB_NAME="SWIGEx 1.0"
 }
 
-build_swigex ()
+build_swigex()
 {
-	pushd SWIGEx
-	mkdir -p Linux/obj/release	
-	make
-	check_build
-	popd
+    pushd SWIGEx
+    mkdir -p Linux/obj/release
+    make
+    check_build
+    popd
 }
 
-clean_swigex ()
+clean_swigex()
 {
-	pushd SWIGEx
-	# Note: the qmake system is required to regenerate the Makefile.
-	#   	This is disabled.
-	# make clean
-	# check_clean
-	popd
+    pushd SWIGEx
+    # Note: the qmake system is required to regenerate the Makefile.
+    #       This is disabled.
+    # make clean
+    # check_clean
+    popd
 }
 
 #**********************************************************
 # Build BDBXML 2.2.13
 # Notes: none
 #**********************************************************
-init_bdxml ()
+
+init_bdxml()
 {
-	LIB_NAME="BDBXML 2.2.13"
+    LIB_NAME="BDBXML 2.2.13"
 }
 
-build_bdxml ()
+build_bdxml()
 {
-	pushd dbxml-2.2.13
-	sh ./buildall.sh
-	check_build
-	popd 
+    pushd dbxml-2.2.13
+    sh ./buildall.sh
+    check_build
+    popd
 }
 
-clean_bdxml ()
+clean_bdxml()
 {
-	pushd dbxml-2.2.13
-	sh ./buildall.sh --clean
-	check_clean
-	popd 
+    pushd dbxml-2.2.13
+    sh ./buildall.sh --clean
+    check_clean
+    popd
 }
+
 #**********************************************************
 # Build CPPUNIT 1.9.14
 # Notes: none
 #**********************************************************
 
-init_cppunit ()
+init_cppunit()
 {
-	LIB_NAME="CPPUNIT 1.9.14"
+    LIB_NAME="CPPUNIT 1.9.14"
 }
 
-build_cppunit ()
+build_cppunit()
 {
-  	pushd CppUnit-1.9.14
-	sh ./configure --prefix="${INSTALLDIR}"
-	make 
-	check_build
-	popd
+    pushd CppUnit-1.9.14
+    sh ./configure --prefix="${INSTALLDIR}"
+    make
+    check_build
+    popd
 }
 
-clean_cppunit ()
+clean_cppunit()
 {
-  	pushd CppUnit-1.9.14
-	sh ./configure --prefix="${INSTALLDIR}"
-	make clean 
-	check_clean
-	popd
+    pushd CppUnit-1.9.14
+    sh ./configure --prefix="${INSTALLDIR}"
+    make clean
+    check_clean
+    popd
 }
 
 #**********************************************************
@@ -320,73 +326,129 @@ clean_cppunit ()
 # Notes: none
 #**********************************************************
 
-init_libfcgi ()
+init_libfcgi()
 {
-	LIB_NAME="LIBFCGI 2.4.0"
+    LIB_NAME="LIBFCGI 2.4.0"
 }
 
-build_libfcgi ()
+build_libfcgi()
 {
-  	pushd fcgi/fcgi-2.4.0
-	sh ./configure --prefix="${INSTALLDIR}"
-	make 
-	check_build
-	popd
+    pushd fcgi/fcgi-2.4.0
+    sh ./configure --prefix="${INSTALLDIR}"
+    make
+    check_build
+    popd
 }
 
-clean_libfcgi ()
+clean_libfcgi()
 {
-  	pushd fcgi/fcgi-2.4.0
-	sh ./configure --prefix="${INSTALLDIR}"
-	make clean 
-	check_clean
-	popd
+    pushd fcgi/fcgi-2.4.0
+    sh ./configure --prefix="${INSTALLDIR}"
+    make clean
+    check_clean
+    popd
 }
 
 #**********************************************************
 # Build IMake 1.0
 # Notes: none
 #**********************************************************
-init_imake ()
+
+init_imake()
 {
-	LIB_NAME="IMake 1.0"
+    LIB_NAME="IMake 1.0"
 }
 
-build_imake ()
+build_imake()
 {
-	pushd ../BuildTools/WebTools/IMake
-	mkdir -p Linux/obj/debug
-	make
-	check_build
-	popd
+    pushd ../BuildTools/WebTools/IMake
+    mkdir -p Linux/obj/debug
+    make
+    check_build
+    popd
 }
 
-clean_imake ()
+clean_imake()
 {
-	pushd ../BuildTools/WebTools/IMake
-	# Note: does not support clean as qmake is needed to regenerate the Makefile.
-	# make clean 
-	# check_clean
-	popd
+    pushd ../BuildTools/WebTools/IMake
+    # Note: does not support clean as qmake is needed to regenerate the Makefile.
+    # make clean
+    # check_clean
+    popd
 }
 
 #**********************************************************
-# Script loop 
+# Build GDAL 1.4.0
+# Notes: none
 #**********************************************************
+
+init_gdal()
+{
+    LIB_NAME="GDAL 1.4.0"
+}
+
+build_gdal()
+{
+    pushd gdal-1.4.0
+    sh ./configure --enable-optimized --prefix="${INSTALLDIR}"
+    make
+    check_build
+    popd
+}
+
+clean_gdal()
+{
+    pushd gdal-1.4.0
+    make clean
+    check_clean
+    popd
+}
+
+#**********************************************************
+# Build PROJ 4.5.0
+# Notes: none
+#**********************************************************
+
+init_proj()
+{
+    LIB_NAME="PROJ 4.5.0"
+}
+
+build_proj()
+{
+    pushd proj-4.5.0
+    sh ./configure --enable-optimized --prefix="${INSTALLDIR}"
+    make
+    check_build
+    popd
+}
+
+clean_proj()
+{
+    pushd proj-4.5.0
+    make clean
+    check_clean
+    popd
+}
+
+#**********************************************************
+# Script loop
+#**********************************************************
+
 pushd Oem
-for lib in ace dwfcore dwftk dwfemap geos php swigex bdxml cppunit libfcgi imake;
+for lib in ace dwfcore dwftk dwfemap geos php swigex bdxml cppunit libfcgi imake gdal proj;
 do
-	echo "$lib: Initialization..........................."
-	init_"$lib"
-	
-	if test "$CLEAN_FLAG" = "1"; then
-		echo "$lib: Clean ....................................."
-		clean_"$lib"
-		echo "$lib: Clean Successful .........................."
-	else
-		echo "$lib: Configure /Build..........................."
-		build_"$lib"	
-		echo "$lib: Build Successful .........................."
-	fi	
+    echo "$lib: Initialization..........................."
+    init_"$lib"
+
+    if test "$CLEAN_FLAG" = "1"; then
+        echo "$lib: Clean ....................................."
+        clean_"$lib"
+        echo "$lib: Clean Successful .........................."
+    else
+        echo "$lib: Configure /Build..........................."
+        build_"$lib"
+        echo "$lib: Build Successful .........................."
+    fi
 done
 popd
