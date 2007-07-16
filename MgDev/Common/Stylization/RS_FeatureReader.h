fe55fe1c7dca93571463f2c2fa52b7c35fe0d2ca
waltweltonlair <waltweltonlair@example.org>
2007-07-16 13:16:09 +0000

    Remove commented method in RS_FeatureReader.
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@1913 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Common/Stylization/RS_FeatureReader.h
+++ MgDev/Common/Stylization/RS_FeatureReader.h
@@ -22,8 +22,7 @@
 #include "CSysTransformer.h"
 #include "RS_Raster.h"
 
-//Defines a feature data reader interface.
-//
+// Defines a feature data reader interface.
 class RS_FeatureReader
 {
 public:
@@ -31,23 +30,22 @@ public:
     virtual void Close   () = 0;
     virtual void Reset   () = 0;
 
-    virtual bool             IsNull          (const wchar_t* propertyName) = 0;
-    virtual bool             GetBoolean      (const wchar_t* propertyName) = 0;
-    virtual unsigned char    GetByte         (const wchar_t* propertyName) = 0;
-    virtual FdoDateTime      GetDateTime     (const wchar_t* propertyName) = 0;
-    virtual float            GetSingle       (const wchar_t* propertyName) = 0;
-    virtual double           GetDouble       (const wchar_t* propertyName) = 0;
-    virtual short            GetInt16        (const wchar_t* propertyName) = 0;
-    virtual int              GetInt32        (const wchar_t* propertyName) = 0;
-    virtual long long        GetInt64        (const wchar_t* propertyName) = 0;
-    virtual const wchar_t*   GetString       (const wchar_t* propertyName) = 0;
-    virtual LineBuffer*      GetGeometry     (const wchar_t* propertyName, LineBuffer* lb, CSysTransformer* xformer) = 0;
-    virtual RS_Raster*       GetRaster       (const wchar_t* propertyName) = 0;
-    virtual const wchar_t*   GetAsString     (const wchar_t* propertyName) = 0;
-    virtual RS_InputStream*  GetBLOB         (const wchar_t* propertyName) = 0;
-    virtual RS_InputStream*  GetCLOB         (const wchar_t* propertyName) = 0;
-//  virtual MgFeatureReader* GetFeatureObject(const wchar_t* propertyName) = 0;
-    virtual int              GetPropertyType (const wchar_t* propertyName) = 0;
+    virtual bool            IsNull         (const wchar_t* propertyName) = 0;
+    virtual bool            GetBoolean     (const wchar_t* propertyName) = 0;
+    virtual unsigned char   GetByte        (const wchar_t* propertyName) = 0;
+    virtual FdoDateTime     GetDateTime    (const wchar_t* propertyName) = 0;
+    virtual float           GetSingle      (const wchar_t* propertyName) = 0;
+    virtual double          GetDouble      (const wchar_t* propertyName) = 0;
+    virtual short           GetInt16       (const wchar_t* propertyName) = 0;
+    virtual int             GetInt32       (const wchar_t* propertyName) = 0;
+    virtual long long       GetInt64       (const wchar_t* propertyName) = 0;
+    virtual const wchar_t*  GetString      (const wchar_t* propertyName) = 0;
+    virtual LineBuffer*     GetGeometry    (const wchar_t* propertyName, LineBuffer* lb, CSysTransformer* xformer) = 0;
+    virtual RS_Raster*      GetRaster      (const wchar_t* propertyName) = 0;
+    virtual const wchar_t*  GetAsString    (const wchar_t* propertyName) = 0;
+    virtual RS_InputStream* GetBLOB        (const wchar_t* propertyName) = 0;
+    virtual RS_InputStream* GetCLOB        (const wchar_t* propertyName) = 0;
+    virtual int             GetPropertyType(const wchar_t* propertyName) = 0;
 
     virtual const wchar_t*        GetGeomPropName  ()           = 0;
     virtual const wchar_t*        GetRasterPropName()           = 0;
