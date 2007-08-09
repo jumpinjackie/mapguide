91a5095e7f2473dd41f93b3bba47d6aebe25a7be
waltweltonlair <waltweltonlair@example.org>
2007-08-09 15:53:15 +0000

    Rendering cleanups
    - Removed unused m_max_advance_width and m_max_advance_height members from RS_Font.
    
    - Updated GDRenderer::ProcessOneMarker to call DrawScreenText directly (in the case
      of a font symbol) instead of calling ProcessLabelGroup and relying on the
      LabelRenderer code to render the text.  Now GDRenderer uses the LabelRenderer
      truly just for labels.
    
    
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@1961 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Common/Stylization/RS_Font.h
+++ MgDev/Common/Stylization/RS_Font.h
@@ -34,8 +34,6 @@ struct RS_Font
             m_descender(0),
             m_height(0),
             m_capheight(0),
-            m_max_advance_width(0),
-            m_max_advance_height(0),
             m_underline_position(0),
             m_underline_thickness(0),
             m_fullname(L""),
@@ -56,9 +54,6 @@ struct RS_Font
         short m_descender;
         short m_height;
         short m_capheight;
-
-        short m_max_advance_width;
-        short m_max_advance_height;
 
         short m_underline_position;
         short m_underline_thickness;
