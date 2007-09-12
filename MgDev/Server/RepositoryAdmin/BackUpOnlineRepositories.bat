0ecf91f71155541d6f1cc82ce513afb2cb5e0989
stevedang <stevedang@example.org>
2007-09-12 22:02:04 +0000

    MapGuide RFC 23: Berkeley DB XML 2.3 Upgrade. - Add scripts (for Windows) to upgrade/recover repositories and to set up repository indices. - Update scripts (for Windows) to back up/restore offline/online repositories.
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@2089 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Server/RepositoryAdmin/BackUpOnlineRepositories.bat
+++ MgDev/Server/RepositoryAdmin/BackUpOnlineRepositories.bat
@@ -1,5 +1,18 @@
-if not exist "..\BackupRepositories\Library" md "..\BackupRepositories\Library"
-if not exist "..\BackupRepositories\Site" md "..\BackupRepositories\Site"
+@echo off
 
+echo.
+echo.
+echo *** Site repository ***
+echo.
+if not exist "..\BackupRepositories\Site" md "..\BackupRepositories\Site"
+php -n RepositoryAdmin.php -c Backup -l 2 -i "%cd%\..\Repositories\Site" -o "%cd%\..\BackupRepositories\Site"
+echo.
+
+echo.
+echo *** Library repository ***
+echo.
+if not exist "..\BackupRepositories\Library" md "..\BackupRepositories\Library"
 php -n RepositoryAdmin.php -c Backup -l 2 -i "%cd%\..\Repositories\Library" -o "%cd%\..\BackupRepositories\Library"
-php -n RepositoryAdmin.php -c Backup -l 2 -i "%cd%\..\Repositories\Site"    -o "%cd%\..\BackupRepositories\Site"
+echo.
+
+@echo on
