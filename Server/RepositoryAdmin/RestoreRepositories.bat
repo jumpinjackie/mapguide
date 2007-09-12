9377241b3867534f77fd6bd3c5efac1f1a0e2e0d
stevedang <stevedang@example.org>
2007-09-12 22:02:04 +0000

    MapGuide RFC 23: Berkeley DB XML 2.3 Upgrade. - Add scripts (for Windows) to upgrade/recover repositories and to set up repository indices. - Update scripts (for Windows) to back up/restore offline/online repositories.
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/MgDev@2089 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Server/RepositoryAdmin/RestoreRepositories.bat
+++
@@ -1,2 +0,0 @@
-php -n RepositoryAdmin.php -c Restore -i "%cd%\..\BackupRepositories\Library\CurrentHotBackup" -o "%cd%\..\Repositories\Library"
-php -n RepositoryAdmin.php -c Restore -i "%cd%\..\BackupRepositories\Site\CurrentHotBackup"    -o "%cd%\..\Repositories\Site"
