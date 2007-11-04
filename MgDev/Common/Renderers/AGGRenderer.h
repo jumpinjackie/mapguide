a9c10b106fa4114c3a7c78f43d5192f77b6e32f6
traianstanev <traianstanev@example.org>
2007-11-04 19:43:54 +0000

    Ticket [https://trac.osgeo.org/mapguide/ticket/299] Add agg renderer.
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@2366 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ MgDev/Common/Renderers/AGGRenderer.h
@@ -0,0 +1,317 @@
+//
+//  Copyright (C) 2004-2006  Autodesk, Inc.
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of version 2.1 of the GNU Lesser
+//  General Public License as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+#ifndef AGGRenderer_H
+#define AGGRenderer_H
+
+//warnings about exception definitions in this version of DWF Toolkit
+//remove when we update to new version and see what happens
+//TODO: well, we still need this even with new version
+//but according to Garrick Evans it is safe to ignore
+#ifdef _WIN32
+#pragma warning(disable : 4290)
+#endif
+
+#include <vector>
+
+#include "Renderer.h"
+#include "LineBuffer.h"
+#include "LabelRendererBase.h"
+#include "RS_ByteData.h"
+#include "SE_Renderer.h"
+#include "RS_Fontengine.h"
+
+
+class WT_File;
+class WT_Viewport;
+class WT_Logical_Point;
+
+struct RS_Font;
+
+class agg_context;
+
+class AGGRenderer : public Renderer, public SE_Renderer, public RS_FontEngine
+{
+    friend class LabelRenderer;
+    friend class LabelRendererBase;
+    friend class LabelRendererLocal;
+
+public:
+    RENDERERS_API AGGRenderer(int width,
+                               int height,
+                               RS_Color& bgColor,
+                               bool requiresClipping,
+                               bool localOverposting = false,
+                               double tileExtentOffset = 0.0);
+    RENDERERS_API ~AGGRenderer();
+
+    ///////////////////////////////////
+    // Renderer implementation
+
+    RENDERERS_API virtual void StartMap(RS_MapUIInfo* mapInfo,
+                                          RS_Bounds&    extents,
+                                          double        mapScale,
+                                          double        dpi,
+                                          double        metersPerUnit,
+                                          CSysTransformer* xformToLL = NULL);
+
+    RENDERERS_API virtual void EndMap();
+
+    RENDERERS_API virtual void StartLayer(RS_LayerUIInfo*      legendInfo,
+                                            RS_FeatureClassInfo* classInfo);
+
+    RENDERERS_API virtual void EndLayer();
+
+    RENDERERS_API virtual void StartFeature(RS_FeatureReader* feature,
+                                            bool              initialPass,
+                                            const RS_String*  tooltip = NULL,
+                                            const RS_String*  url = NULL,
+                                            const RS_String*  theme = NULL,
+                                            double            zOffset = 0.0,
+                                            double            zExtrusion = 0.0,
+                                            RS_ElevationType  zOffsetType = RS_ElevationType_RelativeToGround);
+
+
+    RENDERERS_API virtual void ProcessPolygon(LineBuffer* lb, RS_FillStyle& fill);
+
+    RENDERERS_API virtual void ProcessPolyline(LineBuffer* lb, RS_LineStroke& lsym);
+
+    RENDERERS_API virtual void ProcessRaster(unsigned char* data,
+                                               int length,
+                                               RS_ImageFormat format,
+                                               int width, int height,
+                                               RS_Bounds& extents);
+
+    RENDERERS_API virtual void ProcessMarker(LineBuffer* lb, RS_MarkerDef& mdef, bool allowOverpost, RS_Bounds* bounds = NULL);
+
+    RENDERERS_API virtual void ProcessLabelGroup( RS_LabelInfo*    labels,
+                                                    int              nlabels,
+                                                    const RS_String& text,
+                                                    RS_OverpostType  type,
+                                                    bool             exclude,
+                                                    LineBuffer*      path);
+
+    RENDERERS_API virtual void SetSymbolManager(RS_SymbolManager* manager);
+
+    RENDERERS_API virtual RS_MapUIInfo* GetMapInfo();
+
+    RENDERERS_API virtual RS_LayerUIInfo* GetLayerInfo();
+
+    RENDERERS_API virtual RS_FeatureClassInfo* GetFeatureClassInfo();
+
+    RENDERERS_API virtual double GetMapScale();
+
+    RENDERERS_API virtual double GetDrawingScale();
+
+    RENDERERS_API virtual RS_Bounds& GetBounds();
+
+    RENDERERS_API virtual double GetDpi();
+
+    RENDERERS_API virtual double GetMetersPerUnit();
+
+    RENDERERS_API virtual double GetMapToScreenScale();
+
+    RENDERERS_API virtual bool RequiresClipping();
+
+    RENDERERS_API virtual bool RequiresLabelClipping();
+
+    RENDERERS_API virtual bool UseLocalOverposting();
+
+    RENDERERS_API virtual void SetBounds(RS_Bounds& bounds);
+
+    /////////////////////////////////////////////
+    // AGGRenderer specific
+    //
+    RENDERERS_API void Save(const RS_String& filename, const RS_String& format);
+    RENDERERS_API void Save(const RS_String& filename, const RS_String& format, int width, int height);
+    RENDERERS_API RS_ByteData* Save(const RS_String& format, int width, int height);
+
+    RENDERERS_API void Combine(const RS_String& fileIn1, const RS_String& fileIn2, const RS_String& fileOut);
+
+    RENDERERS_API void SetRenderSelectionMode(bool mode);
+
+
+    void DrawString(const RS_String& s,
+                    int              x,
+                    int              y,
+                    double           width,
+                    double           height,
+                    const RS_Font*   font,
+                    RS_Color&  color,
+                    double           angle);
+
+    void MeasureString(const RS_String& s,
+                       double           height,
+                       const RS_Font*   font,
+                       double           angle,
+                       RS_F_Point*      res,
+                       float*           offsets);
+
+    const RS_Font* FindFont(RS_FontDef& def);
+
+
+    virtual void DrawScreenPolyline(LineBuffer* polyline, const SE_Matrix* xform, unsigned int color, double weight); // px
+    virtual void DrawScreenPolygon(LineBuffer* polygon, const SE_Matrix* xform, unsigned int fill);
+    virtual void DrawScreenRaster(unsigned char* data, int length, RS_ImageFormat format, int native_width, int native_height, 
+        double x, double y, double w, double h, double angledeg);
+    virtual void DrawScreenText(const RS_String& txt, RS_TextDef& tdef, double insx, double insy, RS_F_Point* path, int npts, double param_position);
+    virtual void GetWorldToScreenTransform(SE_Matrix& xform);
+    virtual void WorldToScreenPoint(double& inx, double& iny, double& ox, double& oy);
+    virtual void ScreenToWorldPoint(double& inx, double& iny, double& ox, double& oy);
+    virtual double GetPixelsPerMillimeterScreen();
+    virtual double GetPixelsPerMillimeterWorld();
+
+    void ProcessLabelGroup(SE_LabelInfo*    labels,
+                                   int              nlabels,
+                                   RS_OverpostType  type,
+                                   bool             exclude,
+                                   LineBuffer*      path);
+
+    //virtual void ProcessLine(LineBuffer* geometry, SE_RenderLineStyle* style);
+
+    virtual RS_FontEngine* GetRSFontEngine();
+
+    virtual void AddExclusionRegion(RS_F_Point* fpts, int npts);
+
+    virtual bool YPointsUp() { return true; }
+
+
+
+    void AddDWFContent(RS_InputStream *,CSysTransformer *,const RS_String &,const RS_String &,const RS_String &)
+    {}
+
+    static void DrawScreenPolyline(agg_context* cxt, LineBuffer* polyline, const SE_Matrix* xform, unsigned int color, double weight); // px
+    static void DrawScreenPolygon(agg_context* cxt, LineBuffer* polygon, const SE_Matrix* xform, unsigned int fill);
+    static void DrawScreenRaster(agg_context* cxt, unsigned char* data, int length, RS_ImageFormat format, int native_width, int native_height, 
+        double x, double y, double w, double h, double angledeg);
+
+
+private:
+
+    double _MeterToMapSize(RS_Units unit, double number);
+
+    LineBuffer* ApplyLineStyle(LineBuffer* srcLB, wchar_t* lineStyle, double lineWidth, double drawingScale, double dpi);
+
+    void ProcessOneMarker(double x, double y, RS_MarkerDef& mdef, bool allowOverpost, RS_Bounds* bounds = NULL);
+
+    //transformation from mapping to W2D space
+    double _TXD(double x);
+    double _TYD(double y);
+    double _ITXD(double x);
+    double _ITYD(double y);
+
+    static void _TransferPoints(agg_context* c, LineBuffer* src, const SE_Matrix* xform, unsigned int* pathids);
+
+    RS_Color m_bgcolor;
+    RS_Bounds m_extents;
+    double m_metersPerUnit;
+    double m_dpi;
+    double m_mapScale;
+    double m_scale;
+    double m_invScale;
+    double m_offsetX;
+    double m_offsetY;
+    int m_width;
+    int m_height;
+    double m_drawingScale;
+    bool m_bRequiresClipping;
+    bool m_bLocalOverposting;
+
+    RS_SymbolManager* m_symbolManager;
+
+   
+    agg_context* m_context;
+    agg_context* c() { return m_context; }
+    //screen buffer
+    unsigned int* m_rows;
+    
+    agg_context* m_imsym;
+    
+    int m_lineColor;
+
+    bool m_bSelectionMode;
+    RS_FillStyle m_selFill;
+
+    LabelRendererBase* m_labeler;
+    
+private:
+
+    //target image for W2D rewriter -- equal to either the target map
+    //image or to a temporary cached symbol image in case we are
+    //processing a symbol from the library
+    agg_context* m_imw2d;
+
+    // map/layer/feature info
+    RS_MapUIInfo* m_mapInfo;
+    RS_LayerUIInfo* m_layerInfo;
+    RS_FeatureClassInfo* m_fcInfo;
+
+
+    /////////////////////////////////////////////////////////
+    //
+    // Functions and structures used during insertion of W2Ds
+    //
+    /////////////////////////////////////////////////////////
+
+    //TODO: these should be extracted to a friend class that holds
+    // all of the W2D rewriting context
+
+public:
+    /*Do not export from DLL*/ void* GetImage() { return c(); } //target map image
+    /*Do not export from DLL*/ void* GetW2DTargetImage() { return m_imw2d; } //target image for W2D rewriter
+    /*Do not export from DLL*/ bool IsViewportSet() { return m_bHaveViewport; }
+    /*Do not export from DLL*/ bool IsSymbolW2D() { return m_bIsSymbolW2D; }
+    /*Do not export from DLL*/ void UpdateSymbolTrans(WT_File& file, WT_Viewport& viewport);
+    /*Do not export from DLL*/ RS_MarkerDef& GetOverrideColorMarker() { return m_mdOverrideColors; }
+    /*Do not export from DLL*/ RS_String& GetLayerFilter() { return m_layerFilter; }
+    /*Do not export from DLL*/ bool& LayerPassesFilter() { return m_bLayerPassesFilter; }
+
+    /*Do not export from DLL*/ RS_InputStream* _GetInputStream() { return m_input; }
+    /*Do not export from DLL*/ LineBuffer* ProcessW2DPoints(WT_File&          file,
+                                                              WT_Logical_Point* srcpts,
+                                                              int               numpts,
+                                                              bool              checkInBounds);
+    /*Do not export from DLL*/ LineBufferPool* GetPool() { return m_pPool; }
+
+    /*Do not export from DLL*/  double ScaleW2DNumber(WT_File& file, long number);
+
+private:
+    void AddW2DContent(RS_InputStream* in, CSysTransformer* xformer, const RS_String& w2dfilter);
+
+    void SetActions(WT_File& file);
+
+    //////
+    //State variables for when DWFRenderer is used to keep
+    //information during DWF rewriting process for symbols
+    //and DWF layers
+    std::vector<int>    m_cntrs;
+    RS_InputStream*     m_input;
+    CSysTransformer*    m_xformer;
+    LineBufferPool*     m_pPool;
+    bool                m_bIsSymbolW2D;
+    bool                m_bHaveViewport;
+    bool                m_bLayerPassesFilter;
+    RS_String           m_layerFilter;
+
+    //storage for override colors
+    RS_MarkerDef m_mdOverrideColors;
+
+    //remember id of last symbol
+    RS_MarkerDef m_lastSymbol;
+};
+
+#endif
---
+++ MgDev/Common/Renderers/agg_context.h
@@ -0,0 +1,123 @@
+
+#ifndef AGG_CONTEXT_H
+#define AGG_CONTEXT_H
+
+#include <map>
+#include "agg_basics.h"
+#include "agg_rendering_buffer.h"
+#include "agg_scanline_u.h"
+#include "agg_scanline_bin.h"
+#include "agg_renderer_scanline.h"
+#include "agg_renderer_outline_aa.h"
+#include "agg_renderer_outline_image.h"
+#include "agg_renderer_primitives.h"
+#include "agg_rasterizer_scanline_aa.h"
+#include "agg_rasterizer_outline_aa.h"
+#include "agg_rasterizer_outline.h"
+#include "agg_conv_curve.h"
+#include "agg_conv_contour.h"
+#include "agg_pixfmt_rgba.h"
+#include "agg_path_storage.h"
+#include "agg_conv_stroke.h"
+#include "agg_font_freetype.h"
+#include "agg_span_interpolator_linear.h"
+#include "agg_image_accessors.h"
+#include "agg_span_pattern_rgba.h"
+#include "agg_trans_affine.h"
+#include "agg_conv_transform.h"
+#include "agg_span_image_filter_rgba.h"
+#include "agg_span_image_filter_rgb.h"
+#include "agg_span_allocator.h"
+#include "agg_pattern_filters_rgba.h"
+//#include "platform/agg_platform_support.h"
+#include "agg_font_freetype.h"
+
+
+//MG specific AGG template instatiations
+enum flip_y_e { flip_y = true };
+
+//#define pix_format agg::pix_format_bgra32
+typedef agg::row_ptr_cache<agg::int8u> mg_rendering_buffer;
+
+typedef agg::pixfmt_alpha_blend_rgba<agg::blender_bgra32, mg_rendering_buffer, agg::pixel32_type> mg_pixfmt_type; //----pixfmt_bgra32
+typedef agg::pixfmt_alpha_blend_rgba<agg::blender_bgra32_pre, mg_rendering_buffer, agg::pixel32_type> mg_pixfmt_type_pre; //----pixfmt_bgra32
+typedef agg::pixfmt_alpha_blend_rgba<agg::blender_rgba32, mg_rendering_buffer, agg::pixel32_type> mg_pixfmt_type_rgba; //----pixfmt_bgra32
+
+typedef agg::renderer_base<mg_pixfmt_type> mg_ren_base;
+typedef agg::renderer_base<mg_pixfmt_type_pre> mg_ren_base_pre;
+typedef agg::renderer_outline_aa<mg_ren_base> mg_ren_base_o;
+typedef agg::renderer_scanline_aa_solid<mg_ren_base> mg_ren_solid;
+
+typedef agg::font_engine_freetype_int32 font_engine_type;
+typedef agg::font_cache_manager<font_engine_type> font_manager_type;
+
+
+//using this in contructor
+#pragma warning(disable:4355)
+
+//encapsulates our rendering context
+class agg_context
+{
+public:
+    agg_context(unsigned int* rows, int width, int height)
+        : 
+    lprof(1.0, agg::gamma_power(1.0)),
+    ren_o(ren, lprof),
+    ras_o(ren_o),
+    fman(feng)
+    {
+        if (!rows)
+        {
+            ownrows = true;
+            m_rows = new unsigned int[width*height];
+        }
+        else
+        {
+            ownrows = false;
+            m_rows = rows;
+        }
+
+        int stride = width * sizeof(unsigned int);
+        rb.attach((agg::int8u*)m_rows, width, height, (flip_y) ? -stride : stride);
+        pf.attach(rb);
+        pf_pre.attach(rb);
+        ren.attach(pf);
+        ren_pre.attach(pf_pre);
+        ras.gamma(agg::gamma_power(1.0));
+        ras.filling_rule(agg::fill_even_odd);
+        feng.flip_y(!flip_y);
+    }
+
+    ~agg_context()
+    {
+        for (std::map<double, agg::line_profile_aa*>::iterator iter = h_lprof.begin();
+            iter != h_lprof.end(); iter++)
+            delete iter->second;
+
+        if (ownrows)
+            delete [] m_rows;
+    }
+
+    //rendering buffer
+    unsigned int* m_rows;
+    bool ownrows;
+    mg_rendering_buffer rb;
+    mg_pixfmt_type pf;
+    mg_pixfmt_type_pre pf_pre;
+    agg::path_storage ps;
+    mg_ren_base ren;
+    mg_ren_base_pre ren_pre;
+    mg_ren_base_o ren_o;
+    agg::rasterizer_scanline_aa<> ras;
+    agg::rasterizer_outline_aa<mg_ren_base_o> ras_o;
+    agg::scanline_u8 sl;
+    
+    agg::line_profile_aa lprof;
+    std::map<double, agg::line_profile_aa*> h_lprof;
+
+    font_engine_type feng;
+    font_manager_type fman;
+};
+
+
+#endif
