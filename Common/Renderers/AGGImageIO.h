d4bed634a4fc1f601e9ae4451f2eb1d57c4c9b04
waltweltonlair <waltweltonlair@example.org>
2007-11-05 15:24:20 +0000

    Set the svn:eol-style to native.
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/MgDev@2377 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Common/Renderers/AGGImageIO.h
+++ Common/Renderers/AGGImageIO.h
@@ -17,20 +17,16 @@
 
 class RS_ByteData;
 
-
-
 class AGGImageIO
 {
 public:
-
-    static bool Save(const RS_String& filename, const RS_String& format, 
+    static bool Save(const RS_String& filename, const RS_String& format,
                      unsigned int* src, int src_width, int src_height,
                      int dst_width, int dst_height);
 
-    static RS_ByteData* Save(const RS_String& format, 
+    static RS_ByteData* Save(const RS_String& format,
                       unsigned int* src, int src_width, int src_height,
                       int dst_width, int dst_height);
-
 
     static void Combine(const RS_String& src1, const RS_String& src2, const RS_String& dst);
 
@@ -39,8 +35,6 @@ public:
     static unsigned int* DecodeImage(const RS_String& filename, int& width, int& height);
 
 private:
-
     static unsigned char* ReadFile(const RS_String& fname, size_t& len);
     static void UnmultiplyAlpha(unsigned int* argb, int len);
 };
-
