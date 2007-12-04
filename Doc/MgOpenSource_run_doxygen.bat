2c0530b47f6312d119f62174674ad1419316498d
philipsharman <philipsharman@example.org>
2007-12-04 23:14:03 +0000

    Improved the files to build documentation via Doxygen on Linux. http://trac.osgeo.org/mapguide/ticket/355
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/MgDev@2554 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Doc/MgOpenSource_run_doxygen.bat
+++ Doc/MgOpenSource_run_doxygen.bat
@@ -3,16 +3,10 @@
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: 
 :: Copy the doxygen.css file
 
-if exist "C:\Program Files\doxygen\html\doxygen.css" goto CopyCSS
-	echo ERROR: Could not find 'doxygen.css' in 'C:\Program Files\doxygen\html'.
-	echo Either copy it manually to 'MgOpenSource_HTML', or change this batch file to the correct path for your machine.
-	echo (The API Reference has not been built.)
-	exit
-
-:CopyCSS
 echo Copying doxygen.css ...
-if not exist MgOpensource_WebAPIReference			mkdir MgOpensource_WebAPIReference\
-copy "C:\Program Files\doxygen\html\doxygen.css" 	MgOpensource_WebAPIReference
+
+if not exist MgOpensource_WebAPIReference		mkdir MgOpensource_WebAPIReference\
+copy css\doxygen.css 							MgOpensource_WebAPIReference
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: 
 :: Copy the XML schemas
@@ -22,7 +16,8 @@ xcopy /s /y "xml_schemas\*"								MgOpensource_WebAPIReference\xml_schemas\
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: 
 :: Run Doxygen
+
 doxygen MgOpenSource_Doxyfile
-echo Doxygen generation is complete.
+echo Doxygen generation is complete. The error log is in 'MgEnterprise_warning_logfile.txt'.
 
 ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: 
