e3d472b28e19a803d2f84dd287b046a6b258a9b7
jasonnogar <jasonnogar@example.org>
2007-12-06 07:15:56 +0000

    Ticket #291:  This submission fixes join calculations in SE_JoinProcessor_Opt.  When limiting a join to a factor of the join width, the join points should be interpolated between the join vertex position and the join extent only (not the whole segment).
    It also adds an error check to the main transform loop, which aborts if somehow two transform sections have been reversed, instead of possibly looping infinitely (or looping quite a bit and then crashing).
    
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/MgDev@2564 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Common/Stylization/SE_JoinProcessor_Opt.h
+++ Common/Stylization/SE_JoinProcessor_Opt.h
@@ -69,7 +69,7 @@ SE_JoinProcessor<OptData>::SE_JoinProcessor( SE_LineJoin join,
 
     /* Set global data */
     OptData::GLOBAL_INFO ginfo;
-    ginfo.join_dilation = 1.5;
+    ginfo.join_dilation = 2.0;
     ginfo.join_height = m_join->join_height();
     m_joinbuf.SetGlobalInfo(ginfo);
     
@@ -100,6 +100,128 @@ void SE_JoinTransform<OptData>::ProcessSegmentInfo(const OptData& data,
         m_out_tx[i].info = (OptData::TX_INFO)data.join_error;
 }
 
+/* Templating gets around the fact that SE_JoinTransform::TxData is a private struct */
+template<class TX_DATA>
+void ProcessSegmentSide( SE_Deque<std::pair<SE_Tuple, double> >& ptvec,
+                         std::vector<TX_DATA>& txvec,
+                         const SE_Tuple& startpt,
+                         const SE_Tuple& endpt,
+                         const SE_Tuple& lctr,
+                         const SE_Tuple& rctr,
+                         const SE_Tuple& nml,
+                         const SE_Tuple& dv,
+                         int offset,
+                         double startpos,
+                         double endpos,
+                         double lpos,
+                         double rpos,
+                         double len,
+                         double dp )
+{
+#ifdef _DEBUG
+    size_t startsize = ptvec.size();
+#endif
+
+    double ilenl, ilenr, lenl, lenr, dr;
+
+    bool l_del = lpos > 0.0 && offset > 0;
+    bool r_del = rpos > 0.0 && offset > 0;
+
+    SE_Tuple dvl, dvr; 
+
+    if (l_del)
+    {
+        /* Parametric position of the last point of the first join + the distance from the point to
+         * the first join delimiter is the parametric distance from the start point to the first delimiter. */
+        lenl = (ptvec[offset-1].second + (ptvec[offset-1].first - (lctr + nml)).length());
+        dvl = dv * (lpos / dp);
+        /* The parametric distance along the outer contour from the second join delimiter (or
+         * first, if there is no second) to the endpoint */
+        if (rpos > 0.0)
+        {
+            lenr = len - lenl - dp + lpos + rpos;
+            dvr = dv * (rpos / dp);
+#ifdef _DEBUG
+            startsize += 2;
+#endif
+        }
+        else
+        {
+            lenr = len - lenl;
+            rpos = dp - lpos;
+            dvr = dv * (rpos / dp);
+#ifdef _DEBUG
+            ++startsize;
+#endif
+        }
+    }
+    else if (r_del)
+    {
+        lpos = dp - rpos;
+        dvl = dv * (lpos / dp);
+        dvr = dv * (rpos / dp);
+        /* The parametric distance along the outer contour from the end point to the only join delmiter is
+         * the total distance along the contour minus the distance to the last point of the first join minus
+         * the distance from that point to the join delimiter. */
+        lenl = (ptvec[offset-1].second + (ptvec[offset-1].first - (rctr + nml)).length());
+        lenr = len - lenl;
+#ifdef _DEBUG
+        ++startsize;
+#endif
+    }
+    else
+    {
+        dvl = dvr = dv;
+        lenr = lenl = len;
+        lpos = rpos = dp;
+    }
+
+    ilenl = 1.0 / lenl;
+    ilenr = 1.0 / lenr;
+    dr = len - lenr;
+
+    for (int i = 0; i < offset; ++i)
+    {
+        const std::pair<SE_Tuple, double>& point = ptvec.head();
+        _ASSERT(point.second <= lenl);
+        txvec.push_back(TX_DATA(point.first,
+            startpt + (dvl * (point.second * ilenl)),
+            startpos + (lpos * point.second * ilenl)));
+        ptvec.pop_head();
+    }
+
+    if (l_del)
+        txvec.push_back(TX_DATA(lctr + nml, lctr, startpos + lpos));
+    if (r_del)
+        txvec.push_back(TX_DATA(rctr + nml, rctr, endpos - rpos));
+
+
+    while (ptvec.size())
+    {
+        const std::pair<SE_Tuple, double>& point = ptvec.head();
+        _ASSERT(point.second >= dr && point.second - dr < lenr);
+        txvec.push_back(TX_DATA(point.first,
+            endpt - (dvr * ((lenr - (point.second - dr)) * ilenr)),
+            endpos - rpos + (rpos * (point.second - dr) * ilenr)));
+        ptvec.pop_head();
+    }
+
+#ifdef _DEBUG
+
+    if (txvec.size() == startsize)
+        --startsize;
+    SE_Tuple t, ix;
+    for (size_t i = 1; i <= startsize; ++i)
+    {
+        /* If the outer edge of the transform is crossing the centerline, something has gone awry. */
+        _ASSERT(!Intersects(txvec[txvec.size()-i].ctr, txvec[txvec.size()-i-1].ctr,
+                            txvec[txvec.size()-i].out, txvec[txvec.size()-i-1].out,
+                            t, ix) && 
+                txvec[txvec.size()-i].pos >= txvec[txvec.size()-i-1].pos);
+    }
+#endif
+}
+
 
 template<>
 void SE_JoinTransform<OptData>::ProcessSegment(double in_len,
@@ -109,7 +231,6 @@ void SE_JoinTransform<OptData>::ProcessSegment(double in_len,
 {
     SE_Tuple dv = end_vert - m_prev_vtx;
     double dp = end_pos - m_prev_pos;
-    double ilen = 1.0 / (in_len + m_in_pts.tail().second);
 
     double lpos = m_prev_data.join_width * m_global_info.join_dilation;
     double rpos = m_cur_data.join_width * m_global_info.join_dilation;
@@ -123,7 +244,7 @@ void SE_JoinTransform<OptData>::ProcessSegment(double in_len,
         out_off = m_cur_out_cnt - (int)m_out_tx.size();
         double idp = 1.0 / dp;
 
-        innml = SE_Tuple(-dv.y * idp, dv.x * idp);
+        innml = SE_Tuple(-dv.y * idp, dv.x * idp) * m_global_info.join_height;
         if (lpos > 0.0)
         {
             lctr = m_prev_vtx + (dv * (lpos * idp));
@@ -132,70 +253,21 @@ void SE_JoinTransform<OptData>::ProcessSegment(double in_len,
         }
         if (rpos > 0.0)
             rctr = end_vert - (dv * (rpos * idp));
-
     }
 
-    _ASSERT(in_off <= (int)m_in_pts.size());
+    _ASSERT(in_off != 0 && in_off <= (int)m_in_pts.size());
+    if (in_off == 0 || in_off > (int)m_in_pts.size())
+        return;
 
-    while (m_in_pts.size() > 0 || in_off >= 0)
-    {
-#ifdef _DEBUG
-        /* If the outer edge of the transform is crossing the centerline, something has gone awry. */
-        SE_Tuple t, ix;
-        _ASSERT(m_in_tx.size() < 2 || !Intersects(m_in_tx[m_in_tx.size()-1].ctr, m_in_tx[m_in_tx.size()-2].ctr,
-                                                  m_in_tx[m_in_tx.size()-1].out, m_in_tx[m_in_tx.size()-2].out,
-                                                  t, ix));
-#endif
-        if (in_off-- == 0)
-        {
-            if (lpos > 0.0)
-                m_in_tx.push_back(TxData(lctr + (innml * m_global_info.join_height), lctr, m_prev_pos + lpos));
-            if (rpos > 0.0)
-                m_in_tx.push_back(TxData(rctr + (innml * m_global_info.join_height), rctr, end_pos - rpos));
-            continue;
-        }
+    ProcessSegmentSide<TxData>(m_in_pts, m_in_tx, m_prev_vtx, end_vert, lctr, rctr, innml, dv,
+        in_off, m_prev_pos, end_pos, lpos, rpos, in_len + m_in_pts.tail().second, dp);
 
-        const std::pair<SE_Tuple, double>& point = m_in_pts.head();
+    _ASSERT(out_off != 0 && out_off <= (int)m_out_pts.size());
+    if (out_off == 0 || out_off > (int)m_out_pts.size())
+        return;
 
-        _ASSERT(dp * point.second * ilen >= 0.0);
-        m_in_tx.push_back(TxData(point.first,
-            m_prev_vtx + (dv * (point.second * ilen)),
-            m_prev_pos + (dp * point.second * ilen)));
-
-        m_in_pts.pop_head();
-    }
-
-    ilen = 1.0 / (out_len + m_out_pts.tail().second);
-
-    _ASSERT(out_off <= (int)m_out_pts.size());
-
-    while (m_out_pts.size() > 0 || out_off >= 0)
-    {
-#ifdef _DEBUG
-        SE_Tuple t, ix;
-        _ASSERT(m_out_tx.size() < 2 || !Intersects(m_out_tx[m_out_tx.size()-1].ctr, m_out_tx[m_out_tx.size()-2].ctr,
-                                                   m_out_tx[m_out_tx.size()-1].out, m_out_tx[m_out_tx.size()-2].out,
-                                                   t, ix));
-#endif
-        if (out_off-- == 0)
-        {
-            if (lpos > 0.0)
-                m_out_tx.push_back(TxData(lctr - (innml * m_global_info.join_height), lctr, m_prev_pos + lpos));
-            if (rpos > 0.0)
-                m_out_tx.push_back(TxData(rctr - (innml * m_global_info.join_height), rctr, end_pos - rpos));
-            continue;
-        }
-
-        const std::pair<SE_Tuple, double>& point = m_out_pts.head();
-
-        _ASSERT(dp * point.second * ilen >= 0.0);
-
-        m_out_tx.push_back(TxData(point.first,
-            m_prev_vtx + (dv * (point.second * ilen)),
-            m_prev_pos + (dp * point.second * ilen)));
-
-        m_out_pts.pop_head();
-    }
+    ProcessSegmentSide<TxData>(m_out_pts, m_out_tx, m_prev_vtx, end_vert, lctr, rctr, -innml, dv,
+        out_off, m_prev_pos, end_pos, lpos, rpos, out_len + m_out_pts.tail().second, dp);
 }
 
 
