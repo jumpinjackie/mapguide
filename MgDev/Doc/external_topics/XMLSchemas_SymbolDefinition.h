c7fffe036bde56a65f407fc4801c2256f55fd878
philipsharman <philipsharman@example.org>
2008-02-23 23:54:26 +0000

    Corrected the documentation for the Symbol Definition XML schema. http://trac.osgeo.org/mapguide/ticket/355
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@2989 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Doc/external_topics/XMLSchemas_SymbolDefinition.h
+++ MgDev/Doc/external_topics/XMLSchemas_SymbolDefinition.h
@@ -5,12 +5,8 @@
 There are now two versions of the SymbolDefinition schema.
 </P>
 <P>
-All symbol definitions from MapGuide versions prior to MapGuide Enterprise 2009 use version 1.0.0.  Symbol definitions created or updated in
-Autodesk Studio 2009 use version 1.1.0. 
-</P>
-<P>
-You don't need to use the new version unless you want to make use of the enhanced vector symbolization supported by the new version:
-
+All symbol definitions from MapGuide Open Source prior to MapGuide Open Source 2.0 use schema version 1.0.0.  Symbol definitions created or updated in
+MapGuide Open Source 2.0 use schema version 1.1.0. 
 </P>
 
 <a href="../../xml_schemas/SymbolDefinition/SymbolDefinition.html">SymbolDefinition-1.0.0.xsd - Details</a><BR>
