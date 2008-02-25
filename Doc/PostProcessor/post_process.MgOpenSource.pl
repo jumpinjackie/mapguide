c2585b227e8f97741f9cf76c4daa7238053b2306
philipsharman <philipsharman@example.org>
2008-02-25 01:58:01 +0000

    Improved the Perl post processing step for the Web API Reference documentation. http://trac.osgeo.org/mapguide/ticket/355
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.0.x/MgDev@2994 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Doc/PostProcessor/post_process.MgOpenSource.pl
+++ Doc/PostProcessor/post_process.MgOpenSource.pl
@@ -7,22 +7,7 @@
 #		Does some post-processing of the HTML files produced
 #		by Doxygen, to fix things that we cannot configure
 #	    through Doxygen.
-#		
-#		1) Removes the "File List" and "File Members" nodes from the Table Of Contents.
-#		2) Removes '__get', '__set', and '__inherit'. (These are SWIG commands
-#		   but because they are in the comments they get picked up by Doxygen.)
-#		3) Sets the title to be shown in the web browser.
-#		4) Fixes the links in the top-of-page tabs on the first page (index.htm)
-#		   the user sees.
-#		   (If you have DISABLE_INDEX set to NO, the default, in the Doxygen
-# 		    configuration file, then Doxygen puts tabs at the top of each page,
-#			with links to "Main Page, "Modules", etc.  We don't want to show
-#  		    the "Files" tab, so we set DISABLE_INDEX to YES, and add the desired tabs
-#		    to our custom header (which we specify via HTML_HEADER).
-#			We also have CREATE_SUBDIRS set to YES, so some pages are in
-#			subdirectories, and some are at the root level.  The links in
-#			our custom header are fine for pages in subdirectories, but need
-#			to be fixed for pages in the root level.  (EG, index.htm.)
+#		See PostProcess.pm for details.
 #	
 #	Usage:	
 #		1) Alter the file paths etc. below (in the Parameters section) if necessary.
@@ -37,6 +22,8 @@
 #		v 1.2	- Changed to not use any external Perl modules, to make it easier to
 #				  distribute.  Also moved the main calling routine back to here and out
 #				  of PostProcessor.pm.  PHS, 2008.02.20
+#		v 1.3	- Added ROOT, call to 'change_root', and call to 'change_namespace()'.
+#                 PHS, 2008.02.24
 #############################################################
 package PostProcessor;
 use lib 'PostProcessor'; # Look in the PostProcessor directory for PostProcessor.pm (assuming we are called from the HTML parent directory).
@@ -58,7 +45,7 @@ use IO::Handle;
 ###		Global Variables
 ################################################################### 
 # These must be defined this way so that  'PostProcessor.pm' can use them.
-use vars qw($DIRECTORY_SEPARATOR $INPUT_DIRECTORY $TITLE $TRUE $FALSE $VERBOSE);
+use vars qw($DIRECTORY_SEPARATOR $INPUT_DIRECTORY $TITLE $ROOT $TRUE $FALSE $VERBOSE);
 $TRUE =  (1==1);
 $FALSE = (1==0);
 # (Sadly, there seems to be no way to use Readonly with variables we want to share with a module.)
@@ -96,6 +83,9 @@ $INPUT_DIRECTORY = '..' . $DIRECTORY_SEPARATOR . 'MgOpensource_WebAPIReference';
 # The title to be shown in the web browser
 $TITLE = 'MapGuide Web API Reference';
 
+# The name for the top node in the TOC
+$ROOT = 'MapGuide Web API Reference';
+
 # Set this to $TRUE if you want more verbose information about what the script is doing
 #$VERBOSE = $TRUE;
 $VERBOSE = $FALSE;
@@ -113,6 +103,8 @@ delete_file_list();
 remove_SWIG_commands_from_directory($INPUT_DIRECTORY);
 change_title();
 fix_links_in_directory($INPUT_DIRECTORY);
+change_root();
+remove_namespace();
 
 # All done
 print "\nPost processing is complete. (Time taken = ", convert_seconds_to_hms_string((time() - $start_time)) , ".)\n"; 
