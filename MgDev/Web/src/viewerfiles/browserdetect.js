0c5d5248df2dfde52eb9def993632c202ea4d0db
trevorwekel <trevorwekel@example.org>
2008-09-26 22:55:41 +0000

    Ticket [https://trac.osgeo.org/mapguide/ticket/715] Ajax viewer pan/zoom executes two spatial queries on IE7 - Backported changeset 3252 into 2.0.x. - See http://trac.osgeo.org/mapguide/changeset/3252 for additional details
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.0.x@3354 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Web/src/viewerfiles/browserdetect.js
+++ MgDev/Web/src/viewerfiles/browserdetect.js
@@ -1,6 +1,6 @@
 var agent = navigator.userAgent.toLowerCase();
-var msie = agent.indexOf("msie") != -1;
 var firefox = agent.indexOf("firefox") != -1;
+var opera = agent.indexOf("opera") != -1;
 
 var safariIndex = agent.indexOf("safari");
 var safari1or2 = false;
@@ -17,5 +17,23 @@ if(safariIndex != -1)
     else
     {
         safari1or2 = true;
+    }
+}
+
+var msieIndex = agent.indexOf("msie");
+var msie = false;
+var msie7plus = false;
+var msie6minus = false;
+if(msieIndex != -1)
+{
+    msie = true;
+    var msieVersion = agent.substr(msieIndex + 5, 1);
+    if(parseFloat(msieVersion) >= 7)
+    {
+        msie7plus = true;
+    }
+    else
+    {
+        msie6minus = true;
     }
 }
