19a701f5bcc6e8e764dcbb5e159ad580e6df59a4
leodai <leodai@example.org>
2009-01-16 06:56:27 +0000

    Fix [https://trac.osgeo.org/mapguide/ticket/822] ( Custom Logo from Print Layout no longer displayed in Basic Layout)
    This problem was caused by the RS_MarkerType of the custom logo.
    Before fix, the initialized value "RS_MarkerType_Marker" was used and it means it is a predefined SLD symbol(circle, cross, etc.). And DWFRenderer will not to read the symbol stream and render it.
    
    My fix is to correct the type of the custom logo as "RS_MarkerType_W2D", which will enable the DWFRenderer to read the symbol stream.
    
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@3552 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Server/src/Services/Mapping/LegendPlotUtil.cpp
+++ MgDev/Server/src/Services/Mapping/LegendPlotUtil.cpp
@@ -1198,6 +1198,7 @@ void MgLegendPlotUtil::AddCustomLogoElement(MgPrintLayout* layout, EPlotRenderer
         }
 
         RS_MarkerDef markerDef;
+        markerDef.type() = RS_MarkerType_W2D; 
         markerDef.units() = RS_Units_Device;
         markerDef.width() = logoWidth;
         markerDef.height() = logoHeight;
