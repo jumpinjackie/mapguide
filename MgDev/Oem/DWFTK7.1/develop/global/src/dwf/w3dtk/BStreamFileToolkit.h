91d99094dbe3cbffc6be2e1e35ede60020958a1a
philipsharman <philipsharman@example.org>
2009-01-28 03:36:42 +0000

    Updated copyright year to 2009.
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@3568 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Oem/DWFTK7.1/develop/global/src/dwf/w3dtk/BStreamFileToolkit.h
+++ MgDev/Oem/DWFTK7.1/develop/global/src/dwf/w3dtk/BStreamFileToolkit.h
@@ -11,7 +11,7 @@
 //
 //
 //
-//  Copyright (C) 1996-2008 by Autodesk, Inc.
+//  Copyright (C) 1996-2009 by Autodesk, Inc.
 //
 //  By using this code, you are agreeing to the terms and conditions of
 //  the License Agreement included in the documentation for this code.
