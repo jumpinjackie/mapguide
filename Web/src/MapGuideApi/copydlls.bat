2ecec6a27ae6a5c0b445f653ad330f1d417cc738
waltweltonlair <waltweltonlair@example.org>
2009-02-05 16:31:42 +0000

    Additional fix for #835 (Change OEM GEOS library from static library to dynamic library)
    The web tier postbuild step needs to copy the GEOS dll to the run directory.
    
    
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/MgDev@3595 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Web/src/MapGuideApi/copydlls.bat
+++ Web/src/MapGuideApi/copydlls.bat
@@ -37,8 +37,14 @@ if not exist %WEB_BIN_RELEASE% mkdir %WEB_BIN_RELEASE%
 echo Copying ACE Release DLL to %WEB_BIN_RELEASE%
 xcopy /r /d /y "..\..\..\Oem\ACE\ACE_wrappers\lib\ACE.dll" %WEB_BIN_RELEASE%
 
+echo Copying GEOS Release DLL to %WEB_BIN_RELEASE%
+xcopy /r /d /y "..\..\..\Oem\geos-2.2.0\VisualStudio\Release\GEOS.dll" %WEB_BIN_RELEASE%
+
 echo Copying JSON Release DLL to %WEB_BIN_RELEASE%
 xcopy /r /d /y "..\..\..\Oem\jsoncpp\lib\lib_json.dll" %WEB_BIN_RELEASE%
+
+echo Copying Xerces Release DLL to %WEB_BIN_RELEASE%
+xcopy /r /d /y "..\..\..\Oem\dbxml-2.3.10\bin\xerces-c_2_7.dll" %WEB_BIN_RELEASE%
 
 echo Copying MgFoundation Release DLL to %WEB_BIN_RELEASE%
 xcopy /r /d /y %COMMON_BIN_RELEASE%\MgFoundation.dll %WEB_BIN_RELEASE%
@@ -58,9 +64,6 @@ xcopy /r /d /y %COMMON_BIN_RELEASE%\MgMdfModel.dll %WEB_BIN_RELEASE%
 echo Copying MgMdfParser Release DLL to %WEB_BIN_RELEASE%
 xcopy /r /d /y %COMMON_BIN_RELEASE%\MgMdfParser.dll %WEB_BIN_RELEASE%
 
-echo Copying Xerces Release DLL to %WEB_BIN_RELEASE%
-xcopy /r /d /y "..\..\..\Oem\dbxml-2.3.10\bin\xerces-c_2_7.dll" %WEB_BIN_RELEASE%
-
 goto end_copy
 
 
@@ -74,8 +77,14 @@ if not exist %WEB_BIN_RELEASE%64 mkdir %WEB_BIN_RELEASE%64
 echo Copying ACE Release DLL to %WEB_BIN_RELEASE%64
 xcopy /r /d /y "..\..\..\Oem\ACE\ACE_wrappers\lib64\ACE.dll" %WEB_BIN_RELEASE%64
 
+echo Copying GEOS Release DLL to %WEB_BIN_RELEASE%64
+xcopy /r /d /y "..\..\..\Oem\geos-2.2.0\VisualStudio\Release64\GEOS.dll" %WEB_BIN_RELEASE%64
+
 echo Copying JSON Release DLL to %WEB_BIN_RELEASE%64
 xcopy /r /d /y "..\..\..\Oem\jsoncpp\lib64\lib_json.dll" %WEB_BIN_RELEASE%64
+
+echo Copying Xerces Release DLL to %WEB_BIN_RELEASE%64
+xcopy /r /d /y "..\..\..\Oem\dbxml-2.3.10\bin64\xerces-c_2_7.dll" %WEB_BIN_RELEASE%64
 
 echo Copying MgFoundation Release DLL to %WEB_BIN_RELEASE%64
 xcopy /r /d /y %COMMON_BIN_RELEASE%64\MgFoundation.dll %WEB_BIN_RELEASE%64
@@ -95,9 +104,6 @@ xcopy /r /d /y %COMMON_BIN_RELEASE%64\MgMdfModel.dll %WEB_BIN_RELEASE%64
 echo Copying MgMdfParser Release DLL to %WEB_BIN_RELEASE%64
 xcopy /r /d /y %COMMON_BIN_RELEASE%64\MgMdfParser.dll %WEB_BIN_RELEASE%64
 
-echo Copying Xerces Release DLL to %WEB_BIN_RELEASE%64
-xcopy /r /d /y "..\..\..\Oem\dbxml-2.3.10\bin64\xerces-c_2_7.dll" %WEB_BIN_RELEASE%64
-
 goto end_copy
 
 
@@ -111,8 +117,14 @@ if not exist %WEB_BIN_DEBUG% mkdir %WEB_BIN_DEBUG%
 echo Copying ACE Debug DLL to %WEB_BIN_DEBUG%
 xcopy /r /d /y "..\..\..\Oem\ACE\ACE_wrappers\lib\ACEd.dll" %WEB_BIN_DEBUG%
 
+echo Copying GEOS Debug DLL to %WEB_BIN_DEBUG%
+xcopy /r /d /y "..\..\..\Oem\geos-2.2.0\VisualStudio\Debug\GEOS.dll" %WEB_BIN_DEBUG%
+
 echo Copying JSON Debug DLL to %WEB_BIN_DEBUG%
 xcopy /r /d /y "..\..\..\Oem\jsoncpp\lib\lib_jsond.dll" %WEB_BIN_DEBUG%
+
+echo Copying Xerces Debug DLL to %WEB_BIN_DEBUG%
+xcopy /r /d /y "..\..\..\Oem\dbxml-2.3.10\bin\debug\xerces-c_2_7D.dll" %WEB_BIN_DEBUG%
 
 echo Copying MgFoundation Debug DLL to %WEB_BIN_DEBUG%
 xcopy /r /d /y %COMMON_BIN_DEBUG%\MgFoundationd.dll %WEB_BIN_DEBUG%
@@ -132,9 +144,6 @@ xcopy /r /d /y %COMMON_BIN_DEBUG%\MgMdfModeld.dll %WEB_BIN_DEBUG%
 echo Copying MgMdfParser Debug DLL to %WEB_BIN_DEBUG%
 xcopy /r /d /y %COMMON_BIN_DEBUG%\MgMdfParserd.dll %WEB_BIN_DEBUG%
 
-echo Copying Xerces Debug DLL to %WEB_BIN_DEBUG%
-xcopy /r /d /y "..\..\..\Oem\dbxml-2.3.10\bin\debug\xerces-c_2_7D.dll" %WEB_BIN_DEBUG%
-
 goto end_copy
 
 
@@ -148,8 +157,14 @@ if not exist %WEB_BIN_DEBUG%64 mkdir %WEB_BIN_DEBUG%64
 echo Copying ACE Debug DLL to %WEB_BIN_DEBUG%64
 xcopy /r /d /y "..\..\..\Oem\ACE\ACE_wrappers\lib64\ACEd.dll" %WEB_BIN_DEBUG%64
 
+echo Copying GEOS Debug DLL to %WEB_BIN_DEBUG%64
+xcopy /r /d /y "..\..\..\Oem\geos-2.2.0\VisualStudio\Debug64\GEOS.dll" %WEB_BIN_DEBUG%64
+
 echo Copying JSON Debug DLL to %WEB_BIN_DEBUG%64
 xcopy /r /d /y "..\..\..\Oem\jsoncpp\lib64\lib_jsond.dll" %WEB_BIN_DEBUG%64
+
+echo Copying Xerces Debug DLL to %WEB_BIN_DEBUG%64
+xcopy /r /d /y "..\..\..\Oem\dbxml-2.3.10\bin\debug64\xerces-c_2_7D.dll" %WEB_BIN_DEBUG%64
 
 echo Copying MgFoundation Debug DLL to %WEB_BIN_DEBUG%64
 xcopy /r /d /y %COMMON_BIN_DEBUG%64\MgFoundationd.dll %WEB_BIN_DEBUG%64
@@ -169,9 +184,6 @@ xcopy /r /d /y %COMMON_BIN_DEBUG%64\MgMdfModeld.dll %WEB_BIN_DEBUG%64
 echo Copying MgMdfParser Debug DLL to %WEB_BIN_DEBUG%64
 xcopy /r /d /y %COMMON_BIN_DEBUG%64\MgMdfParserd.dll %WEB_BIN_DEBUG%64
 
-echo Copying Xerces Debug DLL to %WEB_BIN_DEBUG%64
-xcopy /r /d /y "..\..\..\Oem\dbxml-2.3.10\bin\debug64\xerces-c_2_7D.dll" %WEB_BIN_DEBUG%64
-
 goto end_copy
 
 
@@ -185,6 +197,7 @@ set /P COPY_DIR="Where do you want to copy the files to? "
 echo Copying all dependent DLLs to %COPY_DIR%
 xcopy /r /d /y Mg*.dll %COPY_DIR%
 xcopy /r /d /y ACE*.dll %COPY_DIR%
+xcopy /r /d /y GEOS*.dll %COPY_DIR%
 xcopy /r /d /y lib_json*.dll %COPY_DIR%
 xcopy /r /d /y isapi*.dll %COPY_DIR%
 
