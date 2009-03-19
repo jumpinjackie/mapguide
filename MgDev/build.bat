166b3f7e5974852a426e7d3d3f333212f6393e85
Jackie Ng <jumpinjackie@gmail.com>
2009-03-19 10:48:47 +0000

    Installer: Fix [https://trac.osgeo.org/mapguide/ticket/914] - Java is not correctly configured. The problem was due to the following:
    - The MgDev build script copied the WEB-INF directory to the wrong directory
    - The MgDev build script did not copy the MapGuide unmanaged API binaries to the Tomcat bin directory
    
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@3745 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/build.bat
+++ MgDev/build.bat
@@ -327,6 +327,8 @@ echo [install]: Web Tier - webconfig.ini
 copy /Y "%MG_WEB_SRC%\webconfig.ini" "%MG_OUTPUT_WEB%\www"
 echo [install]: Web Tier - mapviewerphp
 %XCOPY% "%MG_WEB_SRC%\mapviewerphp" "%MG_OUTPUT_WEB%\www\mapviewerphp" /EXCLUDE:svn_excludes.txt+%TYPEBUILD%_excludes.txt
+echo [install]: Web Tier - Tomcat - MapGuide API unmanaged binaries
+%XCOPY% "%MG_WEB_BIN%\%TYPEBUILD%" "%MG_OUTPUT_WEB%\Tomcat\bin"/EXCLUDE:java_excludes.txt+svn_excludes.txt+%TYPEBUILD%_excludes.txt
 echo [install]: Web Tier - php - MapGuide API unmanaged binaries
 %XCOPY% "%MG_WEB_BIN%\%TYPEBUILD%" "%MG_OUTPUT_WEB%\Php"/EXCLUDE:php_excludes.txt+svn_excludes.txt+%TYPEBUILD%_excludes.txt
 echo [install]: Web Tier - php ext - MapGuide API unmanaged binaries
@@ -338,7 +340,7 @@ echo [install]: Web Tier - mapviewernet - MapGuide API unmanaged binaries
 echo [install]: Web Tier - mapviewerjava
 %XCOPY% "%MG_WEB_SRC%\mapviewerjava" "%MG_OUTPUT_WEB%\www\mapviewerjava" /EXCLUDE:svn_excludes.txt
 echo [install]: Web Tier - mapviewerjava - WEB-INF
-%XCOPY% "%MG_WEB_SRC%\WEB-INF" "%MG_OUTPUT_WEB%\www\mapviewerjava\WEB-INF" /EXCLUDE:svn_excludes.txt
+%XCOPY% "%MG_WEB_SRC%\WEB-INF" "%MG_OUTPUT_WEB%\www" /EXCLUDE:svn_excludes.txt
 echo [install]: Web Tier - fusion templates
 %XCOPY% "%MG_OEM%\fusionMG" "%MG_OUTPUT_WEB%\www\fusion" /EXCLUDE:svn_excludes.txt
 echo [install]: Web Tier - Apache module
