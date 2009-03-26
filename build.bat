250b479e34b3642d113b1be6116fede2a6dc6a17
jbirch <jbirch@example.org>
2009-03-26 09:52:09 +0000

    In conjunction with r3761, fixes #913 and #928
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/MgDev@3762 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- build.bat
+++ build.bat
@@ -288,6 +288,8 @@ goto custom_error
 echo [install]: Server
 echo [install]: Server - Binaries
 %XCOPY% "%MG_SERVER%\bin\%TYPEBUILD%" "%MG_OUTPUT_SERVER%\bin" /EXCLUDE:svn_excludes.txt+%TYPEBUILD%_excludes.txt
+echo [install]: Server - DBXML
+copy /Y "%MG_OEM%\dbxml-2.3.10\bin\*.exe" "%MG_OUTPUT_SERVER%\bin"
 echo [install]: Server - RepositoryAdmin
 %XCOPY% "%MG_SERVER%\RepositoryAdmin" "%MG_OUTPUT_SERVER%\RepositoryAdmin" /EXCLUDE:svn_excludes.txt+%TYPEBUILD%_excludes.txt
 echo [install]: CsMap Dictionaries
@@ -295,6 +297,8 @@ SET CSMAP_DICT=%MG_OUTPUT_SERVER%\CsMap\Dictionaries
 if not exist "%CSMAP_DICT%" mkdir "%CSMAP_DICT%"
 copy /Y "%MG_OEM%\CsMap\Dictionaries\*.MRT" "%CSMAP_DICT%"
 copy /Y "%MG_OEM%\CsMap\Dictionaries\*.CSD" "%CSMAP_DICT%"
+copy /Y "%MG_OEM%\CsMap\Dictionaries\*.CSV" "%CSMAP_DICT%"
+copy /Y "%MG_OEM%\CsMap\Dictionaries\license.txt" "%CSMAP_DICT%"
 if not "%TYPECOMPONENT%"=="all" goto quit
 
 :install_doc
