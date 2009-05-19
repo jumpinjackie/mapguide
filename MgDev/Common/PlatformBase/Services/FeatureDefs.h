3f7be3be058e1f0d1edb909a9c12b14afcdade55
brucedechant <brucedechant@example.org>
2009-05-19 20:33:40 +0000

    Revert change that was mistakenly submitted with submission to trac ticket 88
    Notes:
    - Restored the list of class IDs that was changed
    
    
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@3872 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Common/PlatformBase/Services/FeatureDefs.h
+++ MgDev/Common/PlatformBase/Services/FeatureDefs.h
@@ -58,14 +58,15 @@ INTERNAL_API:
     static const int GetIdentityProperties_Id       = 0x1111ED1C;
     static const int DescribeWfsFeatureType_Id      = 0x1111ED1D;
     static const int GetWfsFeature_Id               = 0x1111ED1E;
-    static const int EnumerateDataStores_Id         = 0x1111ED1F;
+    static const int CloseGwsFeatureReader_Id       = 0x1111ED1F;
 
-    static const int GetSchemaMapping_Id            = 0x1111ED20;
-    static const int SelectFeaturesWithCS_Id        = 0x1111ED21;
-    static const int GetLockedFeatures_Id           = 0x1111ED22;
-    static const int SetLongTransaction_Id          = 0x1111ED23;
-    static const int GetFdoCacheInfo_Id             = 0x1111ED24;
-    static const int GetClassDefinition2_Id         = 0x1111ED25;
+    static const int EnumerateDataStores_Id         = 0x1111ED20;
+    static const int GetSchemaMapping_Id            = 0x1111ED21;
+    static const int SelectFeaturesWithCS_Id        = 0x1111ED22;
+    static const int GetLockedFeatures_Id           = 0x1111ED23;
+    static const int SetLongTransaction_Id          = 0x1111ED24;
+    static const int GetFdoCacheInfo_Id             = 0x1111ED25;
+    static const int GetClassDefinition2_Id         = 0x1111ED26;
 };
 /// \endcond
 
