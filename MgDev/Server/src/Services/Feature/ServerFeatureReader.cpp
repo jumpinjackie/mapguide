801ce58bca1a02f7a17ad5df3668c6a9d947fd03
brucedechant <brucedechant@example.org>
2009-07-13 21:34:45 +0000

    Fix for trac ticket 1031 - Readers will serialize exceptions twice http://trac.osgeo.org/mapguide/ticket/1031
    Notes:
    - Removed exception being written twice for readers Serialize() method.
    
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.1@4028 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Server/src/Services/Feature/ServerFeatureReader.cpp
+++ MgDev/Server/src/Services/Feature/ServerFeatureReader.cpp
@@ -829,7 +829,8 @@ void MgServerFeatureReader::Serialize(MgStream* stream)
         stream->WriteObject((MgException*)mgException);
     }
 
-    MG_FEATURE_SERVICE_THROW();
+    // Serialize method must not THROW the exception because it has already been written to the stream
+    // and in doing so would only end up writing the same exception to the stream twice.
 }
 
 //////////////////////////////////////////////////////////////////
