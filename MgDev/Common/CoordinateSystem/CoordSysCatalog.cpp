8f794ad5d2d31e493eb6f9868f1d3853c042c14e
uvlite <uvlite@example.org>
2009-09-21 15:20:46 +0000

    add another error message if configuration error
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.1@4240 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Common/CoordinateSystem/CoordSysCatalog.cpp
+++ MgDev/Common/CoordinateSystem/CoordSysCatalog.cpp
@@ -185,6 +185,7 @@ STRING CCoordinateSystemCatalog::GetDefaultDictionaryDir()
 
     bool bResult=false;
 #ifdef _WIN32
+    //prepares the default path for dictionary failes
 
     STRING sDirDefault;
 
@@ -203,7 +204,10 @@ STRING CCoordinateSystemCatalog::GetDefaultDictionaryDir()
             sDirDefault = szPath;
             sDirDefault += _T("\\Autodesk\\Geospatial Coordinate Systems"); //NOXLATE
             szPathVar = sDirDefault.c_str();
-        }
+        } 
+        //else {
+        //    ACE_DEBUG((LM_DEBUG, ACE_TEXT("(%t) CCoordinateSystemCatalog::GetDefaultDictionaryDir() - Could not read SpecialFolderPath from registry\n")));
+        //}
     }
 
     if(szPathVar)
@@ -242,7 +246,7 @@ STRING CCoordinateSystemCatalog::GetDefaultDictionaryDir()
 
     if (!bResult)
     {
-        throw new MgCoordinateSystemInitializationFailedException(L"MgCoordinateSystemCatalog.GetDefaultDictionaryDir", __LINE__, __WFILE__, NULL, L"", NULL);
+        throw new MgCoordinateSystemInitializationFailedException(L"MgCoordinateSystemCatalog.GetDefaultDictionaryDir ", __LINE__, __WFILE__, NULL, L"", NULL);
     }
 
     //And return success.
