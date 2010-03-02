076ddd03068aa7c111a483453df74d2db2a4f4a3
NormOlsen <NormOlsen@example.org>
2010-03-02 23:27:26 +0000

    MapGuide Trac Ticket 1253
    This submission replaces the previous line string to arbitrary polygon clipper in its entirety.  The clipper is now a four phase process and where each phase is implemented as an internally referenced methods.  The new clipping algorithms correct problems related to coordinates with hard core 0.0 values, line string points which coincide with the clipping boundary, and the effect of line string segments which are collinear with (and overlap) clipping boundary segments.  To accomplish this, the intelligence of the SegmentIntersection method has been improve significantly.
    
    The submission includes a new header file (SpatialUtilityStatus.h) in which the status return values of various methods has been defined and documented.
    
    
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/MgDev@4612 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Common/Geometry/Spatial/SpatialUtility.h
+++ Common/Geometry/Spatial/SpatialUtility.h
@@ -111,39 +111,55 @@ public:
 
     ///////////////////////////////////////////////////////////////////////////
     /// <summary>
+    /// Extracts the greatest magnitude of the all the coordinates in a
+    /// coordinate collection.  Return value is always positive.
+    /// </summary>
+    /// <param name="coordinteIterator">
+    /// An iterator dervied from the target coordinate collection.
+    /// </param>
+    /// <returns>
+    /// Returns a positive value which is the largest magnitude of all
+    /// ordinates in the collection.
+    /// </returns>
+    /// <remarks>
+    /// Intended to be used to determine a suitable tolerance value for such
+    /// things as coordinate comparisons.
+    /// </remarks>
+    static double GreatestMagnitude (MgCoordinateIterator* coordinateIterator);
+
+    ///////////////////////////////////////////////////////////////////////////
+    /// <summary>
     /// Computes the intersection of two 2D line segments.  Intersection point,
     /// if any, is returned in the provided result coordinate which (of course)
-    /// must already exist.
+    /// must already exist.  All point arguments are expected (not required)
+    /// to be MgCoordinateXY objects.
     /// </summary>
     /// <param name="result">
     /// The calculated intersection point is returned in this variable which,
-    /// of course, must exist.
+    /// of course, must exist.  Coordinates of <c>result</c> remain unaltered
+    /// if an intersection does not exist.  Only X and Y are returned.
+    /// </param>
     /// <param name="seg1From">
     /// The initial point of the first line segment.
+    /// </param>
     /// <param name="seg1To">
     /// The end point of the first line segment.
+    /// </param>
     /// <param name="seg2From">
     /// The initial point of the second line segment.
+    /// </param>
     /// <param name="seg2To">
     /// The end point of the second line segment.
+    /// </param>
+    /// <param name="magnitude">
+    /// A value which represents the greatest coordinate value in the dataset
+    /// being processed.  Used to calculate an appropriate "fuzzy" value for
+    /// coordinate comparisions, etc.
+    /// </param>
     /// <returns>
-    /// Return status:
-    /// <list type="table">
-    /// <listheader>
-    ///     <term>Status</term>
-    ///     <description>Meaning</description>
-    /// </listheader>
-    /// <item>
-    ///     <term>-1</term>
-    ///     <description>no intersection, segments are parallel or collinear or
-    ///        a segemnt is of zero length; result remains unchanged
-    ///     </description>
-    /// </item>
-    /// <item><term> 0</term><desription>intersection exists, intersection point is not on either line</description></item>
-    /// <item><term> 1</term><desription>intersection exists, intersection point is on segment 1 only</description></item>
-    /// <item><term> 2</term><desription>intersection exists, intersection point is on segment 2 only</description></item>
-    /// <item><term> 3</term><desription>intersection exists, intersection point is on both segments</description></item>
-    /// </list>
+    /// Return status of the intersection of the two segments as a bit map of
+    /// values as defined in <c>SpatialUtilityStatus.h</c>
+    /// </returns>
     /// <remarks>
     /// In determining if the intersection point resides on a line, an intersection
     /// point identical to the 'to' point is considered on the line, but an
@@ -158,7 +174,8 @@ public:
     static INT32 SegmentIntersection (MgCoordinate* result,MgCoordinate* seg1From,
                                                            MgCoordinate* seg1To,
                                                            MgCoordinate* seg2From,
-                                                           MgCoordinate* seg2To);
+                                                           MgCoordinate* seg2To,
+                                                           double magnitude = 1.0E+10);
 
     ///////////////////////////////////////////////////////////////////////////////
     /// <summary>
@@ -178,26 +195,23 @@ public:
     /// closed ring.
     /// </param>
     /// <returns>
-    /// A 2D coordinate collection of all intersection points; can and often is
-    /// an empty collection.
+    /// A new collection of <c>MgCoordinateXYM</c> points, one such point for each
+    /// intersection is returned.  This collection can be, and often is, an empty
+    /// collection.  A null pointer is never returned.<para>
+    /// The X and Y ordinates of each point in the returned collection represent
+    /// the actual location of the intersection.  The M ordinate will carry, as
+    /// a double, the status of the intersection as returned by the
+    /// <c>SegmentIntersection</c> function.
     /// </returns>
-    /// <remarks>
-    /// This is a 2D function only, Z and M coordinates are ignored; the returned
-    /// point collection is a collection of <c>MgCoordinateXY</c> objects.<para>
-    /// Note that the first argument is a CoordinateIterator; so it doesn't have to be
-    /// an <c>MgPolygon</c>.  However, this function assumes that the iterator does indeed
-    /// point to a closed ring.
-    /// </remarks>
     static MgCoordinateCollection* PolySegIntersection (MgCoordinateIterator* polyItr,
                                                         MgCoordinate* segFrom,
                                                         MgCoordinate* segTo);
-
 
     ///////////////////////////////////////////////////////////////////////////////
     /// <summary>
     /// Adds a coordinate to a coordinate collection such that the resulting
     /// collection represents a collection of points in sequence from the
-    /// [rpvoded base point.
+    /// provided base point.
     /// </summary>
     /// <param name="collection">
     /// The coordinate collection to which the <c>newPoint</c> is to be added.
@@ -212,23 +226,25 @@ public:
     /// the index at which the insertion occurred.
     /// </returns>
     /// <remarks>
-    /// This is a 2D function only, Z and M coordinates are ignored; the provided
-    /// point collection is expected to be a collection of <c>MgCoordinateXY</c> objects.<para>
     /// The purpose of this function is serve as a helper for the PolySegIntersection
     /// function.  It enables PolySegIntersection to return a point collection such
     /// that the points in the collection present an orderly sequence of points from
     /// the provided base point.  Thus, if the original line segment provided to
     /// PolySegIntersection proceeded in the south to north direction, the point
     /// collection returned would also be returned in that order; regardless of the
-    /// shape of the polygon or the direction in which it proceeds.
+    /// shape of the polygon or the direction in which it proceeds.<para>
+    /// The determination as to the insertion point of <c>newPoint</c> is based solely
+    /// on the X and Y ordinates of <c>newPoint</c> and the points which already
+    /// exist in the provided collection.  The type of <c>newPoint</c> and the points
+    /// in the collection is immaterial.
     /// </remarks>
     static INT32 AddToCoordinateCollection (MgCoordinateCollection* collection,MgCoordinate* newPoint,
-                                                                        MgCoordinate* basePoint);
+                                                                               MgCoordinate* basePoint);
 
     ///////////////////////////////////////////////////////////////////////////////
     /// <summary>
     /// Determines if the provided point is inside (or actually on) the closed
-    /// ring provided bythe polyItr argument.
+    /// ring provided by the <c>polyItr</c> argument.
     /// </summary>
     /// <param name="polyItr">
     /// An iterator of the closed ring which is the subject polygon.
@@ -237,22 +253,63 @@ public:
     /// The 2D point whose status is to be determined.
     /// </param>
     /// <returns>
-    /// Returns true if the query point is inside or on the provided closed
-    /// ring.
+    /// Returns zero if the query point is outside the closed ring, +1 if the
+    /// query point is precisely on the closed ring (per MgMathUtility::DblCmp),
+    /// or +2 if the query point is inside the closed ring.
     /// </returns>
     /// <remarks>
     /// Currently, this function calculates the envelope of the provided closed
     /// ring in order to determine a point which is known to be outside of the
     /// closed ring.  An overloaded function which accepts a point known to be
-    /// outside the closed ring would be a lot faster.
+    /// outside the closed ring which is a lot faster is also available.
     /// </remarks>
-    static bool PointIsInPolygon (MgCoordinateIterator* polyItr,MgCoordinate* queryPoint);
+    static INT32 PointIsInPolygon (MgCoordinateIterator* polyItr,MgCoordinate* queryPoint);
+
+    ///////////////////////////////////////////////////////////////////////////////
+    /// <summary>
+    /// Determines a point guaranteed to be ouside the provided polygon.
+    /// </summary>
+    /// <param name="polyItr">
+    /// An iterator of the closed ring which is the subject polygon.
+    /// </param>
+    /// <returns>
+    /// Returns an <c>MgCoordinateXY</c> point which is guaranteed to be ooutside
+    /// the provided polygon.
+    /// </returns>
+    static MgCoordinate* MgSpatialUtility::PointOutsidePolygon (MgCoordinateIterator* polyItr);
+
+    ///////////////////////////////////////////////////////////////////////////////
+    /// <summary>
+    /// Determines if the provided point is inside (or actually on) the closed
+    /// ring provided by the <c>polyItr</c> argument.
+    /// </summary>
+    /// <param name="polyItr">
+    /// An iterator of the closed ring which is the subject polygon.
+    /// </param>
+    /// <param name="outsidePoint">
+    /// A 2D point which is known to be outside of the provided polygon.  Can
+    /// be obtained from <c>PointOutsidePolygon</c>.
+    /// <param name="queryPoint">
+    /// The 2D point whose status is to be determined.
+    /// </param>
+    /// <returns>
+    /// Returns zero if the query point is outside the closed ring, +1 if the
+    /// query point is precisely on the closed ring (per MgMathUtility::DblCmp),
+    /// or +2 if the query point is inside the closed ring.
+    /// </returns>
+    /// <remarks>
+    /// This function requires that the calling module supply a point known to
+    /// be outside the polygon.  Thus, higher performance can be obtained when
+    /// multiple queries against the same polygon are required.
+    /// </remarks>
+    static INT32 PointIsInPolygon (MgCoordinateIterator* polyItr,MgCoordinate* outsidePoint,
+                                                                 MgCoordinate* queryPoint);
 
     ///////////////////////////////////////////////////////////////////////////////
     /// <summary>
     /// Clips a line string to an arbitrary polygon returning a collection of line
-    /// strings which represent the portions of the the provided line string which
-    /// are inside the provided closed ring.
+    /// strings which represent the portions of the provided line string which are
+    /// inside the provided closed ring.
     /// </summary>
     /// <param name="polyItr">
     /// An iterator to the closed ring to which the provided line string is to be
@@ -261,18 +318,22 @@ public:
     /// <param name="lineItr">
     /// An iterator for the line string which is to be clipped.
     /// </param>
+    /// <param name="includeCollinear">
+    /// A true value indicates that segments, and/or portions of segments,
+    /// of the line string to be clipped which are collinear with the clipping
+    /// boundary are to be considered to be inside the clipping boundary.  A
+    /// false value causes collinear segments (or portions thereof) to be
+    /// outside the clipping boundary and thus excluded from the returned
+    /// line string collection.
+    /// </param>
     /// <returns>
     /// A collection of line string objects which represents the portions of the
     /// provided line string which are inside of the provided closed ring.  This
-    /// collection may be empty.
+    /// collection may be empty; a null pointer is never returned.
     /// </returns>
-    /// <remarks>
-    /// This is a pure 2D function.  The line strings generated will contain
-    /// collections of <c>MgCoordinateXY</c> objects.
-    /// </remarks>
     static MgLineStringCollection* ClipStringToPolygon (MgCoordinateIterator* polyItr,
-                                                        MgCoordinateIterator* lineItr);
-
+                                                        MgCoordinateIterator* lineItr,
+                                                        bool includeCollinear = false);
 protected:
 
     MgSpatialUtility() {};
@@ -287,6 +348,29 @@ protected:
     static void AppendPositionsToDistinctCollection(
         MgCoordinateCollection * distinctPositions,
         MgCoordinateCollection * positionsToAppend);
+
+    // Values which are stored in the M member of the internally used point
+    // collection; i.e. the status of each point relative to the clip ploygon.
+    static const INT32 StringPointNotDetermined = 0;
+    static const INT32 StringPointIsOutside     = 1;
+    static const INT32 StringPointIsOnBoundary  = 2;
+    static const INT32 StringPointIsInside      = 3;
+    
+    // Values which are stored in the Z member of the internally used point
+    // collection; i.e. the status of each segment relative to the clip
+    // ploygon.  The value stored refers to the segment which starts at
+    // the point in which the segment status is stored.
+    static const INT32 StringSegNotDetermined = 0;
+    static const INT32 StringSegIsOutside     = 1;
+    static const INT32 StringSegIsCollinear   = 2;
+    static const INT32 StringSegIsInside      = 3;
+
+    static MgCoordinateCollection* StringClipPhaseOne (MgCoordinateIterator* lineString,
+                                                       MgCoordinateIterator* itrPolygon);
+    static void StringClipPhaseTwo (MgCoordinateIterator* lineString,MgCoordinateIterator* itrPolygon);
+    static void StringClipPhaseThree (MgCoordinateIterator* lineString,MgCoordinateIterator* itrPolygon);
+    static MgLineStringCollection* StringClipPhaseFour (MgCoordinateIterator* lineString,bool collinear = false,
+                                                                                         bool outside = false);
 };
 /// \endcond
 
