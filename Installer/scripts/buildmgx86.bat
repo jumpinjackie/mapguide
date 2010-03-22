82d52a01ec78f647afb256f630adf46eca623b59
trevorwekel <trevorwekel@example.org>
2010-03-22 13:39:15 +0000

    Ticket [https://trac.osgeo.org/mapguide/ticket/1295] Improve build scripts for 2.2 release cycle Initial 32 bit Windows installer integration into build script
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@4679 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Installer/scripts/buildmgx86.bat
+++ Installer/scripts/buildmgx86.bat
@@ -1,12 +1,15 @@
 SET BUILDROOT=c:\builds\mg22win32
-SET FDOBUILD=c:\builds\fdo35win32\install\Fdo
+rem SET FDOBUILD=c:\builds\fdo35win32\install\Fdo
+SET FDOBUILD=c:\fdo32rc1
 SET MGSOURCE=%BUILDROOT%\mgdev
-SET INSTALLROOT=%BUILDROOT%\install
+SET MGINSTALL=%BUILDROOT%\installer
+SET INSTALLROOT=%MGSOURCE%\Release
 SET SOLUTIONCONFIG="Release|Win32"
 SET OEMFDO=%MGSOURCE%\Oem\FDO
+SET STARTDIR=%CD%
 
 rmdir /S /Q %BUILDROOT%
-mkdir %INSTALLROOT%
+rmdir /S /Q %INSTALLROOT%
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
 
@@ -15,6 +18,7 @@ rem TODO get build number from Subversion
 svn export svn://svn.bld.mgproto.net/mapguide/trunk/MgDev %MGSOURCE%
 svn export svn://svn.bld.mgproto.net/metacrs/csmap/trunk/CsMapDev %MGSOURCE%\Oem\CsMap
 svn export svn://svn.bld.mgproto.net/fusion/trunk %MGSOURCE%\Oem\fusion
+svn export svn://svn.bld.mgproto.net/mapguide/trunk/Installer %MGINSTALL% 
 
 mkdir %OEMFDO%\Bin\Release
 xcopy /Y /E %FDOBUILD%\Bin\* %OEMFDO%\Bin\Release
@@ -32,3 +36,14 @@ devenv Server.sln /build %SOLUTIONCONFIG%
 
 cd %MGSOURCE%\Web\src
 devenv WebTier.sln /build %SOLUTIONCONFIG%
+
+cd %MGSOURCE%
+call build.bat
+call build.bat -a=install 
+
+cd %MGINSTALL%
+call build.bat -source=%INSTALLROOT% -a=prepare
+call build.bat -source=%INSTALLROOT% -a=generate 
+call build.bat -source=%INSTALLROOT% -version=2.2.0.0 -name=MapGuideOpenSource-2.2.0-Beta1 -title="MapGuide Open Source 2.2 Beta 1"
+
+cd %STARTDIR%
