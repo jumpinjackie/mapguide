f4504b67910b667223af8fbdae003a4414a0a4ac
Jackie Ng <jumpinjackie@gmail.com>
2010-03-25 12:23:55 +0000

    Re-org the help documentation. Web API docs now gets copied to help\webapi
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@4693 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/build.bat
+++ MgDev/build.bat
@@ -432,7 +432,7 @@ if "%TYPECOMPONENT%"=="web" goto quit
 
 :install_doc
 echo [install]: Documentation
-%XCOPY% "%MG_DOC_OUTPUT%" "%MG_OUTPUT_WEB%\www\help" /EXCLUDE:svn_excludes.txt+%TYPEBUILD%_excludes.txt
+%XCOPY% "%MG_DOC_OUTPUT%" "%MG_OUTPUT_WEB%\www\help\webapi" /EXCLUDE:svn_excludes.txt+%TYPEBUILD%_excludes.txt
 echo [install]: Developer's Guide Sample Code
 %XCOPY% "%MG_DOC_DEVGUIDE_SAMPLES%" "%MG_OUTPUT_WEB%\www\devguide" /EXCLUDE:svn_excludes.txt+%TYPEBUILD%_excludes.txt
 echo [install]: Viewer API documentation
