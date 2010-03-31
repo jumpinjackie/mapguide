3b4838afd20389b3f93467642b34e2c28d0d584e
trevorwekel <trevorwekel@example.org>
2010-03-31 23:00:45 +0000

    Ticket [https://trac.osgeo.org/mapguide/ticket/1295] Improve build scripts for 2.2 release cycle - Added preliminary MapGuide packages into Ubuntu install script
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@4730 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Installer/scripts/mginstallubuntu.sh
+++ Installer/scripts/mginstallubuntu.sh
@@ -2,21 +2,50 @@
 TEMPDIR=/tmp/build_mapguide
 URL="http://download.osgeo.org/mapguide/testing"
 FDOVER=3.5.0-5343_i386
+MGVER=2.2.0-4724_i386
 # Create temporary download directory
 mkdir -p ${TEMPDIR}
 pushd ${TEMPDIR}
 
-# Download Ubuntu packages
+# Download Ubuntu packages for FDO
 for file in core gdal kingoracle ogr postgis rdbms sdf shp sqlite wfs wms
 do
   wget -N ${URL}/fdo-${file}_${FDOVER}.deb
 done
 
-# Install Ubuntu packages
+# Download Ubuntu packages for MapGuide
+for file in common server webextensions httpd
+do
+  wget -N ${URL}/mapguideopensource-${file}_${MGVER}.deb
+done
+
+# Install Ubuntu packages for FDO
 for file in core gdal kingoracle ogr postgis rdbms sdf shp sqlite wfs wms
 do
   dpkg -E -G --install fdo-${file}_${FDOVER}.deb
 done
 
+# Install Ubuntu packages for MapGuide
+for file in common server webextensions httpd
+do
+  dpkg -E -G --install mapguideopensource-${file}_${MGVER}.deb
+done
 
-pushd 
+popd
+
+# Install required packages missed due CentOS 5.4 / Ubuntu 9.10 differences
+apt-get install expat libssl0.9.8
+
+# Resolve CentOS 5.4 / Ubuntu 9.10 shared lib differences with symlinks
+if [ ! -e /lib/libcrypto.so.6 ]; then
+  ln -s /lib/libcrypto.so.0.9.8 /lib/libcrypto.so.6
+fi
+
+if [ ! -e /lib/libssl.so.6 ]; then
+  ln -s /lib/libssl.so.0.9.8 /lib/libssl.so.6
+fi
+
+if [ ! -e /lib/libexpat.so.0 ]; then
+  ln -s /lib/libexpat.so.1 /lib/libexpat.so.0
+fi
+
