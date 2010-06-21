4685b479f14b5dcbe70d04d946c71954b17526fa
trevorwekel <trevorwekel@example.org>
2010-06-21 21:49:19 +0000

    Ticket [https://trac.osgeo.org/mapguide/ticket/1295] Improve build scripts for 2.2 release cycle - Add preliminary build/install work for 64 bit Windows
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@4964 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Installer/scripts/buildmgx64.bat
+++ Installer/scripts/buildmgx64.bat
@@ -1,27 +1,43 @@
 SET BUILDROOT=c:\builds\mg22x64
+rem SET FDOBUILD=c:\builds\fdo35x64\install\Fdo
+SET FDOBUILD=c:\fdo64rc1
 SET MGSOURCE=%BUILDROOT%\mgdev
-SET INSTALLROOT=%BUILDROOT%\install
-SET PROJECTCONFIG="Release|x64"
+SET MGINSTALL=%BUILDROOT%\installer
+SET INSTALLROOT=%MGSOURCE%\Release64
+SET OEMFDO=%MGSOURCE%\Oem\FDO
+SET STARTDIR=%CD%
 
 rmdir /S /Q %BUILDROOT%
-mkdir %INSTALLROOT%
+rmdir /S /Q %INSTALLROOT%
 
 call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat" x86
 
-rem TODO get build number from Subversion
+svn info svn://svn.bld.mgproto.net/mapguide/trunk/MgDev | perl revnum.pl > revision.txt
+set /p REVISION= < revision.txt
+set REVISION=4829
 
-svn export svn://svn.bld.mgproto.net/mapguide/trunk/mgdev %MGSOURCE%
-svn export svn://svn.bld.mgproto.net/metacrs/csmap/trunk/CsMapDev %MGSOURCE%\Oem\CsMap
-svn export svn://svn.bld.mgproto.net/fusion/trunk %MGSOURCE%\Oem\fusion
+svn export -r %REVISION%  svn://svn.bld.mgproto.net/mapguide/trunk/MgDev %MGSOURCE%
 
-cd %MGSOURCE%\Oem
-devenv /Build oem.sln /ProjectConfig %PROJECTCONFIG%
 
-cd %MGSOURCE%\Oem\CsMap
-devenv /Build OpenSource.sln /ProjectConfig %PROJECTCONFIG%
+svn export svn://svn.bld.mgproto.net/mapguide/trunk/Installer %MGINSTALL% 
 
-cd %MGSOURCE%\Server
-devenv /Build Server.sln /ProjectConfig %PROJECTCONFIG%
+cd %MGSOURCE%
+cscript updateversion.vbs /major:2 /minor:2 /point:0 /build:%REVISION%
 
-cd %MGSOURCE%\Web\src
-devenv /Build WebTier.sln /ProjectConfig %PROJECTCONFIG%
+
+mkdir %OEMFDO%\Bin\Release64
+mkdir %OEMFDO%\Bin\Lib64
+xcopy /Y /E %FDOBUILD%\Bin\* %OEMFDO%\Bin\Release64
+xcopy /Y /E /I %FDOBUILD%\Inc %OEMFDO%\Inc
+xcopy /Y /E /I %FDOBUILD%\Lib %OEMFDO%\Lib64
+
+cd %MGSOURCE%
+call build64.bat
+call build64.bat -a=install 
+
+cd %MGINSTALL%
+call build64.bat -source=%INSTALLROOT% -a=prepare
+call build64.bat -source=%INSTALLROOT% -a=generate 
+call build64.bat -source=%INSTALLROOT% -version=2.2.0.%REVISION% -name=MapGuideOpenSource-2.2.0.%REVISION%-Beta1-x64 -title="MapGuide Open Source 2.2 x64 Beta 1"
+
+cd %STARTDIR%
