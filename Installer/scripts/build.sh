3315fbc995d0ea9cd7b41c608d6a8b125f069344
trevorwekel <trevorwekel@example.org>
2011-04-17 10:52:05 +0000

    Build improvement for Linux - burn version information of all svn repos into build script
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.2@5703 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Installer/scripts/build.sh
+++ Installer/scripts/build.sh
@@ -12,10 +12,18 @@ SVNROOT="svn://svn.bld.mgproto.net"
 rm -rf ${MGSOURCE}
 rm -rf ${INSTALLROOT}
 
-REVISION=`svn info ${SVNROOT}/mapguide/branches/2.2/MgDev | perl revnum.pl`
+MGDEVREV=5624
+FUSIONREV=2366
+CSMAPREV=1964
+
+REVISION=`svn info ${SVNROOT}/mapguide/branches/2.2/Installer | perl revnum.pl`
 echo ${REVISION} > revnum.txt
+
+
 echo "Building Revision ${BUILDNUM}.${REVISION}" 
-svn export -q -r ${REVISION} ${SVNROOT}/mapguide/branches/2.2/MgDev ${MGSOURCE}
+svn export --ignore-externals -r ${MGDEVREV}  svn://svn.bld.mgproto.net/mapguide/branches/2.2/MgDev ${MGSOURCE}
+svn export -r ${FUSIONREV} svn://svn.bld.mgproto.net/fusion/branches/fusion-2.2 ${MGSOURCE}/Oem/fusion
+svn export -r ${CSMAPREV} svn://svn.bld.mgproto.net/metacrs/csmap/branches/12.02/CsMapDev ${MGSOURCE}/Oem/CsMap
 
 cd ${MGSOURCE}
 
@@ -45,7 +53,8 @@ make
 make install
 
 # Prepare binaries for packaging by removing unnecessary
-# .la and .a files and stripping unneeded symbols from the binaries
+# .la and .a files.  Remove executable permission to make Debian packager happy
+# Keep symbols around for Debian packaging
 pushd ${INSTALLROOT}
 LIBDIRS="lib server/lib webserverextensions/lib" 
 for libdir in ${LIBDIRS}
@@ -54,11 +63,32 @@ do
    rm -f ${libdir}/*.la
    rm -f ${libdir}/*.a
 
-   # Remove unneeded symbols from files in the lib directories
+   # Remove execute permission from files in lib directory
+   for file in `find ${libdir}/lib*.so* -type f -print`
+   do
+      chmod a-x ${file}
+   done
+done
+popd
+
+# Tarball the whole works and put it in a bin directory
+pushd ${BUILDROOT}
+if [ ! -d bin ]; then
+   mkdir -p bin
+fi
+tar -zcf bin/mapguideopensource-${BUILDNUM}.${REVISION}_symbols.tar.gz ${INSTALLROOT} ${LOCKFILEDIR}
+popd
+
+
+# For the final executables, strip the symbols before tarballing
+pushd ${INSTALLROOT}
+LIBDIRS="lib server/lib webserverextensions/lib" 
+for libdir in ${LIBDIRS}
+do
+   # Strip symbols from final executable
    for file in `find ${libdir}/lib*.so* -type f -print`
    do
       strip --strip-unneeded ${file}
-      chmod a-x ${file}
    done
 done
 popd
