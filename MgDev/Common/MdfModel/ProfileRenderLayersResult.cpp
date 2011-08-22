690f5c47955a31bcae7afc464a246b162dcfcb3e
liuar <liuar@example.org>
2011-08-22 04:56:16 +0000

    The RFC 110 Profiling Service (IV).
    Implement MdfModel and MdfParser (serialize only) for profile results.
    
    
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@6084 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ MgDev/Common/MdfModel/ProfileRenderLayersResult.cpp
@@ -0,0 +1,82 @@
+//
+//  Copyright (C) 2010-2011 by Autodesk, Inc.
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of version 2.1 of the GNU Lesser
+//  General Public License as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+//-------------------------------------------------------------------------
+// DESCRIPTION:
+// The ProfileRenderLayersResult class implementation.
+//-------------------------------------------------------------------------
+
+#include "stdafx.h"
+#include "ProfileRenderLayersResult.h"
+
+using namespace MDFMODEL_NAMESPACE;
+
+//-------------------------------------------------------------------------
+// PURPOSE: Construct and initialize an instance of the ProfileRenderLayersResult class.
+//-------------------------------------------------------------------------
+ProfileRenderLayersResult::ProfileRenderLayersResult()
+{
+    this->m_dRenderTime = 0.0;
+
+}
+
+//-------------------------------------------------------------------------
+// PURPOSE: Destructor. Release any dynamically allocated resources.
+//-------------------------------------------------------------------------
+ProfileRenderLayersResult::~ProfileRenderLayersResult()
+{
+}
+
+//-------------------------------------------------------------------------
+// PURPOSE: Accessor method for the RenderTime property.
+//          
+// RETURNS: The time of stylizing all layers in the map.
+//-------------------------------------------------------------------------
+const double ProfileRenderLayersResult::GetRenderTime() const
+{
+    return this->m_dRenderTime;
+}
+
+//-------------------------------------------------------------------------
+// PURPOSE: Accessor method to the RenderTime property.
+// PARAMETERS:
+//      Input:
+//          dRenderTime - The time of stylizing all layers in the map.
+//-------------------------------------------------------------------------
+void ProfileRenderLayersResult::SetRenderTime(const double& dRenderTime)
+{
+    this->m_dRenderTime = dRenderTime;
+}
+
+//-------------------------------------------------------------------------
+// PURPOSE: Accessor method for the ProfileRenderLayerResults property.
+//          
+// RETURNS: A collection of ProfileRenderLayerResult.
+//-------------------------------------------------------------------------
+ProfileRenderLayerResultCollection* ProfileRenderLayersResult::GetProfileRenderLayerResults()
+{
+    return &this->m_profileRenderLayerResults;
+}
+
+#ifdef _WIN32
+#ifdef _DEBUG
+int ProfileRenderLayersResult::DumpMemoryLeaks()
+{
+    return _CrtDumpMemoryLeaks();
+}
+#endif
+#endif
---
+++ MgDev/Common/MdfModel/ProfileResult.cpp
@@ -0,0 +1,69 @@
+//
+//  Copyright (C) 2004-2011 by Autodesk, Inc.
+//
+//  This library is free software; you can redistribute it and/or
+//  modify it under the terms of version 2.1 of the GNU Lesser
+//  General Public License as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//  Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public
+//  License along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+//
+
+//-------------------------------------------------------------------------
+// DESCRIPTION:
+// The ScaleRange class implementation.
+//-------------------------------------------------------------------------
+
+#include "stdafx.h"
+#include "ProfileResult.h"
+
+using namespace MDFMODEL_NAMESPACE;
+
+//-------------------------------------------------------------------------
+// PURPOSE: Construct and initialize an instance of the ProfileResult class.
+//-------------------------------------------------------------------------
+ProfileResult::ProfileResult()
+{
+}
+
+//-------------------------------------------------------------------------
+// PURPOSE: Destructor.
+//-------------------------------------------------------------------------
+ProfileResult::~ProfileResult()
+{
+}
+
+//-------------------------------------------------------------------------
+// PURPOSE: Accessor method for the ProfileResultType attribute.
+// RETURNS: A profile result type attribute
+//-------------------------------------------------------------------------
+ProfileResult::ProfileResultType ProfileResult::GetProfileResultType() const
+{
+    return this->m_eProfileResultType;
+}
+   
+//-------------------------------------------------------------------------
+// PURPOSE: Accessor method for the ProfileResultType attribute.
+// PARAMETERS:
+//      Input:
+//          type - profile result type
+//-------------------------------------------------------------------------
+void ProfileResult::SetProfileResultType(ProfileResult::ProfileResultType type)
+{
+    this->m_eProfileResultType = type;
+}
+
+#ifdef _WIN32
+#ifdef _DEBUG
+int ProfileResult::DumpMemoryLeaks()
+{
+    return _CrtDumpMemoryLeaks();
+}
+#endif
+#endif
