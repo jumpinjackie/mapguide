44550067d3ed31fce5ff8e24397203785fd18034
Jackie Ng <jumpinjackie@gmail.com>
2011-09-13 02:45:36 +0000

    #1804: Omit building Common.sln as building Server and WebTier solutions will build whatever shared components are in this solution
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/MgDev@6132 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- build.bat
+++ build.bat
@@ -184,7 +184,6 @@ goto custom_error
 :get_component
 SET TYPECOMPONENT=%2
 if "%2"=="oem" goto next_param
-if "%2"=="common" goto next_param
 if "%2"=="server" goto next_param
 if "%2"=="web" goto next_param
 if "%2"=="doc" goto next_param
@@ -235,7 +234,6 @@ if "%TYPEACTION%"=="install" goto install
 
 :clean
 if "%TYPECOMPONENT%"=="oem" goto clean_oem
-if "%TYPECOMPONENT%"=="common" goto clean_common
 if "%TYPECOMPONENT%"=="server" goto clean_server
 if "%TYPECOMPONENT%"=="web" goto clean_web
 if "%TYPECOMPONENT%"=="doc" goto clean_doc
@@ -245,11 +243,6 @@ echo [clean]: Clean Oem
 %MSBUILD_CLEAN% %MG_OEM%\Oem.sln
 echo [clean]: Clean Oem - CS-Map
 %MSBUILD_CLEAN% %MG_OEM%\CSMap\VC90\OpenSource.sln
-if not "%TYPECOMPONENT%"=="all" goto quit
-
-:clean_common
-echo [clean]: Clean Common
-%MSBUILD_CLEAN% %MG_COMMON%\Common.sln
 if not "%TYPECOMPONENT%"=="all" goto quit
 
 :clean_server
@@ -276,7 +269,6 @@ rem =======================================================
 
 :build
 if "%TYPECOMPONENT%"=="oem" goto build_oem
-if "%TYPECOMPONENT%"=="common" goto build_common
 if "%TYPECOMPONENT%"=="server" goto build_server
 if "%TYPECOMPONENT%"=="web" goto build_web
 if "%TYPECOMPONENT%"=="doc" goto build_doc
@@ -296,17 +288,6 @@ echo [build]: Building Oem - CSMap
 %MSBUILD% %MG_OEM%\CsMap\VC90\OpenSource.sln
 if "%errorlevel%"=="1" goto error
 if "%TYPECOMPONENT%"=="oem" 	goto quit
-if "%TYPECOMPONENT%"=="common" 	goto quit
-if "%TYPECOMPONENT%"=="server" 	goto quit
-if "%TYPECOMPONENT%"=="web" 	goto quit
-if "%TYPECOMPONENT%"=="doc" 	goto quit
-
-:build_common
-echo [build]: Building Common
-%MSBUILD% %MG_COMMON%\Common.sln
-if "%errorlevel%"=="1" goto error
-if "%TYPECOMPONENT%"=="oem" 	goto quit
-if "%TYPECOMPONENT%"=="common" 	goto quit
 if "%TYPECOMPONENT%"=="server" 	goto quit
 if "%TYPECOMPONENT%"=="web" 	goto quit
 if "%TYPECOMPONENT%"=="doc" 	goto quit
@@ -316,7 +297,6 @@ echo [build]: Building Server
 %MSBUILD% %MG_SERVER%\Server.sln
 if "%errorlevel%"=="1" goto error
 if "%TYPECOMPONENT%"=="oem" 	goto quit
-if "%TYPECOMPONENT%"=="common" 	goto quit
 if "%TYPECOMPONENT%"=="server" 	goto quit
 if "%TYPECOMPONENT%"=="web" 	goto quit
 if "%TYPECOMPONENT%"=="doc" 	goto quit
@@ -326,7 +306,6 @@ echo [build]: Building Web Tier
 %MSBUILD% %MG_WEB_SRC%\WebTier.sln
 if "%errorlevel%"=="1" goto error
 if "%TYPECOMPONENT%"=="oem" 	 goto quit
-if "%TYPECOMPONENT%"=="common" 	 goto quit
 if "%TYPECOMPONENT%"=="server" 	 goto quit
 if "%TYPECOMPONENT%"=="web" 	 goto quit
 if "%TYPECOMPONENT%"=="doc" 	 goto quit
@@ -473,7 +452,6 @@ echo                                  install,
 echo                                  clean,
 echo Component:             -w[ith]=all(default),
 echo                                oem,
-echo                                common,
 echo                                server,
 echo                                web,
 echo                                doc
