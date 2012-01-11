5d9c6778f93eae6637c3887e7f58a5fc8fb347ee
Jackie Ng <jumpinjackie@gmail.com>
2012-01-11 06:53:50 +0000

    [https://trac.osgeo.org/mapguide/ticket/1841], [https://trac.osgeo.org/mapguide/ticket/894]: Define the required build variables in setenvironment.bat and setenvironment64.bat. This has to be called first before calling the respective build.bat and build64.bat. The build scripts have also been updated to call into the new build_fusion.bat
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@6417 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/build.bat
+++ MgDev/build.bat
@@ -43,80 +43,6 @@ rem
 rem Please note that -a=install does nothing if -w=oem
 
 rem ==================================================
-rem Top-level vars
-rem ==================================================
-SET OLDPATH=%PATH%
-SET TYPEACTION=build
-SET TYPEBUILD=Release
-SET TYPECOMPONENT=all
-
-rem ==================================================
-rem MapGuide vars
-rem ==================================================
-SET MG_DEV=%CD%
-SET MG_OEM=%MG_DEV%\Oem
-rem DBXML seems to change frequently enough that we should externalize this information
-SET MG_OEM_DBXML=dbxml
-SET MG_OEM_DB=db-4.8.26
-SET MG_COMMON=%MG_DEV%\Common
-SET MG_SERVER=%MG_DEV%\Server
-SET MG_WEB=%MG_DEV%\Web
-SET MG_WEB_SRC=%MG_WEB%\src
-SET MG_WEB_BIN=%MG_WEB%\bin
-SET MG_UNIT_TEST=%MG_DEV%\UnitTest
-SET MG_FUSION=%MG_OEM%\Fusion
-SET MG_DOC=%MG_DEV%\Doc
-SET MG_DOC_OUTPUT=%MG_DOC%\MgOpensource_WebAPIReference
-SET MG_DOC_DEVGUIDE_SAMPLES=%MG_DOC%\samples\
-SET MG_BUILD_TEMP=%MG_DEV%\BuildTemp
-
-SET MG_OUTPUT=%MG_DEV%\%TYPEBUILD%
-SET MG_OUTPUT_SERVER=%MG_OUTPUT%\Server
-SET MG_OUTPUT_WEB=%MG_OUTPUT%\Web
-SET MG_OUTPUT_CSMAP=%MG_OUTPUT%\CS-Map
-SET MG_BUILD_COMPONENT=
-
-SET MG_DEFAULT_INSTALLDIR=C:\Program Files\MapGuideOpenSource2.0
-SET MG_ERROR=0
-
-rem ==================================================
-rem Extra tool paths
-rem 
-rem Your paths may be different, adjust accordingly or
-rem set the appropriate environment variables before
-rem running
-rem ==================================================
-
-IF "%JAVA_HOME%" == "" SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_21
-IF "%DOXYGEN%" == "" SET DOXYGEN=%MG_DEV%\BuildTools\doxygen
-IF "%GNUWIN32%" == "" SET GNUWIN32=C:\Program Files\GnuWin32\bin
-IF "%SEVENZ%" == "" SET SEVENZ=%MG_DEV%\BuildTools\WebTools\7-Zip
-IF "%ANT_HOME%" == "" SET ANT_HOME=%MG_DEV%\BuildTools\apache-ant-1.7.1
-
-SET PATH=%PATH%;%DOXYGEN%;%GNUWIN32%;%SEVENZ%;%ANT_HOME%\bin
-
-rem ==================================================
-rem MSBuild Settings
-rem ==================================================
-
-rem If the NUMBER_OF_PROCESSORS environment variable is wrong for any reason. Change this value.
-SET CPU_CORES=%NUMBER_OF_PROCESSORS%
-
-rem Uncomment the line below to enable msbuild logging
-rem SET MSBUILD_LOG=/l:FileLogger,Microsoft.Build.Engine;logfile=Build.log;verbosity=diagnostic
-SET MSBUILD_VERBOSITY=/v:q
-
-rem ==================================================
-rem Command aliases
-rem ==================================================
-rem SET XCOPY=xcopy /E /Y /I /F
-SET XCOPY=xcopy /E /Y /I /Q /H
-SET XCOPY_SINGLE=xcopy /Y /I /Q
-SET MSBUILD=msbuild.exe /nologo /m:%CPU_CORES% /p:Configuration=%TYPEBUILD% %MSBUILD_VERBOSITY% %MSBUILD_LOG%
-SET MSBUILD_CLEAN=msbuild.exe /nologo /m:%CPU_CORES% /p:Configuration=%TYPEBUILD% /t:Clean %MSBUILD_VERBOSITY%
-SET ANT=ant
-
-rem ==================================================
 rem Parameter Handling
 rem ==================================================
 
@@ -199,16 +125,16 @@ rem ==================================================
 :pre_build_check
 :check_java
 if exist "%JAVA_HOME%" goto check_ant
-SET ERRORMSG=Unable to find java sdk installation. Please install the java sdk and set the JAVA_HOME environment variable
+SET ERRORMSG=Unable to find java sdk installation. Please install the java sdk and set the JAVA_HOME environment variable in setenvironment.bat
 goto custom_error_no_help
 :check_ant
 if exist "%ANT_HOME%" goto check_fdo
-SET ERRORMSG=Unable to find Apache ant installation. Please install apache ant and set the ANT_HOME environment variable
+SET ERRORMSG=Unable to find Apache ant installation. Please install apache ant and set the ANT_HOME environment variable in setenvironment.bat
 goto custom_error_no_help
 :check_fdo
 :check_fdo_bin
 if exist "%MG_OEM%\FDO\Bin\%TYPEBUILD%" goto check_fdo_inc
-SET ERRORMSG=Unable to find FDO binaries. Copy the FDO binaries %MG_OEM%\FDO\Bin\%TYPEBUILD%
+SET ERRORMSG=Unable to find FDO binaries. Copy the FDO binaries to %MG_OEM%\FDO\Bin\%TYPEBUILD%
 goto custom_error_no_help
 :check_fdo_inc
 if exist "%MG_OEM%\FDO\Bin\%TYPEBUILD%" goto check_fdo_lib
@@ -403,13 +329,12 @@ echo [install]: Web Tier - mapviewerjava
 %XCOPY% "%MG_WEB_SRC%\mapviewerjava" "%MG_OUTPUT_WEB%\www\mapviewerjava" /EXCLUDE:%TYPEBUILD%_excludes.txt
 echo [install]: Web Tier - mapviewerjava - WEB-INF
 %XCOPY% "%MG_WEB_SRC%\WEB-INF" "%MG_OUTPUT_WEB%\www\WEB-INF" /EXCLUDE:svn_excludes.txt+%TYPEBUILD%_excludes.txt
-echo [install]: Web Tier - fusion templates
-%XCOPY% "%MG_OEM%\fusionMG" "%MG_OUTPUT_WEB%\www\fusion" /EXCLUDE:svn_excludes.txt+%TYPEBUILD%_excludes.txt
+echo [install]: Web Tier - fusion
+call build_fusion.bat
+%XCOPY% "%MG_OEM%\fusion_build" "%MG_OUTPUT_WEB%\www\fusion" /EXCLUDE:%CONFIGURATION%_excludes.txt
 echo [install]: Web Tier - Apache module
 if not exist "%MG_OUTPUT_WEB%\Apache2\modules" mkdir "%MG_OUTPUT_WEB%\Apache2\modules"
 %XCOPY% /F "%MG_WEB_SRC%\mapagent\mod_mgmapagent.so" "%MG_OUTPUT_WEB%\Apache2\modules"
-echo [install]: Web Tier - fusion
-call %ANT% deploy -Ddeploy.home="%MG_OUTPUT_WEB%\www\fusion" -f "%MG_OEM%\fusion\build.xml"
 if "%TYPECOMPONENT%"=="server" goto quit
 if "%TYPECOMPONENT%"=="web" goto quit
 
