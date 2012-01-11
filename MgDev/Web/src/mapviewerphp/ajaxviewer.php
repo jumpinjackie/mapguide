7cc2a90b3c8a8a8330df0c21304e51940f999075
Jackie Ng <jumpinjackie@gmail.com>
2012-01-11 10:38:34 +0000

    This submission is to test my RFC111-enabled installation
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@6418 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Web/src/mapviewerphp/ajaxviewer.php
+++ MgDev/Web/src/mapviewerphp/ajaxviewer.php
@@ -17,7 +17,7 @@
 //  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 //
 
-// Start output buffering
+// Start output buffering 
 ob_start();
 
 include 'mainframe.php';
