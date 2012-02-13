c322885b6074f5390f78e7ff4b19a86a5f62a92b
Jackie Ng <jumpinjackie@gmail.com>
2012-02-13 13:17:15 +0000

    [https://trac.osgeo.org/mapguide/ticket/1954]: Update internal version numbers to 2.4.0.0
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@6504 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Common/ProductVersion.h
+++ MgDev/Common/ProductVersion.h
@@ -18,7 +18,7 @@
 #ifndef PRODUCTVERSION_H_
 #define PRODUCTVERSION_H_
 
-const STRING ProductVersion = L"2.3.0.0";
-const STRING ApiVersion     = L"2.3";  // Major.Minor only
+const STRING ProductVersion = L"2.4.0.0";
+const STRING ApiVersion     = L"2.4";  // Major.Minor only
 
 #endif // PRODUCTVERSION_H_
