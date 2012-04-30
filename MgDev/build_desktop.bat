ca98223435a11a69726d2006116dd134dc4dee50
Jackie Ng <jumpinjackie@gmail.com>
2012-04-30 15:33:59 +0000

    Modify build_desktop.bat to build the desktop .net solution with a default configuration/platform. Valid invocations include:  - build_desktop.bat [builds for x86]  - build_desktop.bat x86 [builds for x86]  - build_desktop.bat x64 [builds for x64]
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.4@6604 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/build_desktop.bat
+++ MgDev/build_desktop.bat
@@ -16,11 +16,16 @@ SET MG_OUTPUT_DESKTOP=%MG_OUTPUT%\Desktop
 SET VS_SLN_SUFFIX=
 IF "%VC_COMPILER_VERSION%" == "10" SET VS_SLN_SUFFIX=_VS2010
 
+SET DESKTOP_PLATFORM_UNMANAGED=Win32
+SET DESKTOP_PLATFORM=%1
+IF "%DESKTOP_PLATFORM%" == "" SET DESKTOP_PLATFORM=x86
+IF "%DESKTOP_PLATFORM%" == "x64" SET DESKTOP_PLATFORM_UNMANAGED=x64
+
 pushd Desktop
-echo [build]: Desktop API
-%MSBUILD% MgDesktopApi%VS_SLN_SUFFIX%.sln
-echo [build]: .net components
-%MSBUILD% MgDesktopDotNet%VS_SLN_SUFFIX%.sln
+echo [build]: Desktop API (%DESKTOP_PLATFORM_UNMANAGED%)
+%MSBUILD% /p:Platform=%DESKTOP_PLATFORM_UNMANAGED% MgDesktopApi%VS_SLN_SUFFIX%.sln
+echo [build]: .net components (%DESKTOP_PLATFORM%)
+%MSBUILD% /p:Platform=%DESKTOP_PLATFORM% MgDesktopDotNet%VS_SLN_SUFFIX%.sln
 popd
 echo [install]: binaries
 %XCOPY% "Desktop\bin\%TYPEBUILD%" "%MG_OUTPUT_DESKTOP%" /EXCLUDE:svn_excludes.txt+%CONFIGURATION%_excludes.txt
