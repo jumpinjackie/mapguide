1b496fc0275c6273bafd20740acae12130e03b20
Jackie Ng <jumpinjackie@gmail.com>
2012-05-03 14:38:06 +0000

    Merge in r6607 and r6608
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@6609 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ MgDev/Desktop/MapViewer/MgControlImpl.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace OSGeo.MapGuide.Viewer
+{
+    /// <summary>
+    /// Base class for views show by their associated <see cref="T:OSGeo.MapGuide.Viewer.MgViewerComponent"/> instances
+    /// </summary>
+    [ToolboxItem(false)]
+    public class MgControlImpl : UserControl
+    {
+        /// <summary>
+        /// Gets or set the title to show if this view is shown in a new window
+        /// </summary>
+        public virtual string Title { get; set; }
+
+        protected MgControlImpl() { this.ModalWindow = false; }
+
+        /// <summary>
+        /// Gets whether to show as a modal window if parent component chooses to show this component
+        /// in a new window. Only applies if parent component shows this component in a new window
+        /// </summary>
+        public virtual bool ModalWindow { get; set; }
+    }
+}
---
+++ MgDev/Desktop/MapViewer/MgInvokeComponent.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms.Design;
+using System.Diagnostics;
+
+namespace OSGeo.MapGuide.Viewer
+{
+    /// <summary>
+    /// A specialized instance of <see cref="T:System.Windows.Forms.ToolStripButton"/> that can
+    /// invoke the assigned <see cref="T:OSGeo.MapGuide.Viewer.MgComponent"/> instance.
+    /// </summary>
+    [ToolStripItemDesignerAvailability(ToolStripItemDesignerAvailability.ToolStrip)]
+    public class MgInvokeComponentButton : ToolStripButton, IMapViewerBusyStateListener
+    {
+        private MgComponent _component;
+
+        /// <summary>
+        /// Gets or sets the target component to be invoked
+        /// </summary>
+        [Category("MapGuide Component Properties")]
+        [Description("The target component to invoke when this component is clicked. All other UI properties are inherited from this compoent")]
+        public MgComponent TargetComponent
+        {
+            get { return _component; }
+            set 
+            {
+                if (_component != null)
+                    _component.RemoveListener(this);
+                _component = value;
+                if (_component != null)
+                {
+                    base.ToolTipText = _component.ToolTipText;
+                    base.Text = _component.Label;
+                    base.Image = _component.Icon;
+                    _component.AddListener(this);
+                }
+            }
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            if (this.TargetComponent == null)
+                throw new InvalidOperationException("No target component assigned to this component");
+
+            this.TargetComponent.Invoke();
+
+            base.OnClick(e);
+        }
+
+        public void SetBusy(bool busy)
+        {
+            this.Enabled = !busy;
+        }
+    }
+
+    /// <summary>
+    /// A specialized instance of <see cref="T:System.Windows.Forms.ToolStripMenuItem"/> that can
+    /// invoke the assigned <see cref="T:OSGeo.MapGuide.Viewer.MgComponent"/> instance.
+    /// </summary>
+    [ToolStripItemDesignerAvailability(ToolStripItemDesignerAvailability.ContextMenuStrip | ToolStripItemDesignerAvailability.MenuStrip)]
+    public class MgInvokeComponentMenuStripItem : ToolStripMenuItem, IMapViewerBusyStateListener
+    {
+        private MgComponent _component;
+
+        /// <summary>
+        /// Gets or sets the target component to be invoked
+        /// </summary>
+        [Category("MapGuide Component Properties")]
+        [Description("The target component to invoke when this component is clicked. All other UI properties are inherited from this compoent")]
+        public MgComponent TargetComponent
+        {
+            get { return _component; }
+            set
+            {
+                if (_component != null)
+                    _component.RemoveListener(this);
+                _component = value;
+                if (_component != null)
+                {
+                    base.ToolTipText = _component.ToolTipText;
+                    base.Text = _component.Label;
+                    base.Image = _component.Icon;
+                    _component.AddListener(this);
+                }
+            }
+        }
+
+        protected override void OnClick(EventArgs e)
+        {
+            if (this.TargetComponent == null)
+                throw new InvalidOperationException("No target component assigned to this component");
+
+            this.TargetComponent.Invoke();
+
+            base.OnClick(e);
+        }
+
+        public void SetBusy(bool busy)
+        {
+            this.Enabled = !busy;
+        }
+    }
+}
