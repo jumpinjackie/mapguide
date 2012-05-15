06f24e2d6b09ab7f9658843c25a710fdcd7745bb
Jackie Ng <jumpinjackie@gmail.com>
2012-05-15 13:41:04 +0000

    Merge in r6650 and r6651
    git-svn-id: http://svn.osgeo.org/mapguide/trunk/MgDev@6652 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ Desktop/MapViewer/MgLoadMapComponent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel;
+
+namespace OSGeo.MapGuide.Viewer
+{
+    [ToolboxItem(true)]
+    public class MgLoadMapComponent : MgComponent
+    {
+        [Description("The map definition to load")]
+        public string MapDefinition
+        {
+            get;
+            set;
+        }
+
+        protected override bool RequiresLoadedMap
+        {
+            get
+            {
+                return false;
+            }
+        }
+
+        public override void Invoke()
+        {
+            var provider = this.Viewer.GetProvider();
+            var mdfId = new MgResourceIdentifier(this.MapDefinition);
+            var map = provider.CreateMap(mdfId, mdfId.GetName());
+            provider.LoadMap(map);
+        }
+    }
+}
