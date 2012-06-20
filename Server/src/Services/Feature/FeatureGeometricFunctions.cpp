c0fc1a6a229708cc8f0ce0de150d0ca35ba64443
Jackie Ng <jumpinjackie@gmail.com>
2012-06-20 11:13:45 +0000

    The EXTENT() function can be derailed by bad geometries (One particular Sheboygan Parcel comes to mind). This submission ensures such cases doesn't disrupt the computation of the result.
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.4/MgDev@6797 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Server/src/Services/Feature/FeatureGeometricFunctions.cpp
+++ Server/src/Services/Feature/FeatureGeometricFunctions.cpp
@@ -108,7 +108,16 @@ MgReader* MgFeatureGeometricFunctions::Execute()
     while(m_reader->ReadNext())
     {
         // Get the geometry
-        Ptr<MgGeometry> val = GetValue();
+        Ptr<MgGeometry> val;
+        try 
+        {
+            val = GetValue();
+        }
+        catch(MgException* ex) //Don't let a bad geometry derail us, just discard and continue
+        {
+            ex->Release();
+            continue;
+        }
 
         // Get the envelope
         Ptr<MgEnvelope> envl = val->Envelope();
