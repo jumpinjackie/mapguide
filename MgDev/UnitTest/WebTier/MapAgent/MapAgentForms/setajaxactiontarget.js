9e6be1cc6afda29630da56272ed28abf54718f40
Jackie Ng <jumpinjackie@gmail.com>
2012-07-06 15:31:55 +0000

    [https://trac.osgeo.org/mapguide/ticket/1299]: Do not hardcode "/mapguide". Instead follow on from that the split url gives us. This allows for virtual directory names other than "mapguide" to work.
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.4@6869 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/UnitTest/WebTier/MapAgent/MapAgentForms/setajaxactiontarget.js
+++ MgDev/UnitTest/WebTier/MapAgent/MapAgentForms/setajaxactiontarget.js
@@ -1,18 +1,18 @@
 function SetAjaxActionTarget()
 {
     var splitUrl = document.URL.split('/');
-    var url = splitUrl[0]+"//"+splitUrl[2];
+    var url = splitUrl[0]+"//"+splitUrl[2]+"/"+splitUrl[3];
 
     if (document.input.agent[0].checked)
     {
-        document.input.action = url+"/mapguide/mapviewerphp/ajaxviewer.php";
+        document.input.action = url+"/mapviewerphp/ajaxviewer.php";
     }
     else if (document.input.agent[1].checked)
     {
-        document.input.action = url+"/mapguide/mapviewernet/ajaxviewer.aspx";
+        document.input.action = url+"/mapviewernet/ajaxviewer.aspx";
     }
     else if (document.input.agent[2].checked)
     {
-        document.input.action = url+"/mapguide/mapviewerjava/ajaxviewer.jsp";
+        document.input.action = url+"/mapviewerjava/ajaxviewer.jsp";
     }
 }
