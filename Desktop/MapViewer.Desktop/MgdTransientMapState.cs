c7bc85021a4bd96e1d22f20ff3b7a2c501781bc4
Jackie Ng <jumpinjackie@gmail.com>
2012-07-10 12:25:43 +0000

    mg-desktop updates:  - #2062: Complete the Sample AppLayout extension with Custom Output and Digitizing/Redlining samples. Also clean up the startup message text.  - Fix incorrect cutoff number for Ctrl-Z during linestring/polygon digitization  - Add a new MgdTransientMapState class allowing for temporary display parameters (x,y,scale,width,height,dpi) to be applied to the current map on the viewer, allowing for custom views and sizes to be rendered off of this map without permanently changing state. The Custom Output sample demonstrates use of this class.
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.4/MgDev@6885 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ Desktop/MapViewer.Desktop/MgdTransientMapState.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OSGeo.MapGuide.Viewer.Desktop
+{
+    /// <summary>
+    /// Represents a stack of transient map states. This is used to manipulate the display parameters
+    /// of a <see cref="T:OSGeo.MapGuide.MgdMap"/> in a manner that can be undone upon disposal
+    /// of this stack, allowing your map to be used for <see cref="T:OSGeo.MapGuide.MgdRenderingService.RenderMap()"/>
+    /// calls without permanently altering state.
+    /// </summary>
+    public class MgdTransientMapState : IDisposable
+    {
+        private MgdMap _map;
+
+        private Stack<MgdMapStateTransition> _states;
+
+        private MgdMapDisplayParameters _origState;
+
+        public MgdTransientMapState(MgdMap map)
+        {
+            _map = map;
+            _origState = GetCurrentState();
+            _states = new Stack<MgdMapStateTransition>();
+        }
+
+        private MgdMapDisplayParameters GetCurrentState()
+        {
+            var pt = _map.ViewCenter;
+            var coord = pt.Coordinate;
+            return new MgdMapDisplayParameters(coord.X, coord.Y, _map.ViewScale, _map.DisplayWidth, _map.DisplayHeight, _map.DisplayDpi);
+        }
+
+        private void ApplyState(MgdMapDisplayParameters state)
+        {
+            _map.SetViewCenterXY(state.X, state.Y);
+            _map.SetViewScale(state.Scale);
+            _map.SetDisplaySize(state.Width, state.Height);
+            if (state.DPI.HasValue)
+                _map.DisplayDpi = state.DPI.Value;
+        }
+
+        public int Depth { get { return _states.Count; } }
+
+        /// <summary>
+        /// Pushes the given state onto the map state stack. The map takes on the display parameters specified
+        /// in this given state.
+        /// </summary>
+        /// <param name="state"></param>
+        public void PushState(MgdMapDisplayParameters state)
+        {
+            if (state == null)
+                throw new ArgumentNullException("state");
+
+            var oldState = GetCurrentState();
+            ApplyState(state);
+
+            _states.Push(new MgdMapStateTransition() { OldState = oldState, NewState = state });
+        }
+
+        /// <summary>
+        /// Pops the latest state from the map state stack. The map state is restored to
+        /// the previously applied state.
+        /// </summary>
+        /// <returns>The state that was previously applied</returns>
+        public MgdMapDisplayParameters PopState()
+        {
+            if (_states.Count == 0)
+                return null;
+
+            var trans = _states.Pop();
+            ApplyState(trans.OldState);
+            return trans.NewState;
+        }
+
+        public void Dispose()
+        {
+            while (_states.Count > 0)
+            {
+                this.PopState();
+            }
+            ApplyState(_origState);
+        }
+    }
+
+    internal class MgdMapStateTransition
+    {
+        public MgdMapDisplayParameters NewState { get; set; }
+
+        public MgdMapDisplayParameters OldState { get; set; }
+    }
+
+    /// <summary>
+    /// Represents display parameters for a map
+    /// </summary>
+    public class MgdMapDisplayParameters
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public double Scale { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+        public int? DPI { get; private set; }
+
+        public MgdMapDisplayParameters(double x, double y, double scale, int width, int height)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Scale = scale;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public MgdMapDisplayParameters(double x, double y, double scale, int width, int height, int dpi)
+            : this(x, y, scale, width, height)
+        {
+            this.DPI = dpi;
+        }
+    }
+}
--- Desktop/SampleExtension/CustomOutput.cs
+++ Desktop/SampleExtension/CustomOutput.cs
@@ -6,6 +6,9 @@ using System.Data;
 using System.Text;
 using System.Windows.Forms;
 using OSGeo.MapGuide.Viewer;
+using OSGeo.MapGuide.Viewer.Desktop;
+using OSGeo.MapGuide;
+using System.IO;
 
 namespace SampleExtension
 {
@@ -20,6 +23,224 @@ namespace SampleExtension
             _viewer = viewer;
         }
 
+        private void btnRenderMap1_Click(object sender, EventArgs e)
+        {
+            //Note we're casting to mg-desktop specific subclasses as this is what mg-desktop rendering APIs expect
+            MgdMap map = (MgdMap)_viewer.GetMap();
+            MgdSelection selection = (MgdSelection)_viewer.GetSelection();
+            MgMapViewerProvider provider = _viewer.GetProvider();
+            MgdRenderingService renderSvc = (MgdRenderingService)provider.CreateService(MgServiceType.RenderingService);
+            MgPoint centerPt = map.ViewCenter;
+            MgCoordinate centerCoord = centerPt.Coordinate;
 
+            //MgdTransientMapState is a helper class which lets us apply transient state to a map, which is automatically
+            //undone on disposal. This is how we can render custom views of a map with specific display parameters without
+            //permanently changing the display parameters used by the map viewer
+            using (MgdTransientMapState tempState = new MgdTransientMapState(map))
+            {
+                MgdMapDisplayParameters state = new MgdMapDisplayParameters(centerCoord.X, centerCoord.Y, 4000, 640, 480, 96);
+                tempState.PushState(state);
+
+                MgByteReader br = renderSvc.RenderMap(map, selection, "PNG");
+                using (SaveFileDialog save = new SaveFileDialog())
+                {
+                    save.Filter = "Portable Network Graphics (*.png)|*.png";
+                    if (save.ShowDialog() == DialogResult.OK)
+                    {
+                        //MgReadOnlyStream is a stream adapter class that provides a .net stream
+                        //interface to the MgByteReader, allowing MgByteReader objects to be used
+                        //anywhere a System.IO.Stream is expected.
+                        using (MgReadOnlyStream stream = new MgReadOnlyStream(br))
+                        {
+                            Image img = Image.FromStream(stream);
+                            img.Save(save.FileName);
+                            MessageBox.Show("Image saved to: " + save.FileName);
+                        }
+                    }
+                    br.Dispose();
+                }
+            }
+        }
+
+        private void btnRenderMap2_Click(object sender, EventArgs e)
+        {
+            //Note we're casting to mg-desktop specific subclasses as this is what mg-desktop rendering APIs expect
+            MgdMap map = (MgdMap)_viewer.GetMap();
+            MgdSelection selection = (MgdSelection)_viewer.GetSelection();
+            MgMapViewerProvider provider = _viewer.GetProvider();
+            MgdRenderingService renderSvc = (MgdRenderingService)provider.CreateService(MgServiceType.RenderingService);
+            MgPoint centerPt = map.ViewCenter;
+            MgCoordinate centerCoord = centerPt.Coordinate;
+
+            //MgdTransientMapState is a helper class which lets us apply transient state to a map, which is automatically
+            //undone on disposal. This is how we can render custom views of a map with specific display parameters without
+            //permanently changing the display parameters used by the map viewer
+            using (MgdTransientMapState tempState = new MgdTransientMapState(map))
+            {
+                MgdMapDisplayParameters state = new MgdMapDisplayParameters(centerCoord.X, centerCoord.Y, 5000, 1024, 768, 96);
+                tempState.PushState(state);
+
+                MgByteReader br = renderSvc.RenderMap(map, selection, "PNG");
+                using (SaveFileDialog save = new SaveFileDialog())
+                {
+                    save.Filter = "Portable Network Graphics (*.png)|*.png";
+                    if (save.ShowDialog() == DialogResult.OK)
+                    {
+                        //MgReadOnlyStream is a stream adapter class that provides a .net stream
+                        //interface to the MgByteReader, allowing MgByteReader objects to be used
+                        //anywhere a System.IO.Stream is expected.
+                        using (MgReadOnlyStream stream = new MgReadOnlyStream(br))
+                        {
+                            Image img = Image.FromStream(stream);
+                            img.Save(save.FileName);
+                            MessageBox.Show("Image saved to: " + save.FileName);
+                        }
+                    }
+                    br.Dispose();
+                }
+            }
+        }
+
+        private void btnRenderMap3_Click(object sender, EventArgs e)
+        {
+            //Note we're casting to mg-desktop specific subclasses as this is what mg-desktop rendering APIs expect
+            MgdMap map = (MgdMap)_viewer.GetMap();
+            MgdSelection selection = (MgdSelection)_viewer.GetSelection();
+            MgMapViewerProvider provider = _viewer.GetProvider();
+            MgdRenderingService renderSvc = (MgdRenderingService)provider.CreateService(MgServiceType.RenderingService);
+            MgPoint centerPt = map.ViewCenter;
+            MgCoordinate centerCoord = centerPt.Coordinate;
+
+            //MgdTransientMapState is a helper class which lets us apply transient state to a map, which is automatically
+            //undone on disposal. This is how we can render custom views of a map with specific display parameters without
+            //permanently changing the display parameters used by the map viewer
+            using (MgdTransientMapState tempState = new MgdTransientMapState(map))
+            {
+                MgdMapDisplayParameters state = new MgdMapDisplayParameters(centerCoord.X, centerCoord.Y, 6000, 1920, 1680, 192);
+                tempState.PushState(state);
+
+                MgByteReader br = renderSvc.RenderMap(map, selection, "PNG");
+                using (SaveFileDialog save = new SaveFileDialog())
+                {
+                    save.Filter = "Portable Network Graphics (*.png)|*.png";
+                    if (save.ShowDialog() == DialogResult.OK)
+                    {
+                        //MgReadOnlyStream is a stream adapter class that provides a .net stream
+                        //interface to the MgByteReader, allowing MgByteReader objects to be used
+                        //anywhere a System.IO.Stream is expected.
+                        using (MgReadOnlyStream stream = new MgReadOnlyStream(br))
+                        {
+                            Image img = Image.FromStream(stream);
+                            img.Save(save.FileName);
+                            MessageBox.Show("Image saved to: " + save.FileName);
+                        }
+                    }
+                    br.Dispose();
+                }
+            }
+        }
+
+        private void btnRenderMap4_Click(object sender, EventArgs e)
+        {
+            //Note we're casting to mg-desktop specific subclasses as this is what mg-desktop rendering APIs expect
+            MgdMap map = (MgdMap)_viewer.GetMap();
+            MgdSelection selection = (MgdSelection)_viewer.GetSelection();
+            MgMapViewerProvider provider = _viewer.GetProvider();
+            MgdRenderingService renderSvc = (MgdRenderingService)provider.CreateService(MgServiceType.RenderingService);
+            MgPoint centerPt = map.ViewCenter;
+            MgCoordinate centerCoord = centerPt.Coordinate;
+
+            //MgdTransientMapState is a helper class which lets us apply transient state to a map, which is automatically
+            //undone on disposal. This is how we can render custom views of a map with specific display parameters without
+            //permanently changing the display parameters used by the map viewer
+            using (MgdTransientMapState tempState = new MgdTransientMapState(map))
+            {
+                MgdMapDisplayParameters state = new MgdMapDisplayParameters(centerCoord.X, centerCoord.Y, 7000, 2048, 1920, 192);
+                tempState.PushState(state);
+
+                MgByteReader br = renderSvc.RenderMap(map, selection, "PNG");
+                using (SaveFileDialog save = new SaveFileDialog())
+                {
+                    save.Filter = "Portable Network Graphics (*.png)|*.png";
+                    if (save.ShowDialog() == DialogResult.OK)
+                    {
+                        //MgReadOnlyStream is a stream adapter class that provides a .net stream
+                        //interface to the MgByteReader, allowing MgByteReader objects to be used
+                        //anywhere a System.IO.Stream is expected.
+                        using (MgReadOnlyStream stream = new MgReadOnlyStream(br))
+                        {
+                            Image img = Image.FromStream(stream);
+                            img.Save(save.FileName);
+                            MessageBox.Show("Image saved to: " + save.FileName);
+                        }
+                    }
+                    br.Dispose();
+                }
+            }
+        }
+
+        private void btnSinglePlot_Click(object sender, EventArgs e)
+        {
+            MgdMap map = (MgdMap)_viewer.GetMap();
+            MgMapViewerProvider provider = _viewer.GetProvider();
+            MgdMappingService mappingSvc = (MgdMappingService)provider.CreateService(MgServiceType.MappingService);
+            MgResourceIdentifier layoutId = new MgResourceIdentifier("Library://Samples/Sheboygan/Layouts/SheboyganMap.PrintLayout");
+            using (SaveFileDialog save = new SaveFileDialog())
+            {
+                save.Filter = "DWF Files (*.dwf)|*.dwf";
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    MgDwfVersion dwfVer = new MgDwfVersion("6.01", "1.2");
+
+                    MgLayout layout = new MgLayout(layoutId, "City of Sheboygan", MgPageUnitsType.Inches);
+                    MgPlotSpecification plotSpec = new MgPlotSpecification(8.5f, 11.0f, MgPageUnitsType.Inches, 0.5f, 0.5f, 0.5f, 0.5f);
+
+                    MgByteReader result = mappingSvc.GeneratePlot(map, plotSpec, layout, dwfVer);
+                    MgByteSink sink = new MgByteSink(result);
+                    sink.ToFile(save.FileName);
+
+                    MessageBox.Show("Saved to " + save.FileName);
+                }
+            }
+        }
+
+        private void btnMultiPlot_Click(object sender, EventArgs e)
+        {
+            MgdMap map = (MgdMap)_viewer.GetMap();
+            MgMapViewerProvider provider = _viewer.GetProvider();
+            MgdMappingService mappingSvc = (MgdMappingService)provider.CreateService(MgServiceType.MappingService);
+            MgResourceIdentifier layoutId = new MgResourceIdentifier("Library://Samples/Sheboygan/Layouts/SheboyganMap.PrintLayout");
+            using (SaveFileDialog save = new SaveFileDialog())
+            {
+                save.Filter = "DWF Files (*.dwf)|*.dwf";
+                if (save.ShowDialog() == DialogResult.OK)
+                {
+                    MgDwfVersion dwfVer = new MgDwfVersion("6.01", "1.2");
+
+                    MgLayout layout = new MgLayout(layoutId, "City of Sheboygan", MgPageUnitsType.Inches);
+                    MgPlotSpecification plotSpec = new MgPlotSpecification(8.5f, 11.0f, MgPageUnitsType.Inches, 0.5f, 0.5f, 0.5f, 0.5f);
+
+                    MgMapPlotCollection plotCollection = new MgMapPlotCollection();
+
+                    MgMapPlot plot1 = new MgMapPlot(map, plotSpec, layout);
+                    plot1.SetCenterAndScale(map.GetViewCenter().GetCoordinate(), map.GetViewScale() * 2);
+                    plotCollection.Add(plot1);
+
+                    // Create a second map for the second sheet in the DWF. This second sheet uses the print layout
+                    // to display a page title and legend.
+
+                    MgdMap map2 = new MgdMap(map.GetMapDefinition(), "Sheet 2");
+                    MgMapPlot plot2 = new MgMapPlot(map2, plotSpec, layout);
+                    plot2.SetCenterAndScale(map.GetViewCenter().GetCoordinate(), map.GetViewScale());
+                    plotCollection.Add(plot2);
+
+                    MgByteReader result = mappingSvc.GenerateMultiPlot(plotCollection, dwfVer);
+                    MgByteSink sink = new MgByteSink(result);
+                    sink.ToFile(save.FileName);
+
+                    MessageBox.Show("Saved to " + save.FileName);
+                }
+            }
+        }
     }
 }
