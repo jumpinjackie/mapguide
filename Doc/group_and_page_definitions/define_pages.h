b1f89dc9883e8f5c0156a98101fb0b73c7d0848e
Jackie Ng <jumpinjackie@gmail.com>
2012-08-29 18:16:56 +0000

    #2097: Make parts of the API reference content conditional. This allows us to have a common base API reference with conditional bits of content for MapGuide and mg-desktop (in the future). Re-usable documentation ftw!
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.4/MgDev@6961 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Doc/group_and_page_definitions/define_pages.h
+++ Doc/group_and_page_definitions/define_pages.h
@@ -3,22 +3,28 @@
 <!-- This is the index.html page that the user first sees. -->
 
 \section summary \ProdName API Reference
-
+\if MAPGUIDEAPI
 For detailed information about using the MapGuide API see the \DevGuide.
-
+\n\n
+\endif
 Maps are made up of layers, where each layer displays features from a feature class. See
 \ref Maps_and_Layers_Module "Maps and Layers" for more details.
-
+\n\n
 Much of the work with this API is done through the following services:
 
  \li \ref Resource_Service_Module "Resource Service": manipulates resources and repositories.
  \li \ref Feature_Service_Module "Feature Service": provides access to FDO providers.
+\if DESKTOPAPI
+ \li \ref Desktop_Module "Desktop Platform": Contains classes specific to mg-desktop and extensions to the core MapGuide classes to allow for use in Desktop Applications
+\endif
+\if MAPGUIDEAPI
  \li \ref Rendering_Service_Module "Rendering Service": renders a map into a bitmapped image.
  \li \ref Drawing_Service_Module "Drawing Service": allows low level access to drawing sources (DWF data).
  \li \ref Mapping_Service_Module "Mapping Service": generates eMaps and ePlots for viewing with the DWF Viewer.
  \li \ref Profiling_Service_Module "Profiling Service": Profiles map and layer rendering for performance data.
  \li \ref Site_Service_Module "Site Service": configures users, groups, and user sessions.
  \li \ref Tile_Service_Module "Tile Service": supports smooth pans and zooms in the AJAX Viewer.
+\endif
 
 Some other important areas of the API are:
 
