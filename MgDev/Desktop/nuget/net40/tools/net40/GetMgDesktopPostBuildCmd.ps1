ebbe8e469a15d7668b3fd24701dcf30063906142
Jackie Ng <jumpinjackie@gmail.com>
2012-08-30 16:14:43 +0000

    Add nuget skeleton for mg-desktop
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.4@6965 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ MgDev/Desktop/nuget/net40/tools/net40/GetMgDesktopPostBuildCmd.ps1
@@ -0,0 +1,14 @@
+﻿$solutionDir = [System.IO.Path]::GetDirectoryName($dte.Solution.FullName) + "\"
+$path = $installPath.Replace($solutionDir, "`$(SolutionDir)")
+
+$MgDesktopPostBuildCmd = "
+if not exist `"`$(TargetDir)FDO`" md `"`$(TargetDir)FDO`"
+xcopy /s /y `"`$(ProjectDir)mg-desktop\FDO`" `"`$(TargetDir)FDO`"
+if not exist `"`$(TargetDir)Dictionaries`" md `"`$(TargetDir)Dictionaries`"
+xcopy /s /y `"`$(ProjectDir)mg-desktop\Dictionaries`" `"`$(TargetDir)Dictionaries`"
+if not exist `"`$(TargetDir)Resources`" md `"`$(TargetDir)Resources`"
+xcopy /s /y `"`$(ProjectDir)mg-desktop\Resources`" `"`$(TargetDir)Resources`"
+if not exist `"`$(TargetDir)Schema`" md `"`$(TargetDir)Schema`"
+xcopy /s /y `"`$(ProjectDir)mg-desktop\Schema`" `"`$(TargetDir)Schema`"
+copy /y `"`$(ProjectDir)mg-desktop\*.dll`" `"`$(TargetDir)`"
+copy /y `"`$(ProjectDir)mg-desktop\Platform.ini`" `"`$(TargetDir)`""
