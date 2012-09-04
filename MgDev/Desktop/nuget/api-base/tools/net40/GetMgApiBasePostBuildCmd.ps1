d6c00e01c31ceab9d8b6728980ca2e81f76cd3f4
Jackie Ng <jumpinjackie@gmail.com>
2012-09-04 14:33:10 +0000

    Split the nuget package into several sub-packages
     - mapguide-api-base: Foundation, Geometry, PlatformBase and a subset of CS-Map dictionaries
     - mg-desktop-net40: mg-desktop components (non-Viewer)
     - mg-desktop-viewer-net40: mg-desktop viewer component
    
    This should make uploading to nuget.org much easier.
    
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.4@6982 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ MgDev/Desktop/nuget/api-base/tools/net40/GetMgApiBasePostBuildCmd.ps1
@@ -0,0 +1,7 @@
+﻿$solutionDir = [System.IO.Path]::GetDirectoryName($dte.Solution.FullName) + "\"
+$path = $installPath.Replace($solutionDir, "`$(SolutionDir)")
+
+$MgApiBasePostBuildCmd = "
+if not exist `"`$(TargetDir)Dictionaries`" md `"`$(TargetDir)Dictionaries`"
+xcopy /s /y `"`$(ProjectDir)mapguide-api-base\Dictionaries`" `"`$(TargetDir)Dictionaries`"
+copy /y `"`$(ProjectDir)mapguide-api-base\*.dll`" `"`$(TargetDir)`""
--- MgDev/Desktop/nuget/api-desktop/tools/net40/GetMgDesktopPostBuildCmd.ps1
+++ MgDev/Desktop/nuget/api-desktop/tools/net40/GetMgDesktopPostBuildCmd.ps1
@@ -4,8 +4,6 @@ $path = $installPath.Replace($solutionDir, "`$(SolutionDir)")
 $MgDesktopPostBuildCmd = "
 if not exist `"`$(TargetDir)FDO`" md `"`$(TargetDir)FDO`"
 xcopy /s /y `"`$(ProjectDir)mg-desktop\FDO`" `"`$(TargetDir)FDO`"
-if not exist `"`$(TargetDir)Dictionaries`" md `"`$(TargetDir)Dictionaries`"
-xcopy /s /y `"`$(ProjectDir)mg-desktop\Dictionaries`" `"`$(TargetDir)Dictionaries`"
 if not exist `"`$(TargetDir)Resources`" md `"`$(TargetDir)Resources`"
 xcopy /s /y `"`$(ProjectDir)mg-desktop\Resources`" `"`$(TargetDir)Resources`"
 if not exist `"`$(TargetDir)Schema`" md `"`$(TargetDir)Schema`"
