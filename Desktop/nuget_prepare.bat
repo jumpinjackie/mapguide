de79b70f7a7ef332663f6ad8bedf9407148bf259
Jackie Ng <jumpinjackie@gmail.com>
2012-09-04 14:34:20 +0000

    Modify nuget scripts to handle the new sub-package layout
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.4/MgDev@6983 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Desktop/nuget_prepare.bat
+++ Desktop/nuget_prepare.bat
@@ -6,19 +6,26 @@ rem
 rem You must call setenvironment[64].bat first with any appropriate
 rem parameters before calling this script
 
-IF "%NUGET_TARGET%" == "" SET NUGET_TARGET=net40
-SET NUGET_CONTENT=%CD%\nuget\%NUGET_TARGET%\content\%NUGET_TARGET%\mg-desktop
-SET NUGET_LIB=%CD%\nuget\%NUGET_TARGET%\lib\%NUGET_TARGET%
+SET NUGET_TARGET=net40
+SET NUGET_CONTENT_BASE=%CD%\nuget\api-base\content\%NUGET_TARGET%\mapguide-api-base
+SET NUGET_LIB_BASE=%CD%\nuget\api-base\lib\%NUGET_TARGET%
+SET NUGET_CONTENT_DESKTOP=%CD%\nuget\api-desktop\content\%NUGET_TARGET%\mg-desktop
+SET NUGET_LIB_DESKTOP=%CD%\nuget\api-desktop\lib\%NUGET_TARGET%
+SET NUGET_CONTENT_VIEWER=%CD%\nuget\api-desktop-viewer\content\%NUGET_TARGET%\mg-desktop-viewer
+SET NUGET_LIB_VIEWER=%CD%\nuget\api-desktop-viewer\lib\%NUGET_TARGET%
 
-echo [install]: Copy files to nuget staging
-%XCOPY% "bin\%TYPEBUILD%\FDO" "%NUGET_CONTENT%\FDO" /EXCLUDE:package_excludes.txt
-%XCOPY% "bin\%TYPEBUILD%\Dictionaries" "%NUGET_CONTENT%\Dictionaries" /EXCLUDE:package_excludes.txt
-%XCOPY% "bin\%TYPEBUILD%\Resources" "%NUGET_CONTENT%\Resources" /EXCLUDE:package_excludes.txt
-%XCOPY% "bin\%TYPEBUILD%\Schema" "%NUGET_CONTENT%\Schema" /EXCLUDE:package_excludes.txt
-xcopy /y "bin\%TYPEBUILD%\*.dll" "%NUGET_CONTENT%" /EXCLUDE:dll_excludes.txt
-copy /y "bin\%TYPEBUILD%\Platform.ini" "%NUGET_CONTENT%"
-copy /y "bin\%TYPEBUILD%\OSGeo.*.dll" "%NUGET_LIB%"
-copy /y "bin\%TYPEBUILD%\OSGeo.MapGuide.Viewer.xml" "%NUGET_LIB%"
+echo [install]: Copy api-base files to nuget staging
+%XCOPY% "bin\%TYPEBUILD%\*" "%NUGET_CONTENT_BASE%" /EXCLUDE:package_excludes.txt+base_package_excludes.txt
+copy /y "bin\%TYPEBUILD%\OSGeo.MapGuide.Foundation.dll" "%NUGET_LIB_BASE%"
+copy /y "bin\%TYPEBUILD%\OSGeo.MapGuide.Geometry.dll" "%NUGET_LIB_BASE%"
+copy /y "bin\%TYPEBUILD%\OSGeo.MapGuide.PlatformBase.dll" "%NUGET_LIB_BASE%"
+echo [install]: Copy api-desktop files to nuget staging
+%XCOPY% "bin\%TYPEBUILD%\*" "%NUGET_CONTENT_DESKTOP%" /EXCLUDE:package_excludes.txt+desktop_package_excludes.txt
+copy /y "bin\%TYPEBUILD%\OSGeo.MapGuide.Desktop.dll" "%NUGET_LIB_DESKTOP%"
+echo [install]: Copy api-desktop-viewer files to nuget staging
+copy /y "bin\%TYPEBUILD%\OSGeo.MapGuide.Viewer.xml" "%NUGET_LIB_VIEWER%"
+copy /y "bin\%TYPEBUILD%\OSGeo.MapGuide.Viewer.dll" "%NUGET_LIB_VIEWER%"
+copy /y "bin\%TYPEBUILD%\OSGeo.MapGuide.Viewer.Desktop.dll" "%NUGET_LIB_VIEWER%"
 echo mg-desktop is ready for nuget packaging and publishing
 goto quit
 
