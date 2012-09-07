69d8fb9e7346621019ad0a371e0166654fd885cb
Jackie Ng <jumpinjackie@gmail.com>
2012-09-07 16:59:40 +0000

    Build the fusion API docs and include them in the output dir for installer packing
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.4@6986 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/build_fusion.bat
+++ MgDev/build_fusion.bat
@@ -1,5 +1,9 @@
 @echo off
 echo [build]: Web Tier - fusion templates
 %XCOPY% "%MG_OEM%\fusionMG" "%MG_OEM%\fusion_build" /EXCLUDE:svn_excludes.txt+%CONFIGURATION%_excludes.txt
+echo [build]: Web Tier - Clean fusion
+call %ANT% clean -f "%MG_OEM%\fusion\build.xml"
+echo [build]: Web Tier - Build Fusion API docs
+call %ANT% docs -f "%MG_OEM%\fusion\build.xml"
 echo [build]: Web Tier - fusion
 call %ANT% deploy -Ddeploy.home="%MG_OEM%\fusion_build" -f "%MG_OEM%\fusion\build.xml"
