1d9f96a4398ee3261a5b458b6cf8fa95400082b7
Jackie Ng <jumpinjackie@gmail.com>
2012-10-09 15:36:26 +0000

    Nuke trunk Desktop directory which is way too old now. Too complex to merge all changes from 2.4 branch. Just resume from 2.4's Desktop dir instead, which we'll put in after this submission.
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@7099 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Desktop/DesktopTestData/UT_Unsortable_Right.fs
+++
@@ -1,8 +0,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<FeatureSource xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xsi:noNamespaceSchemaLocation="FeatureSource-1.0.0.xsd">
-  <Provider>OSGeo.SDF</Provider>
-  <Parameter>
-    <Name>File</Name>
-    <Value>%MG_DATA_FILE_PATH%UnsortableRight.sdf</Value>
-  </Parameter>
-</FeatureSource>
