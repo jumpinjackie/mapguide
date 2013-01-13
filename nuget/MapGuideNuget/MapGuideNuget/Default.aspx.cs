c6d66498204a7a74011a1f1141a82c903fc59e7f
Jackie Ng <jumpinjackie@gmail.com>
2013-01-13 13:01:56 +0000

    Add a set of test applications (VS2012, .net 4.0) for testing our nuget packages
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.5/MgDev@7311 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ nuget/MapGuideNuget/MapGuideNuget/Default.aspx.cs
@@ -0,0 +1,23 @@
+﻿using OSGeo.MapGuide;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MapGuideNuget
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            MgUserInformation user = new MgUserInformation("Anonymous", "");
+            MgSite site = new MgSite();
+            site.Open(user);
+            string sessionId = site.CreateSession();
+            string wl = "Library://Samples/Sheboygan/Layouts/SheboyganAsp.WebLayout";
+            Response.Redirect("http://localhost/mapguide/mapviewerajax/?WEBLAYOUT=" + wl + "&SESSION=" + sessionId);
+        }
+    }
+}
