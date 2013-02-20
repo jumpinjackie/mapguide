5dd872bfa824321df0a0dc83c3cf0b070149c703
Jackie Ng <jumpinjackie@gmail.com>
2013-02-20 10:56:47 +0000

    [https://trac.osgeo.org/mapguide/ticket/237]: Undo r7359
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.5@7375 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Installer/scripts/build_oem_ubuntu.sh
+++ Installer/scripts/build_oem_ubuntu.sh
@@ -235,9 +235,7 @@ init_php()
 build_php()
 {
     pushd php
-    # #237: Include unixODBC support for PHP
-    # Most distros will have unixODBC headers under /usr instead of php's assumed default [/usr/local]
-    sh ./configure --with-unixODBC=/usr --with-openssl --with-curl --enable-mbstring --with-zlib
+    sh ./configure
     check_build
     popd
 }
