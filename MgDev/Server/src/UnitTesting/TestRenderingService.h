016f1b3439e50358e1f51091ab7cb5fe01740a4b
Jackie Ng <jumpinjackie@gmail.com>
2013-11-26 06:36:14 +0000

    [https://trac.osgeo.org/mapguide/ticket/2380]: Update MgLegendPlotUtil to skip over empty layer groups (including children) when rendering a legend. This will reduce clutter if rendering a legend for a map that has lots of visible groups that have no visible layers in them. Previously, such empty groups are included. With this submission, such groups are omitted from the final image.
    Includes unit tests to exercise such cases.
    
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@7929 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Server/src/UnitTesting/TestRenderingService.h
+++ MgDev/Server/src/UnitTesting/TestRenderingService.h
@@ -42,6 +42,7 @@ class TestRenderingService : public CppUnit::TestFixture
     CPPUNIT_TEST(TestCase_RenderMapPNG);
     CPPUNIT_TEST(TestCase_RenderMapWithWatermarkPNG);
     CPPUNIT_TEST(TestCase_RenderLegendPNG);
+    CPPUNIT_TEST(TestCase_RenderLegendEmptyGroupsPNG);
     CPPUNIT_TEST(TestCase_RenderLegendMultiFTSPNG);
     CPPUNIT_TEST(TestCase_RenderLegendMultiCTSPNG);
     CPPUNIT_TEST(TestCase_RenderLegendMultiFTSWithThemePNG);
@@ -51,7 +52,7 @@ class TestRenderingService : public CppUnit::TestFixture
 
     CPPUNIT_TEST(TestCase_StylizationFunctionsPNG8);
 
-	CPPUNIT_TEST(TestCase_SymbologyPointsPNG8);
+    CPPUNIT_TEST(TestCase_SymbologyPointsPNG8);
     CPPUNIT_TEST(TestCase_SymbologyPointsParamPNG8);
     CPPUNIT_TEST(TestCase_SymbologyLinesPNG8);
     CPPUNIT_TEST(TestCase_SymbologyLinesCrossTickPNG8);
@@ -66,6 +67,7 @@ class TestRenderingService : public CppUnit::TestFixture
     CPPUNIT_TEST(TestCase_RenderMapPNG8);
     CPPUNIT_TEST(TestCase_RenderMapWithWatermarkPNG8);
     CPPUNIT_TEST(TestCase_RenderLegendPNG8);
+    CPPUNIT_TEST(TestCase_RenderLegendEmptyGroupsPNG8);
     CPPUNIT_TEST(TestCase_RenderLegendMultiFTSPNG8);
     CPPUNIT_TEST(TestCase_RenderLegendMultiCTSPNG8);
     CPPUNIT_TEST(TestCase_RenderLegendMultiFTSWithThemePNG8);
@@ -75,7 +77,7 @@ class TestRenderingService : public CppUnit::TestFixture
 
     CPPUNIT_TEST(TestCase_StylizationFunctionsGIF);
 
-	CPPUNIT_TEST(TestCase_SymbologyPointsGIF);
+    CPPUNIT_TEST(TestCase_SymbologyPointsGIF);
     CPPUNIT_TEST(TestCase_SymbologyPointsParamGIF);
     CPPUNIT_TEST(TestCase_SymbologyLinesGIF);
     CPPUNIT_TEST(TestCase_SymbologyLinesCrossTickGIF);
@@ -90,6 +92,7 @@ class TestRenderingService : public CppUnit::TestFixture
     CPPUNIT_TEST(TestCase_RenderMapGIF);
     CPPUNIT_TEST(TestCase_RenderMapWithWatermarkGIF);
     CPPUNIT_TEST(TestCase_RenderLegendGIF);
+    CPPUNIT_TEST(TestCase_RenderLegendEmptyGroupsGIF);
     CPPUNIT_TEST(TestCase_RenderLegendMultiFTSGIF);
     CPPUNIT_TEST(TestCase_RenderLegendMultiCTSGIF);
     CPPUNIT_TEST(TestCase_RenderLegendMultiFTSWithThemeGIF);
@@ -99,7 +102,7 @@ class TestRenderingService : public CppUnit::TestFixture
 
     CPPUNIT_TEST(TestCase_StylizationFunctionsJPG);
 
-	CPPUNIT_TEST(TestCase_SymbologyPointsJPG);
+    CPPUNIT_TEST(TestCase_SymbologyPointsJPG);
     CPPUNIT_TEST(TestCase_SymbologyPointsParamJPG);
     CPPUNIT_TEST(TestCase_SymbologyLinesJPG);
     CPPUNIT_TEST(TestCase_SymbologyLinesCrossTickJPG);
@@ -114,6 +117,7 @@ class TestRenderingService : public CppUnit::TestFixture
     CPPUNIT_TEST(TestCase_RenderMapJPG);
     CPPUNIT_TEST(TestCase_RenderMapWithWatermarkJPG);
     CPPUNIT_TEST(TestCase_RenderLegendJPG);
+    CPPUNIT_TEST(TestCase_RenderLegendEmptyGroupsJPG);
     CPPUNIT_TEST(TestCase_RenderLegendMultiFTSJPG);
     CPPUNIT_TEST(TestCase_RenderLegendMultiCTSJPG);
     CPPUNIT_TEST(TestCase_RenderLegendMultiFTSWithThemeJPG);
@@ -137,14 +141,15 @@ public:
     void TestStart();
     void TestEnd();
 
-	void TestCase_QueryFeatures();
+    void TestCase_QueryFeatures();
 
-	//Parameterized versions that all format-specific tests call into
+    //Parameterized versions that all format-specific tests call into
     void TestCase_RenderDynamicOverlay(CREFSTRING imageFormat, CREFSTRING extension);
     void TestCase_RenderDynamicOverlayTiledMap(CREFSTRING imageFormat, CREFSTRING extension);
     void TestCase_RenderMap(CREFSTRING imageFormat, CREFSTRING extension);
     void TestCase_RenderMapWithWatermark(CREFSTRING imageFormat, CREFSTRING extension);
     void TestCase_RenderLegend(CREFSTRING imageFormat, CREFSTRING extension);
+    void TestCase_RenderLegendEmptyGroups(CREFSTRING imageFormat, CREFSTRING extension);
     void TestCase_RenderLegendMultiFTS(CREFSTRING imageFormat, CREFSTRING extension);
     void TestCase_RenderLegendMultiCTS(CREFSTRING imageFormat, CREFSTRING extension);
     void TestCase_RenderLegendMultiFTSWithTheme(CREFSTRING imageFormat, CREFSTRING extension);
@@ -164,12 +169,13 @@ public:
 
     void TestCase_StylizationFunctions(CREFSTRING imageFormat, CREFSTRING extension);
 
-	//PNG output tests
-	void TestCase_RenderDynamicOverlayPNG() { TestCase_RenderDynamicOverlay(L"PNG", L"png"); }
+    //PNG output tests
+    void TestCase_RenderDynamicOverlayPNG() { TestCase_RenderDynamicOverlay(L"PNG", L"png"); }
     void TestCase_RenderDynamicOverlayTiledMapPNG() { TestCase_RenderDynamicOverlayTiledMap(L"PNG", L"png"); }
     void TestCase_RenderMapPNG() { TestCase_RenderMap(L"PNG", L"png"); }
     void TestCase_RenderMapWithWatermarkPNG() { TestCase_RenderMapWithWatermark(L"PNG", L"png"); }
     void TestCase_RenderLegendPNG() { TestCase_RenderLegend(L"PNG", L"png"); }
+    void TestCase_RenderLegendEmptyGroupsPNG() { TestCase_RenderLegendEmptyGroups(L"PNG", L"png"); }
     void TestCase_SymbologyPointsPNG() { TestCase_SymbologyPoints(L"PNG", L"png"); }
     void TestCase_SymbologyPointsParamPNG() { TestCase_SymbologyPointsParam(L"PNG", L"png"); }
     void TestCase_SymbologyLinesPNG() { TestCase_SymbologyLines(L"PNG", L"png"); }
@@ -186,12 +192,13 @@ public:
     void TestCase_RenderLegendSingleFTSMultiCTSPNG() { TestCase_RenderLegendSingleFTSMultiCTS(L"PNG", L"png"); }
     void TestCase_StylizationFunctionsPNG() { TestCase_StylizationFunctions(L"PNG", L"png"); }
 
-	//PNG8 output tests
-	void TestCase_RenderDynamicOverlayPNG8() { TestCase_RenderDynamicOverlay(L"PNG8", L"png"); }
+    //PNG8 output tests
+    void TestCase_RenderDynamicOverlayPNG8() { TestCase_RenderDynamicOverlay(L"PNG8", L"png"); }
     void TestCase_RenderDynamicOverlayTiledMapPNG8() { TestCase_RenderDynamicOverlayTiledMap(L"PNG8", L"png"); }
     void TestCase_RenderMapPNG8() { TestCase_RenderMap(L"PNG8", L"png"); }
     void TestCase_RenderMapWithWatermarkPNG8() { TestCase_RenderMapWithWatermark(L"PNG8", L"png"); }
     void TestCase_RenderLegendPNG8() { TestCase_RenderLegend(L"PNG8", L"png"); }
+    void TestCase_RenderLegendEmptyGroupsPNG8() { TestCase_RenderLegendEmptyGroups(L"PNG8", L"png"); }
     void TestCase_SymbologyPointsPNG8() { TestCase_SymbologyPoints(L"PNG8", L"png"); }
     void TestCase_SymbologyPointsParamPNG8() { TestCase_SymbologyPointsParam(L"PNG8", L"png"); }
     void TestCase_SymbologyLinesPNG8() { TestCase_SymbologyLines(L"PNG8", L"png"); }
@@ -208,12 +215,13 @@ public:
     void TestCase_RenderLegendSingleFTSMultiCTSPNG8() { TestCase_RenderLegendSingleFTSMultiCTS(L"PNG8", L"png"); }
     void TestCase_StylizationFunctionsPNG8() { TestCase_StylizationFunctions(L"PNG8", L"png"); }
 
-	//GIF output tests
-	void TestCase_RenderDynamicOverlayGIF() { TestCase_RenderDynamicOverlay(L"GIF", L"gif"); }
+    //GIF output tests
+    void TestCase_RenderDynamicOverlayGIF() { TestCase_RenderDynamicOverlay(L"GIF", L"gif"); }
     void TestCase_RenderDynamicOverlayTiledMapGIF() { TestCase_RenderDynamicOverlayTiledMap(L"GIF", L"gif"); }
     void TestCase_RenderMapGIF() { TestCase_RenderMap(L"GIF", L"gif"); }
     void TestCase_RenderMapWithWatermarkGIF() { TestCase_RenderMapWithWatermark(L"GIF", L"gif"); }
     void TestCase_RenderLegendGIF() { TestCase_RenderLegend(L"GIF", L"gif"); }
+    void TestCase_RenderLegendEmptyGroupsGIF() { TestCase_RenderLegendEmptyGroups(L"GIF", L"gif"); }
     void TestCase_SymbologyPointsGIF() { TestCase_SymbologyPoints(L"GIF", L"gif"); }
     void TestCase_SymbologyPointsParamGIF() { TestCase_SymbologyPointsParam(L"GIF", L"gif"); }
     void TestCase_SymbologyLinesGIF() { TestCase_SymbologyLines(L"GIF", L"gif"); }
@@ -230,12 +238,13 @@ public:
     void TestCase_RenderLegendSingleFTSMultiCTSGIF() { TestCase_RenderLegendSingleFTSMultiCTS(L"GIF", L"gif"); }
     void TestCase_StylizationFunctionsGIF() { TestCase_StylizationFunctions(L"GIF", L"gif"); }
 
-	//JPG output tests
-	void TestCase_RenderDynamicOverlayJPG() { TestCase_RenderDynamicOverlay(L"JPG", L"jpg"); }
+    //JPG output tests
+    void TestCase_RenderDynamicOverlayJPG() { TestCase_RenderDynamicOverlay(L"JPG", L"jpg"); }
     void TestCase_RenderDynamicOverlayTiledMapJPG() { TestCase_RenderDynamicOverlayTiledMap(L"JPG", L"jpg"); }
     void TestCase_RenderMapJPG() { TestCase_RenderMap(L"JPG", L"jpg"); }
     void TestCase_RenderMapWithWatermarkJPG() { TestCase_RenderMapWithWatermark(L"JPG", L"jpg"); }
     void TestCase_RenderLegendJPG() { TestCase_RenderLegend(L"JPG", L"jpg"); }
+    void TestCase_RenderLegendEmptyGroupsJPG() { TestCase_RenderLegendEmptyGroups(L"JPG", L"jpg"); }
     void TestCase_SymbologyPointsJPG() { TestCase_SymbologyPoints(L"JPG", L"jpg"); }
     void TestCase_SymbologyPointsParamJPG() { TestCase_SymbologyPointsParam(L"JPG", L"jpg"); }
     void TestCase_SymbologyLinesJPG() { TestCase_SymbologyLines(L"JPG", L"jpg"); }
@@ -260,7 +269,7 @@ private:
     MgMap* CreateTestStylizationFunctionMap();
     MgMap* CreateTestMapWithWatermark();
     MgPolygon* CreateSelectionPolygon(MgMap* map, double width, double height);
-	static STRING GetPath(CREFSTRING basePath, CREFSTRING imageFormat, CREFSTRING extension);
+    static STRING GetPath(CREFSTRING basePath, CREFSTRING imageFormat, CREFSTRING extension);
 
 private:
     Ptr<MgSiteConnection> m_siteConnection;
