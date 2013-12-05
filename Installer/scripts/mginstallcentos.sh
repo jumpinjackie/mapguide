e949750445d7873c560224ce64089d9bdfd7b195
Jackie Ng <jumpinjackie@gmail.com>
2013-12-05 10:31:52 +0000

    Update CentOS install script for 2.5.2
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.5@7952 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Installer/scripts/mginstallcentos.sh
+++ Installer/scripts/mginstallcentos.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-URL="http://download.osgeo.org/mapguide/releases/2.5.1"
-#URL="http://192.168.0.7/downloads/2.5.1"
+URL="http://download.osgeo.org/mapguide/releases/2.5.2"
+#URL="http://192.168.0.10/downloads"
 FDOVER_MAJOR_MINOR=3.8
 FDOVER_MAJOR_MINOR_REV=${FDOVER_MAJOR_MINOR}.0
-FDOVER=${FDOVER_MAJOR_MINOR_REV}_3601
+FDOVER=${FDOVER_MAJOR_MINOR_REV}_6957
 MGVER_MAJOR_MINOR=2.5
-MGVER_MAJOR_MINOR_REV=${MGVER_MAJOR_MINOR}.1
-MGVER=${MGVER_MAJOR_MINOR_REV}.7768
-FDO_TARBALL=fdosdk-centos5-${FDOVER}.tar.gz
+MGVER_MAJOR_MINOR_REV=${MGVER_MAJOR_MINOR}.2
+MGVER=${MGVER_MAJOR_MINOR_REV}.7949
+FDO_TARBALL=fdosdk-centos6-${FDOVER}.tar.gz
 MG_TARBALL=mapguideopensource-${MGVER}.tar.gz
 MG_URL=${URL}/${MG_TARBALL}
-FDO_URL="http://download.osgeo.org/fdo/3.8.0/release/binaries/linux32/${FDO_TARBALL}"
-#FDO_URL=${URL}/${FDO_TARBALL}
+#FDO_URL="http://download.osgeo.org/fdo/${FDOVER_MAJOR_MINOR_REV}/release/binaries/linux32/${FDO_TARBALL}"
+FDO_URL=${URL}/${FDO_TARBALL}
 
 # Must have root
 if [[ $EUID -ne 0 ]]; then
@@ -23,9 +23,11 @@ if [ ! -f ${FDO_TARBALL} ]; then
 wget -N ${FDO_URL}
 fi
 
-#tar -C / -zxvf ${FDO_TARBALL}
-mkdir -p /usr/local/fdo-3.8.0
-tar -C /usr/local/fdo-3.8.0/ -zxvf ${FDO_TARBALL}
+# Our FDO tarball requires extraction to root
+tar -C / -zxvf ${FDO_TARBALL}
+mkdir -p /usr/local/fdo-${FDOVER_MAJOR_MINOR_REV}
+# The FDO tarball requires extraction to the expected dir
+#tar -C /usr/local/fdo-${FDOVER_MAJOR_MINOR_REV}/ -zxvf ${FDO_TARBALL}
 
 if [ ! -f ${MG_TARBALL} ]; then
 wget -N ${MG_URL}
