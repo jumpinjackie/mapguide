c1e720eecfed5ada3054e54f13e9ecd1bd0cd2fb
Jackie Ng <jumpinjackie@gmail.com>
2014-03-03 23:29:44 +0000

    vagrant is a batch file, so we must use call syntax in order for execution to continue
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@7986 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Tools/Vagrant/build.bat
+++ Tools/Vagrant/build.bat
@@ -10,17 +10,19 @@ pushd ubuntu\x86
 SET COMPONENT=Ubuntu Build
 echo [build]: MapGuide Ubuntu build
 if exist build rd /S /Q build
-vagrant up
+call vagrant up
+echo [build]: vagrant returned %errorlevel%
 if "%errorlevel%"=="1" goto error
-vagrant destroy -f
+call vagrant destroy -f
 popd
 pushd centos\x86
 SET COMPONENT=CentOS Build
 echo [build]: MapGuide CentOS build
 if exist build rd /S /Q build
-vagrant up
+call vagrant up
+echo [build]: vagrant returned %errorlevel%
 if "%errorlevel%"=="1" goto error
-vagrant destroy -f
+call vagrant destroy -f
 popd
 goto quit
 :error
