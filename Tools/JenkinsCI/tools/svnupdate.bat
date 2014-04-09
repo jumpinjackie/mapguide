89c442f65444d9f74f6241cd8c17dc2df229c88e
Jackie Ng <jumpinjackie@gmail.com>
2014-04-09 15:39:35 +0000

    Build job updates to prepare for 2.6 release cycle. Main change is pre-stamp the revision files after a batch svn update so that there is no lock contention on the svn working copies for the latest revision number. Instead these build jobs can source this number from the pre-stamped files.
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@8023 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ Tools/JenkinsCI/tools/svnupdate.bat
@@ -0,0 +1,8 @@
+SET MG_SOURCES_DIR=C:\working\sources
+svn update "%MG_SOURCES_DIR%\fdo-3.8"
+svn update "%MG_SOURCES_DIR%\fdo-trunk"
+svn update "%MG_SOURCES_DIR%\mg-2.5\Installer"
+svn update "%MG_SOURCES_DIR%\mg-2.5\MgDev"
+svn update "%MG_SOURCES_DIR%\mg-trunk\Installer"
+svn update "%MG_SOURCES_DIR%\mg-trunk\MgDev"
+svn update "%MG_SOURCES_DIR%\MgInstantSetup"
