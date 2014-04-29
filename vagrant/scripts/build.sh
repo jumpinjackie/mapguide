a2f62a604eff4c1f7cf8102654960e725a63df18
Jackie Ng <jumpinjackie@gmail.com>
2014-04-29 11:20:42 +0000

    Branch configurations for 2.6
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.6@8085 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ vagrant/scripts/build.sh
@@ -0,0 +1,167 @@
+#!/bin/bash
+
+# Global vars for this script. Modify as necessary
+APIVERSION=2.6
+BUILDNUM=${APIVERSION}.0
+BUILDROOT=`pwd`
+MGCPUPLATFORM=i386
+INSTALLROOT=/usr/local/mapguideopensource-${BUILDNUM}
+#INSTALLROOT=/usr/local/mapguideopensource-trunk
+LOCKFILEDIR=/var/lock/mgserver
+MGSOURCE=${BUILDROOT}/mgdev
+VERFILE=${MGSOURCE}/Common/ProductVersion.h
+# Use LOCALSVN=1 if you want to build from an existing copy instead of building
+# from an export (saves bandwidth and faster)
+LOCALSVN=1
+SVNROOT=/home/vagrant
+#SVNROOT="svn://svn.bld.mgproto.net"
+#SVNROOT="http://svn.osgeo.org"
+SVNRELPATH=/mapguide/trunk/MgDev
+MY_MAKE_OPTS="-j 2"
+UBUNTU=0
+PRESERVE_BUILD_ROOT=1
+
+echo "******************************************************************"
+echo " MapGuide will be installed to: ${INSTALLROOT}"
+echo " SVN Source is: ${SVNROOT}${SVNRELPATH}"
+echo " Make Options: ${MY_MAKE_OPTS}"
+echo " Is Ubuntu?: ${UBUNTU}"
+echo " Preserving the build dir?: ${PRESERVE_BUILD_ROOT}"
+echo " JAVA_HOME: ${JAVA_HOME}"
+echo "******************************************************************"
+
+# Need JAVA_HOME for JavaApi
+if [ ! -d ${JAVA_HOME} ];
+then
+echo "ERROR: Environment variable JAVA_HOME not set. Please set this enviroment variable first"
+exit
+fi
+
+check_build()
+{
+    error=$?
+    if [ $error -ne 0 ]; then
+        echo "$BUILD_COMPONENT: Error build failed ($error)................."
+        exit $error
+    fi
+}
+
+if [ ! ${PRESERVE_BUILD_ROOT} -eq 1 ]
+then
+    echo "Removing existing build dir at ${MGSOURCE}"
+    rm -rf ${MGSOURCE}
+else
+    echo "Preserving existing build dir"
+fi
+rm -rf ${INSTALLROOT}
+
+REVISION=`svn info ${SVNROOT}${SVNRELPATH} | perl revnum.pl`
+echo ${REVISION} > revnum.txt
+if [ ! ${PRESERVE_BUILD_ROOT} -eq 1 -o ! -d ${MGSOURCE} ];
+then
+    if [ ! -d ${MGSOURCE} ];
+    then
+    	echo "Build root ${MGSOURCE} does not exist. Doing svn export"
+    fi
+    echo "Exporting svn revision ${REVISION}"
+    if [ ${LOCALSVN} -eq 1 ] 
+    then
+        echo "Making local SVN copy of ${SVNROOT}${SVNRELPATH} to ${MGSOURCE}"
+        cp -R ${SVNROOT}${SVNRELPATH} ${MGSOURCE}
+        echo "Cleaning out .svn directories"
+	pushd ${MGSOURCE}
+        find . -name .svn -exec rm -rf {} \;
+	popd
+    else
+        echo "Performing fresh SVN export of ${SVNROOT}${SVNRELPATH} (r${REVISION}) to ${MGSOURCE}"
+        svn export -q -r ${REVISION} ${SVNROOT}${SVNRELPATH} ${MGSOURCE}
+    fi
+fi
+
+start_time=`date +%s`
+
+echo "Building Revision ${BUILDNUM}.${REVISION}" 
+cd ${MGSOURCE}
+
+echo "#ifndef PRODUCTVERSION_H_" > ${VERFILE}
+echo "#define PRODUCTVERSION_H_" >> ${VERFILE}
+echo 'const STRING ProductVersion = L"'${BUILDNUM}'.'${REVISION}'";' >> ${VERFILE}
+echo 'const STRING ApiVersion     = L"'${APIVERSION}'";' >> ${VERFILE}
+echo '#endif' >> ${VERFILE}
+
+echo "Building LinuxApt"
+pushd ${MGSOURCE}/Oem/LinuxApt
+BUILD_COMPONENT="LinuxApt"
+./build_apt.sh --prefix ${INSTALLROOT} --with-tomcat
+check_build
+popd
+
+echo "Building Oem"
+BUILD_COMPONENT="Oem"
+./build_oem.sh --prefix ${INSTALLROOT}
+check_build
+
+echo "Building Fusion"
+pushd ${MGSOURCE}/Oem/fusion
+BUILD_COMPONENT="Fusion"
+ant prepare
+ant compress
+check_build
+popd
+
+echo "Building MapGuide"
+BUILD_COMPONENT="MapGuide"
+aclocal
+libtoolize --force
+automake --add-missing --copy
+autoconf
+if [ $(uname -m) = "x86_64" ]; then
+    MGCPUPLATFORM=amd64
+    ./configure --enable-optimized --enable-silent-rules --enable-64bit --prefix=${INSTALLROOT}
+else
+    MGCPUPLATFORM=i386
+    ./configure --enable-optimized --enable-silent-rules --prefix=${INSTALLROOT}
+fi
+make $MY_MAKE_OPTS
+check_build
+BUILD_COMPONENT="MapGuide Install"
+make install
+check_build
+
+end_time=`date +%s`
+
+echo "Preparing binaries for packaging"
+# Prepare binaries for packaging by removing unnecessary
+# .la and .a files and stripping unneeded symbols from the binaries
+pushd ${INSTALLROOT}
+LIBDIRS="lib server/lib webserverextensions/lib" 
+echo "Stripping symbols from binaries"
+for libdir in ${LIBDIRS}
+do
+    # Remove any .la and .a files in lib directories
+    rm -f ${libdir}/*.la
+    rm -f ${libdir}/*.a
+
+    # Remove unneeded symbols from files in the lib directories
+    for file in `find ${libdir}/lib*.so* -type f -print`
+    do
+        strip --strip-unneeded ${file}
+        chmod a-x ${file}
+    done
+done
+popd
+
+# Make tarball if not ubuntu
+if [ ${UBUNTU} -eq 0 ];
+then
+    echo "Creating MapGuide Open Source binary tarball"
+    # Tarball the whole works and put it in a bin directory
+    pushd ${BUILDROOT}
+    if [ ! -d bin ]; then
+        mkdir -p bin
+    fi
+
+    tar -zcf bin/mapguideopensource-${BUILDNUM}.${REVISION}.${MGCPUPLATFORM}.tar.gz ${INSTALLROOT} ${LOCKFILEDIR}
+fi
+echo "Build complete!"
+echo Main build execution: `expr $end_time - $start_time` s
---
+++ vagrant/scripts/build_fdo.sh
@@ -0,0 +1,334 @@
+#!/bin/bash
+
+# build_fdo.sh
+#
+# Builds FDO and creates a tarball from the binary output
+#
+# Modify setenvironment.sh to point to the correct Oracle, MySQL and PostgreSQL paths
+# before building
+
+FDO_BUILD_COMPONENT=
+
+# FDO version. Make sure this matches your FDO build source
+FDO_VER_MAJOR=3
+FDO_VER_MINOR=9
+FDO_VER_REV=0
+
+UBUNTU=0
+FDO_CPU=x86
+FDO_PLATFORM=32
+FDO_BUILD_CPU=i386
+LIB_DIRNAME=lib
+
+# FDO install directory
+FDO_VER_FULL=${FDO_VER_MAJOR}.${FDO_VER_MINOR}.${FDO_VER_REV}
+FDO_INST=/usr/local/fdo-${FDO_VER_FULL}
+
+check_build()
+{
+	error=$?
+	if [ $error -ne 0 ]; then
+		echo "[error]: ${FDO_BUILD_COMPONENT} - Error build failed ($error)"
+		exit $error
+	fi
+}
+
+check_fdo_lib()
+{
+	libname=$1-${FDO_VER_FULL}.so
+	libpath=${FDO_INST}/lib/${libname}
+	if [ ! -e ${libpath} ]; then
+		echo "[error]: Error building ${libname}"
+		exit 1
+	fi
+}
+
+save_current_file_list()
+{
+	echo "[info]: Saving current FDO dir file list"
+	pushd $FDO_INST
+	# For lazy folks who build from svn working copies instead of svn exports, we need to weed out any .svn dirs before compiling the file-list
+	find . -name .svn -exec rm -rf {} \;
+	find . -type f -print > ${FDO_FILELIST}/temp.lst
+	find . -type l -print >> ${FDO_FILELIST}/temp.lst
+	sort ${FDO_FILELIST}/temp.lst > ${FDO_FILELIST}/orig.lst
+	find . -type d -print | sort > ${FDO_FILELIST}/origdir.lst
+	popd
+}
+
+update_fdocore_file_list()
+{
+	echo "[info]: Updating FDO core file list for deb packaging"
+	pushd $FDO_INST
+	# For lazy folks who build from svn working copies instead of svn exports, we need to weed out any .svn dirs before compiling the file-list
+	find . -name .svn -exec rm -rf {} \;
+	find . -type f -print > ${FDO_FILELIST}/temp.lst
+	find . -type l -print >> ${FDO_FILELIST}/temp.lst
+	sort ${FDO_FILELIST}/temp.lst > ${FDO_FILELIST}/fdocore.lst
+	find . -type d -print | sort > ${FDO_FILELIST}/fdocoredir.lst
+	popd
+}
+
+update_provider_file_list()
+{
+	PROVIDER=$1
+	echo "[info]: Updating $PROVIDER file list for deb packaging"
+	pushd $FDO_INST
+	# For lazy folks who build from svn working copies instead of svn exports, we need to weed out any .svn dirs before compiling the file-list
+	find . -name .svn -exec rm -rf {} \;
+	#mkdir -p $BUILDLIST
+	find . -type f -print > ${FDO_FILELIST}/temp.lst
+	find . -type l -print >> ${FDO_FILELIST}/temp.lst
+	cat ${FDO_FILELIST}/orig.lst >> ${FDO_FILELIST}/temp.lst
+	sort ${FDO_FILELIST}/temp.lst | uniq -u > ${FDO_FILELIST}/${PROVIDER}.lst
+	find . -type d -print | sort > ${FDO_FILELIST}/temp.lst
+	cat ${FDO_FILELIST}/origdir.lst >> ${FDO_FILELIST}/temp.lst
+	sort ${FDO_FILELIST}/temp.lst | uniq -u > ${FDO_FILELIST}/${PROVIDER}dir.lst
+	popd
+}
+
+BUILDROOT=`pwd`
+
+LOCALSVN=1
+PRESERVE_BUILD_ROOT=1
+CMAKE=0
+
+FDO_SRC=/home/vagrant/fdo/trunk
+#FDO_SRC=http://svn.osgeo.org/fdo/trunk
+FDO_BUILD_AREA=${BUILDROOT}/fdo_build_area
+FDO_FILELIST=${FDO_BUILD_AREA}/install/filelist
+
+shim_thirdparty_lib_paths()
+{
+    echo "[info]: Shimming include/lib paths"
+    # Note:
+    #
+    # FDO assumes you're going to be linking against an SDK whose directory structure
+    # is different from how system dev libraries are installed on Ubuntu, so we leverage the
+    # power of symlinks to set up the expected directory structure that points to the system
+    # headers and libraries, and modify setenvironment.sh to point to this shimmed directory
+    # structure
+    #
+    # This structure assumes you've apt-get installed the following:
+    #
+    #   libmysqlclient-dev libpq-dev
+    #
+    mkdir -p ~/fdo_rdbms_thirdparty_system/pgsql/$FDO_CPU
+    # PostgreSQL include path
+    if [ ! -d ~/fdo_rdbms_thirdparty_system/pgsql/$FDO_CPU/include ];
+    then
+        ln -s /usr/include/postgresql ~/fdo_rdbms_thirdparty_system/pgsql/$FDO_CPU/include
+        echo "[info]: Symlinked PostgreSQL include path"
+    else
+        echo "[info]: PostgreSQL include path already symlinked"
+    fi
+    # PostgreSQL lib path
+    if [ ! -d ~/fdo_rdbms_thirdparty_system/pgsql/$FDO_CPU/$LIB_DIRNAME ];
+    then
+        ln -s /usr/lib ~/fdo_rdbms_thirdparty_system/pgsql/$FDO_CPU/$LIB_DIRNAME
+        echo "[info]: Symlinked PostgreSQL lib path"
+    else
+        echo "[info]: PostgreSQL lib path already symlinked"
+    fi
+    mkdir -p ~/fdo_rdbms_thirdparty_system/mysql/$FDO_CPU
+    # MySQL include path
+    if [ ! -d ~/fdo_rdbms_thirdparty_system/mysql/$FDO_CPU/include ];
+    then
+        ln -s /usr/include/mysql ~/fdo_rdbms_thirdparty_system/mysql/$FDO_CPU/include
+        echo "[info]: Symlinked MySQL include path"
+    else
+        echo "[info]: MySQL include path already symlinked"
+    fi
+    # MySQL lib path
+    if [ ! -d ~/fdo_rdbms_thirdparty_system/mysql/$FDO_CPU/$LIB_DIRNAME ];
+    then
+        if [ ${FDO_PLATFORM} -eq 32 ]; 
+        then
+            ln -s /usr/lib/i386-linux-gnu ~/fdo_rdbms_thirdparty_system/mysql/$FDO_CPU/$LIB_DIRNAME
+            echo "[info]: Symlinked MySQL lib path (x86)"
+        else
+            ln -s /usr/lib/x86_64-linux-gnu ~/fdo_rdbms_thirdparty_system/mysql/$FDO_CPU/$LIB_DIRNAME
+            echo "[info]: Symlinked MySQL lib path (x64)"
+        fi
+    else
+        echo "[info]: MySQL lib path already symlinked"
+    fi
+}
+
+modify_sdk_paths()
+{
+    if [ ${UBUNTU} -eq 1 ];
+    then
+        echo "[info]: Updating setenvironment.sh"
+	    sed -i 's/export FDOMYSQL=$FDOTHIRDPARTY\/mysql\/rhlinux/export FDOMYSQL=\/home\/vagrant\/fdo_rdbms_thirdparty_system\/mysql\/'"$FDO_CPU"'/g' ${FDO_BUILD_AREA}/setenvironment.sh
+	    sed -i 's/export FDOPOSTGRESQL=$FDOTHIRDPARTY\/pgsql/export FDOPOSTGRESQL=\/home\/vagrant\/fdo_rdbms_thirdparty_system\/pgsql\/'"$FDO_CPU"'/g' ${FDO_BUILD_AREA}/setenvironment.sh
+    else
+        echo "[info]: Updating setenvironment.sh"
+	    # Note: Change your paths here if they're different
+	    sed -i 's/export FDOMYSQL=$FDOTHIRDPARTY\/mysql\/rhlinux/export FDOMYSQL=\/home\/vagrant\/fdo_rdbms_thirdparty\/mysql\/'"$FDO_CPU"'/g' ${FDO_BUILD_AREA}/setenvironment.sh
+	    sed -i 's/export FDOPOSTGRESQL=$FDOTHIRDPARTY\/pgsql/export FDOPOSTGRESQL=\/home\/vagrant\/fdo_rdbms_thirdparty\/pgsql/g' ${FDO_BUILD_AREA}/setenvironment.sh
+    fi
+	echo "export FDOORACLE=/home/vagrant/fdo_rdbms_thirdparty/oracle/${FDO_CPU}/instantclient_11_2/sdk" >> ${FDO_BUILD_AREA}/setenvironment.sh
+}
+
+# Must have root
+if [[ $EUID -ne 0 ]]; then
+	echo "[error]: You must run this script with superuser privileges"
+	exit 1
+fi
+
+echo "***********************************************************"
+echo " FDO Source: ${FDO_SRC}"
+echo " FDO Build Area: ${FDO_BUILD_AREA}"
+echo " FDO Install dir: ${FDO_INST}"
+echo " FDO CPU Target: ${FDO_BUILD_CPU}"
+echo " CMake build: ${CMAKE}"
+echo " Is Ubuntu?: ${UBUNTU}"
+echo " Export from local SVN checkout: ${LOCALSVN}"
+echo " Re-use previous build area: ${PRESERVE_BUILD_ROOT}"
+echo "***********************************************************"
+start_time=`date +%s`
+REVISION=`svn info ${FDO_SRC} | perl revnum.pl`
+
+if [ ${UBUNTU} -eq 1 ];
+then
+    shim_thirdparty_lib_paths
+fi
+
+if [ -d ${FDO_INST} ];
+then
+	echo "[info]: Deleting directory ${FDO_INST} before build"
+	rm -rf ${FDO_INST}
+else
+	echo "[info]: ${FDO_INST} doesn't exist. Continuing build"
+fi
+
+if [ ${CMAKE} -eq 1 ];
+then
+	echo "[error]: CMake build of FDO not supported yet"
+	exit 1
+else
+	echo "[info]: Using automake build"
+	if [ -d ${FDO_BUILD_AREA} ];
+	then
+		if [ ${PRESERVE_BUILD_ROOT} -eq 1 ];
+		then
+			echo "[info]: FDO build area ${FDO_BUILD_AREA} exists. Going straight to build"
+			modify_sdk_paths
+		else
+			echo "[info]: Removing old FDO build area at ${FDO_BUILD_AREA}"
+			rm -rf ${FDO_BUILD_AREA}
+			if [ ${LOCALSVN} -eq 1 ] 
+			then
+				svn export -q ${FDO_SRC} ${FDO_BUILD_AREA}
+				modify_sdk_paths
+			else
+				echo "[info]: Performing fresh SVN export of ${FDO_SRC} (r${REVISION}) to ${FDO_BUILD_AREA}"
+				svn export -q -r ${REVISION} ${FDO_SRC} ${FDO_BUILD_AREA}
+				modify_sdk_paths
+			fi
+		fi
+	else
+		echo "[info]: FDO build area ${FDO_BUILD_AREA} does not exist. Doing svn export"
+		echo "[info]: Exporting svn revision ${REVISION}"
+		if [ ${LOCALSVN} -eq 1 ] 
+		then
+			svn export -q ${FDO_SRC} ${FDO_BUILD_AREA}
+			modify_sdk_paths
+		else
+			echo "[info]: Performing fresh SVN export of ${FDO_SRC} (r${REVISION}) to ${FDO_BUILD_AREA}"
+			svn export -q -r ${REVISION} ${FDO_SRC} ${FDO_BUILD_AREA}
+			modify_sdk_paths
+		fi
+	fi
+fi
+
+echo "[info]: Building FDO (${FDO_VER_MAJOR}.${FDO_VER_MINOR}.${FDO_VER_REV}) rev (${REVISION})"
+cd ${FDO_BUILD_AREA}
+source ./setenvironment.sh
+if [ ! -d ${FDO_INST} ];
+then
+	echo "[error]: FDO install directory ${FDO_INST} doesn't exist. setenvironment.sh should've created this directory. Ensure the FDO version number in this script is correct"
+	exit 1;
+fi
+FDO_BUILD_COMPONENT="FDO Thirdparty"
+./build_thirdparty.sh -b ${FDO_PLATFORM}
+check_build
+
+if [ ${CMAKE} -eq 1 ];
+then
+	FDO_BUILD_COMPONENT="FDO (cmake)"
+	echo "[error]: CMake build of FDO not supported yet"
+	exit 1;
+else
+	#NOTE: We never build ArcSDE provider because we haven't paid the ESRI tax for their ArcSDE SDK
+	for comp in fdocore fdo utilities
+	do
+		FDO_BUILD_COMPONENT="$comp (automake)"
+		./build_linux.sh --w $comp --p ${FDO_INST} --b ${FDO_PLATFORM}
+		update_fdocore_file_list
+		check_build
+	done
+	for comp in shp sqlite gdal ogr wfs wms rdbms kingoracle sdf
+	do
+		save_current_file_list
+		FDO_BUILD_COMPONENT="$comp (automake)"
+		./build_linux.sh --w $comp --p ${FDO_INST} --b ${FDO_PLATFORM}
+		update_provider_file_list $comp
+		check_build
+	done
+fi
+check_fdo_lib libFDO
+check_fdo_lib libExpressionEngine
+check_fdo_lib libSDFProvider
+check_fdo_lib libSHPProvider
+check_fdo_lib libSHPOverrides
+check_fdo_lib libWFSProvider
+check_fdo_lib libWMSProvider
+check_fdo_lib libWMSOverrides
+# ArcSDE provider currently disabled due to missing libraries
+#./buildfdoprovider.sh arcsde
+#check_fdo_lib libArcSDEProvider
+check_fdo_lib libFdoMySQL
+check_fdo_lib libFdoPostgreSQL
+check_fdo_lib libFdoODBC
+check_fdo_lib libSchemaMgr_OV
+check_fdo_lib libGRFPProvider
+check_fdo_lib libGRFPOverrides
+check_fdo_lib libOGRProvider
+check_fdo_lib libKingOracleProvider
+check_fdo_lib libKingOracleOverrides
+check_fdo_lib libSQLiteProvider
+
+end_time=`date +%s`
+
+FDO_BUILD_COMPONENT="Remove .la files from ${FDO_INST}"
+# Remove .la files from lib directory
+rm -f ${FDO_INST}/lib/*.la
+check_build
+
+FDO_BUILD_COMPONENT="Strip so symbols and remove execute flag"
+# Remove unneeded symbols from files in the lib directory
+# and make them non-executable
+for file in `find ${FDO_INST}/lib/lib*.so* -type f -print`
+do
+	strip --strip-unneeded ${file}
+	chmod a-x ${file}
+done
+check_build
+
+cd $BUILDROOT
+FDO_BUILD_COMPONENT="Make tarball"
+# Create a binary tar ball for FDO
+tar -zcf fdosdk-centos6-${FDO_BUILD_CPU}-${FDO_VER_FULL}_${REVISION}.tar.gz ${FDO_INST}
+check_build
+
+if [ ${UBUNTU} -eq 1 ];
+then
+cd ${FDO_BUILD_AREA}/install
+dos2unix *
+./dpkgall.sh ${FDO_BUILD_CPU} ${REVISION}
+fi
+
+echo "[info]: FDO build complete!"
+echo Main build execution time: `expr $end_time - $start_time` s
