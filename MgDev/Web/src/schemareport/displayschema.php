159eee4643e92c6a121e65f678877082ed45698c
Jackie Ng <jumpinjackie@gmail.com>
2014-05-08 17:37:30 +0000

    [https://trac.osgeo.org/mapguide/ticket/2444]: Fix deprecated usage of preg_replace() in PHP 5.5
    git-svn-id: http://svn.osgeo.org/mapguide/branches/2.6@8122 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/Web/src/schemareport/displayschema.php
+++ MgDev/Web/src/schemareport/displayschema.php
@@ -68,18 +68,28 @@
                 // cannot contain the translated or encoded characters. This replaces '-xffXX-' with 'ffXX' for that
                 // particular string while the rest of the encoded chars are translated properly.
                 $subXml = substr($xml, strpos($xml, 'xs:element'), -1);
-                $subXml = preg_replace("/-x([A-Za-z0-9]{1,4})-/e", "html_entity_decode('&#'.hexdec('$1').';',ENT_NOQUOTES,'UTF-8')", $subXml);
-                $subXml = preg_replace("/_x([A-Za-z0-9]{1,4})-/e", "html_entity_decode('&#'.hexdec('$1').';',ENT_NOQUOTES,'UTF-8')", $subXml);
+                $subXml = preg_replace_callback("/-x([A-Za-z0-9]{1,4})-/", function($m) {
+                    return html_entity_decode('&#'.hexdec($m[1]).';',ENT_NOQUOTES,'UTF-8');
+                }, $subXml);
+                $subXml = preg_replace_callback("/_x([A-Za-z0-9]{1,4})-/", function($m) {
+                    return html_entity_decode('&#'.hexdec($m[1]).';',ENT_NOQUOTES,'UTF-8');
+                }, $subXml);
                 $xml = substr_replace($xml, $subXml, strpos($xml, 'xs:element'), -1);
 
                 $nameSpaceStart = strpos($xml, 'targetNamespace="');
                 $nameSpaceEnd = strpos($xml, '"', $nameSpaceStart+17);
                 $nameSpace = substr($xml, $nameSpaceStart, $nameSpaceEnd-$nameSpaceStart);
-                $nameSpaceModified = preg_replace("/-x([A-Za-z0-9]{1,4})-/e", "html_entity_decode('&#'.hexdec('$1').';',ENT_NOQUOTES,'UTF-8')", $nameSpace);
-                $nameSpaceModified = preg_replace("/_x([A-Za-z0-9]{1,4})-/e", "html_entity_decode('&#'.hexdec('$1').';',ENT_NOQUOTES,'UTF-8')", $nameSpace);
+                $nameSpaceModified = preg_replace_callback("/-x([A-Za-z0-9]{1,4})-/", function($m) {
+                    return html_entity_decode('&#'.hexdec($m[1]).';',ENT_NOQUOTES,'UTF-8');
+                }, $nameSpace);
+                $nameSpaceModified = preg_replace_callback("/_x([A-Za-z0-9]{1,4})-/", function($m) {
+                    return html_entity_decode('&#'.hexdec($m[1]).';',ENT_NOQUOTES,'UTF-8');
+                }, $nameSpace);
 
                 $xml = str_replace($nameSpace, $nameSpaceModified, $xml);
-                $xml = preg_replace("/-x([A-Za-z0-9]{1,4})-/e", "'$1'", $xml);
+                $xml = preg_replace_callback("/-x([A-Za-z0-9]{1,4})-/", function($m) {
+                    return $m[1];
+                }, $xml);
 
                 $xsl_file = 'displayschema.xsl';
 
