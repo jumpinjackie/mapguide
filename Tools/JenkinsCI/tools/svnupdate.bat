af1be359343cb1153a788fb275cfd697cca21c16
Jackie Ng <jumpinjackie@gmail.com>
2014-05-12 12:31:52 +0000

    This is the Jenkins configuration used to produce the MapGuide 2.6 beta 1 release and Maestro 6.0a1 release.
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@8127 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Tools/JenkinsCI/tools/svnupdate.bat
+++ Tools/JenkinsCI/tools/svnupdate.bat
@@ -1,8 +1,9 @@
+@echo off
 SET MG_SOURCES_DIR=C:\working\sources
-svn update "%MG_SOURCES_DIR%\fdo-3.8"
+svn update "%MG_SOURCES_DIR%\fdo-3.9"
 svn update "%MG_SOURCES_DIR%\fdo-trunk"
-svn update "%MG_SOURCES_DIR%\mg-2.5\Installer"
-svn update "%MG_SOURCES_DIR%\mg-2.5\MgDev"
+svn update "%MG_SOURCES_DIR%\mg-2.6\Installer"
+svn update "%MG_SOURCES_DIR%\mg-2.6\MgDev"
 svn update "%MG_SOURCES_DIR%\mg-trunk\Installer"
 svn update "%MG_SOURCES_DIR%\mg-trunk\MgDev"
 svn update "%MG_SOURCES_DIR%\MgInstantSetup"
