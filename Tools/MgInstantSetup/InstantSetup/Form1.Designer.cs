9b90074c01dcac5b0c03b587ee929a0eaf2761cc
Jackie Ng <jumpinjackie@gmail.com>
2015-01-06 15:28:16 +0000

    UI sizing tweaks for MgInstantSetup
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@8494 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Tools/MgInstantSetup/InstantSetup/Form1.Designer.cs
+++ Tools/MgInstantSetup/InstantSetup/Form1.Designer.cs
@@ -85,7 +85,7 @@
             this.tabControl1.Location = new System.Drawing.Point(3, 16);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(580, 428);
+            this.tabControl1.Size = new System.Drawing.Size(380, 428);
             this.tabControl1.TabIndex = 1;
             // 
             // TAB_APACHE
@@ -93,7 +93,7 @@
             this.TAB_APACHE.Location = new System.Drawing.Point(4, 22);
             this.TAB_APACHE.Name = "TAB_APACHE";
             this.TAB_APACHE.Padding = new System.Windows.Forms.Padding(3);
-            this.TAB_APACHE.Size = new System.Drawing.Size(572, 402);
+            this.TAB_APACHE.Size = new System.Drawing.Size(372, 402);
             this.TAB_APACHE.TabIndex = 1;
             this.TAB_APACHE.Text = "Apache";
             this.TAB_APACHE.UseVisualStyleBackColor = true;
@@ -230,7 +230,7 @@
             this.groupBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox1.Location = new System.Drawing.Point(0, 0);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(586, 447);
+            this.groupBox1.Size = new System.Drawing.Size(386, 447);
             this.groupBox1.TabIndex = 13;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Web";
@@ -249,7 +249,7 @@
             this.groupBox2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.groupBox2.Location = new System.Drawing.Point(0, 0);
             this.groupBox2.Name = "groupBox2";
-            this.groupBox2.Size = new System.Drawing.Size(294, 447);
+            this.groupBox2.Size = new System.Drawing.Size(494, 447);
             this.groupBox2.TabIndex = 14;
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Server";
@@ -260,7 +260,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label7.Location = new System.Drawing.Point(19, 386);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(257, 45);
+            this.label7.Size = new System.Drawing.Size(457, 45);
             this.label7.TabIndex = 8;
             this.label7.Text = "NOTE: Some providers (eg. MySQL) require 3rd party external dependencies in order" +
     " to sucessfully register";
@@ -276,7 +276,7 @@
             this.chkProviders.Location = new System.Drawing.Point(20, 137);
             this.chkProviders.Name = "chkProviders";
             this.chkProviders.ScrollAlwaysVisible = true;
-            this.chkProviders.Size = new System.Drawing.Size(256, 244);
+            this.chkProviders.Size = new System.Drawing.Size(456, 244);
             this.chkProviders.TabIndex = 7;
             // 
             // label6
@@ -291,7 +291,7 @@
             // btnFdoRegUtilPath
             // 
             this.btnFdoRegUtilPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnFdoRegUtilPath.Location = new System.Drawing.Point(249, 86);
+            this.btnFdoRegUtilPath.Location = new System.Drawing.Point(449, 86);
             this.btnFdoRegUtilPath.Name = "btnFdoRegUtilPath";
             this.btnFdoRegUtilPath.Size = new System.Drawing.Size(27, 23);
             this.btnFdoRegUtilPath.TabIndex = 5;
@@ -302,7 +302,7 @@
             // btnFdoPath
             // 
             this.btnFdoPath.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnFdoPath.Location = new System.Drawing.Point(249, 41);
+            this.btnFdoPath.Location = new System.Drawing.Point(449, 41);
             this.btnFdoPath.Name = "btnFdoPath";
             this.btnFdoPath.Size = new System.Drawing.Size(27, 23);
             this.btnFdoPath.TabIndex = 4;
@@ -316,7 +316,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtFdoRegUtilPath.Location = new System.Drawing.Point(20, 88);
             this.txtFdoRegUtilPath.Name = "txtFdoRegUtilPath";
-            this.txtFdoRegUtilPath.Size = new System.Drawing.Size(223, 20);
+            this.txtFdoRegUtilPath.Size = new System.Drawing.Size(423, 20);
             this.txtFdoRegUtilPath.TabIndex = 3;
             // 
             // label5
@@ -334,7 +334,7 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.txtFdoDir.Location = new System.Drawing.Point(20, 43);
             this.txtFdoDir.Name = "txtFdoDir";
-            this.txtFdoDir.Size = new System.Drawing.Size(223, 20);
+            this.txtFdoDir.Size = new System.Drawing.Size(423, 20);
             this.txtFdoDir.TabIndex = 1;
             this.txtFdoDir.TextChanged += new System.EventHandler(this.txtFdoDir_TextChanged);
             // 
@@ -364,7 +364,7 @@
             // 
             this.splitContainer1.Panel2.Controls.Add(this.groupBox1);
             this.splitContainer1.Size = new System.Drawing.Size(884, 447);
-            this.splitContainer1.SplitterDistance = 294;
+            this.splitContainer1.SplitterDistance = 494;
             this.splitContainer1.TabIndex = 15;
             // 
             // Form1
