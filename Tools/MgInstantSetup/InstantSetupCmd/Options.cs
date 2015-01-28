c728d66526f2f04dfa1c8f726888c2a90cfae437
Jackie Ng <jumpinjackie@gmail.com>
2015-01-28 11:49:12 +0000

    [https://trac.osgeo.org/mapguide/ticket/2498]: Initial implementation of command-line instant setup.
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@8523 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ Tools/MgInstantSetup/InstantSetupCmd/Options.cs
@@ -0,0 +1,105 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using InstantSetup.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace InstantSetupCmd
+{
+    public enum MapGuideWebConfiguration
+    {
+        Apache,
+        IIS
+    }
+
+    public class Options
+    {
+        [Option('v', "viewer", DefaultValue = ApiType.Php, HelpText = "The default viewer type that the mapviewerajax virtual directory will bind to (Php, Java or DotNet)")]
+        public ApiType DefaultViewer { get; set; }
+
+        [Option("admin-port", DefaultValue = 2820, HelpText = "The port to listen on for admin connections")]
+        public int ServerAdminPort { get; set; }
+
+        [Option("client-port", DefaultValue = 2820, HelpText = "The port to listen on for client connections")]
+        public int ServerClientPort { get; set; }
+
+        [Option("site-port", DefaultValue = 2820, HelpText = "The port to listen on for site connections")]
+        public int ServerSitePort { get; set; }
+
+        [Option("write-mentor-dictionary-path", DefaultValue = false, HelpText = "Write code to set up MENTOR_DICTIONARY_PATH on batch scripts. Only applicable for MGOS 2.2 or older")]
+        public bool WriteMentorDictionaryPath { get; set; }
+
+        [Option("enable-php", DefaultValue = true, HelpText = "Enable PHP for this MapGuide installation")]
+        public bool EnablePhp { get; set; }
+
+        [Option("64bit", DefaultValue = false, HelpText = "This is a 64-bit MapGuide installation. Must be explicitly set for IIS-based install")]
+        public bool Is64BitMapGuideForIIS { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "The path where generated batch scripts will be saved to")]
+        public string BatchFileOutputDirectory { get; set; }
+
+        [Option("service-name", HelpText = "The name of the service to use if installing MapGuide as a windows service. Only applicable if --install-services is specified")]
+        public string MapGuideServiceName { get; set; }
+
+        [Option("install-services", DefaultValue = false, HelpText = "If true, will generate batch scripts to register MapGuide as a windows service")]
+        public bool InstallServices { get; set; }
+
+        [Option("backup-path", HelpText = "The directory where backups of configuration files will be copied to")]
+        public string BackupConfigPath { get; set; }
+
+        [Option("backup-files", DefaultValue = true, HelpText = "If true, will create backups of existing configuration files before overwriting")]
+        public bool BackupConfigFiles { get; set; }
+
+        [Option("source-dir", HelpText = "The root path of the MapGuide installation (where InstantSetupCmd will do its configuration)")]
+        public string SourceRootDir { get; set; }
+
+        [Option("virtual-dir", DefaultValue = "mapguide", HelpText = "The name of the mapguide virtual directory to create on IIS/Apache")]
+        public string VirtualDirectoryName { get; set; }
+
+        [Option("webtier-config", Required = true, DefaultValue = MapGuideWebConfiguration.Apache, HelpText = "The web tier configuration to use (IIS / Apache)")]
+        public MapGuideWebConfiguration WebTierConfig { get; set; }
+
+#region Apache-specific
+        [Option("enable-java", DefaultValue = false, HelpText = "Enables Java for this MapGuide installation")]
+        public bool EnableJava { get; set; }
+
+        [Option("tomcat-port", DefaultValue = 8019, HelpText = "The port number to use for the bundled copy of Tomcat. Only applicable if using Apache configuration and Java is enabled")]
+        public int TomcatPortNumber { get; set; }
+
+        [Option("httpd-port", DefaultValue = 8018, HelpText = "The port number to use for the bundled copy of Apache. Only applicable if using Apache configuration")]
+        public int ApachePortNumber { get; set; }
+
+        [Option("httpd-service-name", HelpText = "The service name to use for Apache if registering it as a service. Only applicable if using Apache confguration and --install-services is specified")]
+        public string HttpdServiceName { get; set; }
+
+        [Option("apache-dir-name", DefaultValue = "Apache24", HelpText = "The name of the directory in the install root that contains the bundled copy of Apache")]
+        public string ApacheDirName { get; set; }
+#endregion
+
+#region IIS-specific
+        [Option("enable-dotnet", DefaultValue = true, HelpText = "Enables .net for this MapGuide installation")]
+        public bool EnableDotNet { get; set; }
+
+        [Option("application-pool", DefaultValue = "MapGuideTrunkAppPool", HelpText = "The name of the IIS application pool to create")]
+        public string ApplicationPool { get; set; }
+
+        [Option("default-web-site", DefaultValue = "Default Web Site", HelpText = "The name of the default IIS web site to install the web extensions into")]
+        public string WebSiteName { get; set; }
+
+        [Option("appcmd-path", DefaultValue = "C:\\Windows\\System32\\inetsrv\\appcmd.exe", HelpText = "The path to appcmd.exe for automating IIS configuration")]
+        public string AppCmdPath { get; set; }
+#endregion
+
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
---
+++ Tools/MgInstantSetup/InstantSetupCmd/Program.cs
@@ -0,0 +1,93 @@
+﻿using InstantSetup.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstantSetupCmd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Options options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                AbstractSetupConfigurationProcess proc = null;
+                switch (options.WebTierConfig)
+                {
+                    case MapGuideWebConfiguration.Apache:
+                        proc = CreateApacheConfig(options);
+                        break;
+                    case MapGuideWebConfiguration.IIS:
+                        proc = CreateIISConfig(options);
+                        break;
+                    default:
+                        Console.WriteLine("Unknown web tier configuration: {0}", options.WebTierConfig);
+                        Environment.Exit(1);
+                        break;
+                }
+                if (proc != null)
+                {
+                    proc.Execute();
+                    Console.WriteLine("Batch scripts saved to: {0}", options.BatchFileOutputDirectory);
+                    Environment.Exit(0);
+                }
+            }
+        }
+
+        private static AbstractSetupConfigurationProcess CreateIISConfig(Options options)
+        {
+            IISSetupConfigurationProcess proc = new IISSetupConfigurationProcess();
+
+            proc.AppCmdPath = options.AppCmdPath;
+            proc.ApplicationPool = options.ApplicationPool;
+            proc.BackupConfigFiles = options.BackupConfigFiles;
+            proc.BackupConfigPath = options.BackupConfigPath;
+            proc.BatchFileOutputDirectory = options.BatchFileOutputDirectory;
+            proc.BuildOutputPath = options.SourceRootDir;
+            proc.DefaultViewer = options.DefaultViewer;
+            proc.EnableDotNet = options.EnableDotNet;
+            proc.EnablePhp = options.EnablePhp;
+            proc.InstallServices = options.InstallServices;
+            proc.Is64BitMapGuide = options.Is64BitMapGuideForIIS;
+            proc.MapGuideServiceName = options.MapGuideServiceName;
+            proc.ServerAdminPort = options.ServerAdminPort;
+            proc.ServerClientPort = options.ServerClientPort;
+            proc.ServerSitePort = options.ServerSitePort;
+            proc.UsingIIS7 = true;
+            proc.VirtualDirectoryName = options.VirtualDirectoryName;
+            proc.WebSiteName = options.WebSiteName;
+            proc.WriteMentorDictionaryPath = options.WriteMentorDictionaryPath;
+
+            return proc;
+        }
+
+        private static AbstractSetupConfigurationProcess CreateApacheConfig(Options options)
+        {
+            ApacheSetupConfigurationProcess proc = new ApacheSetupConfigurationProcess();
+
+            proc.ApacheDirName = options.ApacheDirName;
+            proc.ApachePortNumber = options.ApachePortNumber;
+            proc.BackupConfigFiles = options.BackupConfigFiles;
+            proc.BackupConfigPath = options.BackupConfigPath;
+            proc.BatchFileOutputDirectory = options.BatchFileOutputDirectory;
+            proc.BuildOutputPath = options.SourceRootDir;
+            proc.DefaultViewer = options.DefaultViewer;
+            proc.EnableJava = options.EnableJava;
+            proc.EnablePhp = options.EnablePhp;
+            proc.HttpdServiceName = options.HttpdServiceName;
+            proc.InstallServices = options.InstallServices;
+            proc.MapGuideServiceName = options.MapGuideServiceName;
+            proc.ServerAdminPort = options.ServerAdminPort;
+            proc.ServerClientPort = options.ServerClientPort;
+            proc.ServerSitePort = options.ServerSitePort;
+            proc.TomcatPortNumber = options.TomcatPortNumber;
+            proc.VirtualDirectoryName = options.VirtualDirectoryName;
+            proc.WriteMentorDictionaryPath = options.WriteMentorDictionaryPath;
+
+            return proc;
+        }
+    }
+}
