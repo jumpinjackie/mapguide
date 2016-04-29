7f7510f53e1bbbcd06b75a0d04b9303e5bf45284
Jackie Ng <jumpinjackie@gmail.com>
2016-04-29 12:49:33 +0000

    Remove liboss4-salsa-dev from ubuntu pre-requisites. We only installed this library only for the atomic.h that is packaged with this library, which our vagrant build system already copies into the expected location if it doesn't already exist.
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@8933 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Tools/Vagrant/packer/scripts/mapguide/ubuntu64.sh
+++ Tools/Vagrant/packer/scripts/mapguide/ubuntu64.sh
@@ -2,7 +2,7 @@
 # Set bash as the default shell
 echo "dash    dash/sh boolean false" | sudo debconf-set-selections
 sudo dpkg-reconfigure --frontend=noninteractive dash
-sudo apt-get install -y build-essential bison automake libtool libpng-dev libjpeg62-dev libxslt-dev libfreetype6-dev libpcre3-dev openjdk-7-jdk subversion lintian ant scons dos2unix liboss4-salsa-dev libldap-dev libssl-dev libcurl4-openssl-dev libexpat-dev libmysqlclient-dev unixODBC-dev python-dev libpq-dev libcppunit-dev
+sudo apt-get install -y build-essential bison automake libtool libpng-dev libjpeg62-dev libxslt-dev libfreetype6-dev libpcre3-dev openjdk-7-jdk subversion lintian ant scons dos2unix libldap-dev libssl-dev libcurl4-openssl-dev libexpat-dev libmysqlclient-dev unixODBC-dev python-dev libpq-dev libcppunit-dev
 # For producing .tar.xz archives
 sudo apt-get install -y xz-utils
 # For deb packaging
