a30522a7b2e2d1981d04e70bedc01ba0eb8bc9ed
Jackie Ng <jumpinjackie@gmail.com>
2016-05-17 12:44:08 +0000

    Merged revision(s) 8964 from trunk/Tools/Vagrant: Merged revision(s) 8963 from branches/3.1/vagrant: Vagrant build environment changes:  - Change verify.bat to be like build_target.bat: Focused on a specific VM target and allows multiple instances to be run in parallel.  - Add parameterized test_provision_*.sh scripts that perform the installation and smoke testing.  - Update test vagrantfiles to use the new parameterized test_provision_*.sh scripts  - Fix wrong VM for Ubuntu14 test vagrantfiles ........
    ........
    
    
    git-svn-id: http://svn.osgeo.org/mapguide/branches/3.0@8965 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- vagrant/scripts/test_provision_ubuntu.sh
+++ vagrant/scripts/test_provision_ubuntu.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 DOWNLOAD_HOST=$1
 URI_PART=$2
-DIST_DIR="ubuntu_x64"
-echo Download host is: $DOWNLOAD_HOST
+DIST_DIR=$3
+echo ***************************************************
+echo DOWNLOAD_HOST: ${DOWNLOAD_HOST}
+echo URI_PART:      ${URI_PART}
+echo DIST_DIR:      ${DIST_DIR}
+echo ***************************************************
 wget "http://$DOWNLOAD_HOST/$URI_PART/Sheboygan.mgp"
-wget "http://$DOWNLOAD_HOST/$URI_PART/smoke_test.sh"
 sudo mv Sheboygan.mgp /tmp/
 wget "http://$DOWNLOAD_HOST/$URI_PART/$DIST_DIR/mginstallubuntu.sh"
-sed -i 's/URL="$URL_ROOT\/$URL_PART"/URL="http:\/\/'"$DOWNLOAD_HOST"'\/'"$URI_PART"'\/'"$DIST_DIR"'"/g' mginstallubuntu.sh
+REPLACE=URL="http://$DOWNLOAD_HOST/$URI_PART/$DIST_DIR"
+sed -i 's#URL="\$URL_ROOT\/\$URL_PART"#'"$REPLACE"'#g' mginstallubuntu.sh
+cp /vagrant/smoke_test.sh .
 chmod +x smoke_test.sh
 chmod +x mginstallubuntu.sh
 sudo ./mginstallubuntu.sh --headless --with-sdf --with-shp --with-ogr --with-gdal --with-sqlite
