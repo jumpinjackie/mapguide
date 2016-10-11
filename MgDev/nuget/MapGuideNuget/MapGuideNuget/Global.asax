14b62f22e04117881cead908d074222bd09b7b30
Jackie Ng <jumpinjackie@gmail.com>
2016-10-11 12:06:58 +0000

    Update nuget package verification solution/projects. Remove the web projects (as this requires a specific VS2012 express SKU) and replace with a console app that exercises some MapGuide Web-specific APIs.
    git-svn-id: http://svn.osgeo.org/mapguide/branches/3.1@9067 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- MgDev/nuget/MapGuideNuget/MapGuideNuget/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MapGuideNuget.Global" Language="C#" %>
--- MgDev/nuget/MapGuideNuget/WebApiTest/Properties/AssemblyInfo.cs
+++ MgDev/nuget/MapGuideNuget/WebApiTest/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MapGuideNuget")]
+[assembly: AssemblyTitle("WebApiTest")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MapGuideNuget")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct("WebApiTest")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("43a5bb36-748c-4637-8a4e-386ed3a7aa82")]
+[assembly: Guid("5714e1dc-b801-4881-b6e6-9c305c523f4d")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,7 +29,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
