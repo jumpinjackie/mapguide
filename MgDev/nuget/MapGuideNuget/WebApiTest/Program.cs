c19927e021f2f9989546dc7d547927bcafb9e39e
Jackie Ng <jumpinjackie@gmail.com>
2016-10-11 13:46:56 +0000

    Merged revision(s) 9067-9068 from branches/3.1/MgDev: Update nuget package verification solution/projects. Remove the web projects (as this requires a specific VS2012 express SKU) and replace with a console app that exercises some MapGuide Web-specific APIs. ........ Fix [https://trac.osgeo.org/mapguide/ticket/2749]: Add .iobj and .ipdb to release output exclusion lists ........
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@9069 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

---
+++ MgDev/nuget/MapGuideNuget/WebApiTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using OSGeo.MapGuide;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var webConfigPath = @"C:\Program Files\OSGeo\MapGuide\Web\www\webconfig.ini";
+            MapGuideApi.MgInitializeWebTier(webConfigPath);
+
+            MgUserInformation user = new MgUserInformation("Anonymous", "");
+            MgSiteConnection conn = new MgSiteConnection();
+            conn.Open(user);
+            MgSite site = conn.GetSite();
+            string sessionId = site.CreateSession();
+
+            Console.WriteLine("Session ID: " + sessionId);
+            MgResourceIdentifier wlId = new MgResourceIdentifier("Library://Samples/Sheboygan/Layouts/SheboyganAsp.WebLayout");
+            MgResourceService resSvc = (MgResourceService)conn.CreateService(MgServiceType.ResourceService);
+
+            MgWebLayout layout = new MgWebLayout(resSvc, wlId);
+            Console.WriteLine("Web Layout Title: " + layout.GetTitle());
+            Console.WriteLine("Looks good! Press any key to continue");
+            Console.Read();
+        }
+    }
+}
