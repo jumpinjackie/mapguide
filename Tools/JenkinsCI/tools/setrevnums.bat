70a784adbd6fa218fe5e4c27ac74fcfa8a728103
Jackie Ng <jumpinjackie@gmail.com>
2016-10-26 14:07:43 +0000

    This is the Jenkins config used to build MGOS 3.1
    git-svn-id: http://svn.osgeo.org/mapguide/trunk@9071 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Tools/JenkinsCI/tools/setrevnums.bat
+++ Tools/JenkinsCI/tools/setrevnums.bat
@@ -2,8 +2,9 @@
 SET MG_SOURCES_DIR=C:\working\sources
 svn info "%MG_SOURCES_DIR%\fdo-3.9" | perl revnum.pl > "%MG_SOURCES_DIR%\fdo_39_revision.txt"
 svn info "%MG_SOURCES_DIR%\fdo-4.0" | perl revnum.pl > "%MG_SOURCES_DIR%\fdo_40_revision.txt"
+svn info "%MG_SOURCES_DIR%\fdo-4.1" | perl revnum.pl > "%MG_SOURCES_DIR%\fdo_41_revision.txt"
 svn info "%MG_SOURCES_DIR%\fdo-trunk" | perl revnum.pl > "%MG_SOURCES_DIR%\fdo_trunk_revision.txt"
 svn info "%MG_SOURCES_DIR%\mg-2.6\MgDev" | perl revnum.pl > "%MG_SOURCES_DIR%\mapguide_26_revision.txt"
 svn info "%MG_SOURCES_DIR%\mg-3.0\MgDev" | perl revnum.pl > "%MG_SOURCES_DIR%\mapguide_30_revision.txt"
+svn info "%MG_SOURCES_DIR%\mg-3.1\MgDev" | perl revnum.pl > "%MG_SOURCES_DIR%\mapguide_31_revision.txt"
 svn info "%MG_SOURCES_DIR%\mg-trunk\MgDev" | perl revnum.pl > "%MG_SOURCES_DIR%\mapguide_trunk_revision.txt"
-svn info "%MG_SOURCES_DIR%\diet_mapguide\MgDev" | perl revnum.pl > "%MG_SOURCES_DIR%\mapguide_31_revision.txt"
