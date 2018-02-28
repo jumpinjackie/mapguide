831e55ab7af200e04c61a496898fcb69e535f2d7
Jackie Ng <jumpinjackie@gmail.com>
2018-02-28 15:24:01 +0000

    [https://trac.osgeo.org/mapguide/ticket/2774]: Update installer version numbers and remove dead shortcuts
    git-svn-id: http://svn.osgeo.org/mapguide/branches/3.1@9327 a442ad82-b40c-0410-ae44-e6d7bd6e4a36

--- Installer/build.bat
+++ Installer/build.bat
@@ -80,9 +80,9 @@ SET PATH=%PATH%;%PARAFFIN%;%NSIS%;%WIX%\bin\;%CD%\..\MgDev\BuildTools\WebTools\7
 rem ==================================================
 rem Web dependencies
 rem ==================================================
-SET HTTPD_VERSION=2.4.18
-SET PHP_VERSION=5.6.23
-SET TOMCAT_VERSION=7.0.68
+SET HTTPD_VERSION=2.4.29
+SET PHP_VERSION=5.6.33
+SET TOMCAT_VERSION=7.0.82
 SET HTTPD_PACKAGE=httpd-%HTTPD_VERSION%-%PLATFORM_CLR%-VC11.zip
 SET TOMCAT_PACKAGE_NAME=apache-tomcat-%TOMCAT_VERSION%
 SET TOMCAT_PACKAGE=%MG_OEM%\LinuxApt\%TOMCAT_PACKAGE_NAME%.tar.gz
